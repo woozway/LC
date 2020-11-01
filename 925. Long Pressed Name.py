a3404609d327197b78986b8ea5517c681d0212bf
vuzway9132 <13806680065@163.com>
2020-11-01 14:57:03 +0800

    -

--- 925. Long Pressed Name.py
+++ 925. Long Pressed Name.py
@@ -4,13 +4,10 @@ class Solution:
         pre = '#'
         i, j = 0, 0
         while i < len(name):
-            while j<len(typed) and typed[j]!=name[i] and typed[j]==pre: j += 1
-            if j<len(typed) and typed[j]==name[i]:
-                j += 1
-            else:
-                flag = False
-                break
+            while j<len(typed) and typed[j]!=name[i] and typed[j]==pre: j+=1
+            if j<len(typed) and typed[j]==name[i]: j+=1
+            else: return False
             pre = name[i]
-            i += 1
-        while j<len(typed) and typed[j]==pre: j += 1
+            i+=1
+        while j<len(typed) and typed[j]==pre: j+=1
         return True if flag==True and j>=len(typed) else False
