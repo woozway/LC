86cb300a526d5cdaaa1b71d8313e3b002b1025dd
vuzway9132 <13806680065@163.com>
2020-11-01 10:24:25 +0800

    -

--- 19. Remove Nth Node From End of List.py
+++ 19. Remove Nth Node From End of List.py
@@ -1,8 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
         dummy = ListNode(-1, head);
@@ -11,4 +6,3 @@ class Solution:
         while runner.next: runner, chaser = runner.next, chaser.next
         chaser.next = chaser.next.next
         return dummy.next
-    
