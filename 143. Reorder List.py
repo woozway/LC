ce8f30cca7d75b63238bcf5a79743041e426f373
vuzway9132 <13806680065@163.com>
2020-11-01 13:01:55 +0800

    -

--- 143. Reorder List.py
+++ 143. Reorder List.py
@@ -7,14 +7,14 @@ class Solution:
         dq = deque()
         tmp = head
         while tmp:
-        	dq.append(tmp)
-        	tmp = tmp.next
+            dq.append(tmp)
+            tmp = tmp.next
         tmp = ListNode(-1, head)
         while len(dq):
-        	if len(dq):
-        		tmp.next = dq.popleft()
-        		tmp = tmp.next
-        	if len(dq):
-        		tmp.next = dq.pop()
-        		tmp = tmp.next
+            if len(dq):
+                tmp.next = dq.popleft()
+                tmp = tmp.next
+            if len(dq):
+                tmp.next = dq.pop()
+                tmp = tmp.next
         tmp.next = None
