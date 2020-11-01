ff2799506c02454f6420f4a0ed840529dc44ee2c
vuzway9132 <13806680065@163.com>
2020-11-01 15:42:26 +0800

    -

--- 763. Partition Labels.py
+++ 763. Partition Labels.py
@@ -1,13 +1,17 @@
 class Solution:
     def partitionLabels(self, S: str) -> List[int]:
+        """
+        record the farthest dist of char in S,
+        init left&right to indicate a group, traverse S, 
+        if dist[ch]>right, update right
+        """
         dic = {}
-        for i, c in enumerate(S):
-            dic[c] = i
-        right = dic[S[0]]
-        left = 0
+        for i, ch in enumerate(S):
+            dic[ch] = i
+        left, right = 0, dic[S[0]]
         res = []
-        for i, c in enumerate(S):
-            right = max(right, dic[c])
+        for i, ch in enumerate(S):
+            right = max(right, dic[ch])
             if i >= right:
                 res.append(right - left + 1)
                 left = right + 1 
