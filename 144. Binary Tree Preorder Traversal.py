8e6ea706d18345b7ea49e5d8f56567e9336ec57a
vuzway9132 <13806680065@163.com>
2020-11-01 18:15:31 +0800

    -

--- 144. Binary Tree Preorder Traversal.py
+++ 144. Binary Tree Preorder Traversal.py
@@ -3,15 +3,12 @@ class Solution:
         """
         Recursive solution is trivial, could you do it iteratively?
         """
-        if not root:
-            return []
+        if not root: return []
         stack = [root]
         ans = []
         while stack:
             tmp = stack.pop()
             ans.append(tmp.val)            
-            if tmp.right:
-                stack.append(tmp.right)
-            if tmp.left:
-                stack.append(tmp.left)
+            if tmp.right: stack.append(tmp.right)
+            if tmp.left:  stack.append(tmp.left)
         return ans
