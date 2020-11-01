28f076668ea1558516566d0b38810c4747fa7e8b
vuzway9132 <13806680065@163.com>
2020-11-01 15:00:34 +0800

    -

--- 925. Long Pressed Name.py
+++ 925. Long Pressed Name.py
@@ -1,13 +1,10 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
-        flag = True
-        pre = '#'
-        i, j = 0, 0
-        while i < len(name):
-            while j<len(typed) and typed[j]!=name[i] and typed[j]==pre: j+=1
-            if j<len(typed) and typed[j]==name[i]: j+=1
+        flag, j, pre = True, 0, '#'
+        for c in name:
+            while j<len(typed) and typed[j]!=c and typed[j]==pre: j+=1
+            if j<len(typed) and typed[j]==c: j+=1
             else: return False
-            pre = name[i]
-            i+=1
+            pre = c
         while j<len(typed) and typed[j]==pre: j+=1
         return True if flag==True and j>=len(typed) else False
