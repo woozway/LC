885082152e63314d84403ff32869d58e8ae81f6f
vuzway9132 <13806680065@163.com>
2020-11-01 10:26:55 +0800

    -

--- 19. Remove Nth Node From End of List.py
+++ 19. Remove Nth Node From End of List.py
@@ -1,7 +1,11 @@
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        '''
+        two pointers, runner and chaser
+        singly-linked list traversal in one pass
+        '''
         dummy = ListNode(-1, head);
-        runner, chaser = dummy, dummy
+        runner = chaser = dummy
         for i in range(n): runner = runner.next
         while runner.next: runner, chaser = runner.next, chaser.next
         chaser.next = chaser.next.next
