c0d36cb3aed42ebcb764352152249dbb9f39cb43
vuzway9132 <13806680065@163.com>
2020-11-01 18:44:46 +0800

    -

--- 144. Binary Tree Preorder Traversal.py
+++ 144. Binary Tree Preorder Traversal.py
@@ -5,12 +5,12 @@ class Solution:
         same result if you switch left&right nodes when traversing
         """
         ans, stack = [], []
-        while root or len(stack):
+        while root or stack:
             while root:
                 stack.append(root)
                 ans.append(root.val)
                 root = root.left
-            if len(stack):
+            if stack:
                 root = stack.pop()
                 root = root.right
         return ans
