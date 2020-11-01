fcd8952351cf2f312a3bfe808413f3d1542e6a4b
vuzway9132 <13806680065@163.com>
2020-11-01 20:20:05 +0800

    -

--- 1024. Video Stitching.py
+++ 1024. Video Stitching.py
@@ -1,15 +1,15 @@
 class Solution:
     def videoStitching(self, clips: List[List[int]], T: int) -> int:
         index, cnt, maxn = 0, 0, 0
-        while index<T:
+        while index < T:
             flag = 0
             for l in clips:
-                if l[0]<=index:
+                if l[0] <= index:
                     tmp = maxn
                     maxn = max(maxn, l[1])
-                    if maxn!=tmp:
+                    if maxn != tmp:
                         flag = 1
-            if flag==0:
+            if flag == 0:
                 return -1
             else:
                 index = maxn
