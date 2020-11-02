2504007149481f19d900841ec90cfb64b908cee3
chopchp <13806680065@163.com>
2020-11-02 19:25:09 +0800

    -

---
+++ 27. Remove Element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = -1
+        j = 0
+        while j < len(nums):
+            if nums[j] != val:
+                i += 1
+                nums[i] = nums[j]
+            j += 1
+        return i+1
