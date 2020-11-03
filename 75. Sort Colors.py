1711f292da5449c2cd61e657f158939e1c9f8c3a
chopchp <13806680065@163.com>
2020-11-03 12:51:08 +0800

    -

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -1,6 +1,7 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
+        invariant: [0...lt] as 0's, [gt...len(nums)-1] as 2's
         without using the library's sort function
         with a one-pass algorithm using only O(1) constant space
         """
