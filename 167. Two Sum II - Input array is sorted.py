554863fdffa4e3b0b54437e6d0e8ac6397ff2660
chopchp <13806680065@163.com>
2020-11-03 19:49:16 +0800

    -

--- 167. Two Sum II - Input array is sorted.py
+++ 167. Two Sum II - Input array is sorted.py
@@ -4,7 +4,7 @@ class Solution:
         j = len(numbers)-1
         while i < j:
             if numbers[i] + numbers[j] == target:
-                return [i+1,j+1]
+                return [i+1, j+1]
             elif numbers[i] + numbers[j] < target:
                 i += 1
             else:
