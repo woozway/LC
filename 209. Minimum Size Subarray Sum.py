ab1654814f3aafa03662057b4bb9fe028d60591d
chopchp <13806680065@163.com>
2020-11-04 21:00:23 +0800

    -

---
+++ 209. Minimum Size Subarray Sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 1 or sum(nums) < s:
+            return 0
+        i = 0
+        j = -1
+        total = 0
+        res = n + 1
+        while i <= n-1:
+            if j+1 < n and total < s:
+                j += 1
+                total += nums[j]
+            else:
+                total -= nums[i]
+                i += 1
+            if (total >= s):
+                res = min(res, j-i+1)
+        if res == n+1:
+            return 0
+        return res
