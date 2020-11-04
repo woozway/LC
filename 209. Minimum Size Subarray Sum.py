9180d2332aa5fad91d3635f16133f7e8db912a93
chopchp <13806680065@163.com>
2020-11-04 21:08:15 +0800

    -

--- 209. Minimum Size Subarray Sum.py
+++ 209. Minimum Size Subarray Sum.py
@@ -1,12 +1,15 @@
 class Solution:
     def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        """
+        O(n) solution
+        """
         n = len(nums)
         if n < 1 or sum(nums) < s:
             return 0
         i = 0
         j = -1
         total = 0
-        res = n + 1
+        res = n+1
         while i <= n-1:
             if j+1 < n and total < s:
                 j += 1
@@ -14,7 +17,7 @@ class Solution:
             else:
                 total -= nums[i]
                 i += 1
-            if (total >= s):
+            if total >= s:
                 res = min(res, j-i+1)
         if res == n+1:
             return 0
