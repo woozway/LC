b4b3e359a31638254c5707d51a02abb713652137
chopchp <13806680065@163.com>
2020-11-05 18:19:34 +0800

    -

--- 914. X of a Kind in a Deck of Cards.py
+++ 914. X of a Kind in a Deck of Cards.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def hasGroupsSizeX(self, deck):
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
         from fractions import gcd
         vals = collections.Counter(deck).values()
         return reduce(gcd, vals) >= 2
