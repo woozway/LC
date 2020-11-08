905e89bf0d801b2b135c7b40791260c4076aab60
chopchop <chopchop@darwin.local>
2020-11-08 10:13:46 +0800

    -

---
+++ Two Pointers/209. Minimum Size Subarray Sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        """
+        sliding window nums[i..j], O(n) solution
+        """
+        n = len(nums)
+        i = 0
+        j = -1
+        total = 0
+        ans = n+1
+        while i < n:
+            if j+1 < n and total < s:
+                j += 1
+                total += nums[j]
+            else:
+                total -= nums[i]
+                i += 1
+            if total >= s:
+                ans = min(ans, j-i+1)
+        return 0 if ans == n+1 else ans
