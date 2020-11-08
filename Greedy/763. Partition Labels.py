02798690eecfecb221531a4f53956c9303a6155e
chopchop <chopchop@darwin.local>
2020-11-08 09:53:16 +0800

    -

---
+++ Greedy/763. Partition Labels.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partitionLabels(self, S: str) -> List[int]:
+        """
+        record the farthest dist of char in S,
+        init left&right to indicate a group, traverse S, 
+        if dist[ch]>right, update right
+        """
+        dic = {}
+        for i, ch in enumerate(S):
+            dic[ch] = i
+        left, right = 0, dic[S[0]]
+        res = []
+        for i, ch in enumerate(S):
+            right = max(right, dic[ch])
+            if i >= right:
+                res.append(right - left + 1)
+                left = right + 1 
+        return res
