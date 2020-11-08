a1285cee427d6cc0ef05d0551de7054a63b5b3ff
chopchp <13806680065@163.com>
2020-11-08 09:41:34 +0800

    -

--- 914. X of a Kind in a Deck of Cards.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def hasGroupsSizeX(self, deck: List[int]) -> bool:
-        from fractions import gcd
-        vals = collections.Counter(deck).values()
-        return reduce(gcd, vals) >= 2
