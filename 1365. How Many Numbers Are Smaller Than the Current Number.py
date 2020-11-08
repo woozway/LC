ed7ca99a670d4c964bc10a21a6654b45c33a9687
chopchp <13806680065@163.com>
2020-11-08 09:38:02 +0800

    -

--- 1365. How Many Numbers Are Smaller Than the Current Number.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        dic = [0]*101      
-        for n in nums:
-            dic[n] += 1
-        return [sum(dic[0:n]) for n in nums]
