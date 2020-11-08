62fbebe559f22459205139ee2132f9e638389092
chopchap <13806680065@163.com>
2020-11-08 14:20:38 +0800

    -

--- Array/1582. Special Positions in a Binary Matrix.py
+++ Array/1582. Special Positions in a Binary Matrix.py
@@ -1,24 +1,6 @@
 class Solution:
     def numSpecial(self, mat: List[List[int]]) -> int:
         m = len(mat)
-        n = len(mat[0])
-        cnt = 0
-        for i in range(m):
-            for j in range(n):
-                if mat[i][j] == 1:
-                    flag = 0
-                    for k in range(m):
-                        if mat[k][j] == 1 and k != i:
-                            flag = 1
-                            break
-                    if flag == 1:
-                        continue
-                    for l in range(n):
-                        if mat[i][l] == 1 and l != j:
-                            flag = 1
-                            break
-                    if flag == 1:
-                        continue
-                    else:
-                        cnt += 1
-        return cnt
+        rowsum = [sum(x) for x in mat]
+        colsum = [sum(x) for x in zip(*mat)]
+        return sum(1 for i in range(m) if rowsum[i]==1 and colsum[mat[i].index(1)]==1)
