beb160b54b42e602fc0dd0f687c7337d53372392
chopchap <13806680065@163.com>
2020-11-10 13:51:06 +0800

    -

---
+++ Array/1431. Kids With the Greatest Number of Candies.py
@@ -0,0 +1,4 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxn = max(candies)
+        return [x+extraCandies >= maxn for x in candies]
