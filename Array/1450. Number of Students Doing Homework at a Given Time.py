2eb500d55ecabd20a8ecd30a6eabd729eb6e6ee7
chopchap <13806680065@163.com>
2020-11-11 14:09:20 +0800

    -

---
+++ Array/1450. Number of Students Doing Homework at a Given Time.py
@@ -0,0 +1,3 @@
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        return sum(1 for i in range(len(startTime)) if startTime[i] <= queryTime <= endTime[i])
