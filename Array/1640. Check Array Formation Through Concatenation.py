58ace6cd56a6aa3dd5d6e44c3123da272ce504e9
chopchap <13806680065@163.com>
2020-11-11 14:34:08 +0800

    -

--- Array/1640. Check Array Formation Through Concatenation.py
+++ Array/1640. Check Array Formation Through Concatenation.py
@@ -1,6 +1,6 @@
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
-        for i in pieces:
-            if i[0] not in arr or i != arr[arr.index(i[0]):arr.index(i[0])+len(i)]:
+        for a in pieces:
+            if a[0] not in arr or a != arr[arr.index(a[0]):arr.index(a[0])+len(a)]:
                 return False
         return True
