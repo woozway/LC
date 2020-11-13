4582f23b4b868d82cc6f6a4d8b43e2a327e5cb7f
chopchap <13806680065@163.com>
2020-11-13 19:06:59 +0800

    -

---
+++ Array/561. Array Partition I.py
@@ -0,0 +1,15 @@
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        d = [0]*20001
+        for i in range(len(nums)):
+            d[nums[i]+10000] += 1
+        i, j, ans = 0, 0, 0
+        while i < 20001:
+            if d[i]:
+                if j%2 == 0:
+                    ans += i-10000
+                d[i] -= 1
+                j += 1
+            else:
+                i += 1
+        return ans
