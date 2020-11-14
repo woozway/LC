ef2b58565e20148f58dab68045bf00d340d086fd
chopchap <13806680065@163.com>
2020-11-14 11:22:15 +0800

    -

--- Array/1475. Final Prices With a Special Discount in a Shop.py
+++ Array/1475. Final Prices With a Special Discount in a Shop.py
@@ -7,10 +7,7 @@ class Solution:
         stack = []
         ans = prices[:]
         for i in range(n):
-            while True:
-                if not stack or prices[i] > prices[stack[-1]]:
-                    stack.append(i)
-                    break
-                else:
-                    ans[stack.pop()] -= prices[i]
+            while stack and prices[i]<=prices[stack[-1]]:
+                ans[stack.pop()]-=prices[i]
+            stack.append(i)
         return ans
