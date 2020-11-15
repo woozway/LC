367679763658874ef3476895cf40d4c7d552a6db
chopchap <13806680065@163.com>
2020-11-15 10:20:26 +0800

    -

--- Array/5550. Defuse the Bomb.py
+++ Array/5550. Defuse the Bomb.py
@@ -2,12 +2,7 @@ class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
         n = len(code)
         ans = [0]*n
-        if k > 0:
-            for i in range(n):
-                for j in range(k):
-                    ans[i] += code[(i+1+j)%n]
-        elif k < 0:
-            for i in range(n):
-                for j in range(abs(k)):
-                    ans[i] += code[(i-1-j+n)%n]
+        for i in range(n):
+            for j in range(abs(k)):
+                ans[i] += code[(i+1+j)%n] if k > 0 else code[(i-1-j+n)%n]
         return ans
