5d85912e92b01e5fe5f6da611fbd41d1413d9ba9
chopchap <13806680065@163.com>
2020-11-15 19:50:08 +0800

    -

---
+++ Array/1394. Find Lucky Integer in an Array.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        freq = Counter(arr)
+        ans = [k for k in freq if k == freq[k]]
+        return max(ans) if len(ans) else -1
