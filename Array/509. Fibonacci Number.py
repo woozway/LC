5e3013e3b90ed6b731b526b933e9b0f9a2f828c7
chopchap <13806680065@163.com>
2020-11-15 20:12:27 +0800

    -

--- Array/509. Fibonacci Number.py
+++ Array/509. Fibonacci Number.py
@@ -1,7 +1,7 @@
 class Solution:
     def fib(self, N: int) -> int:
         """
-        T=O(1), S=O(1)
+        T=O(1), S=O(1), Binet's formula
         """
         golden_ratio = (1 + 5 ** 0.5) / 2
         return int((golden_ratio ** N + 1) / 5 ** 0.5)
