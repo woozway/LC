6c1a7ccd85b74aceaa7e2ca727fa49abff701eb7
chopchap <13806680065@163.com>
2020-11-16 11:00:43 +0800

    -

--- Array/66. Plus One.py
+++ Array/66. Plus One.py
@@ -1,11 +1,10 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        n = len(digits)
         carryin = 1
-        for i in range(n-1, -1, -1):
+        for i in range(len(digits)-1, -1, -1):
             tmp = digits[i] + carryin
             carryin = tmp // 10
             digits[i] = tmp % 10
-        if carryin == 1:
+        if carryin:
             digits.insert(0, 1)
         return digits
