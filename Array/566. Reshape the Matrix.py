6ed8d1c1e92639ab8577c4a00f9f71f4aba249bc
chopchap <13806680065@163.com>
2020-11-16 10:15:19 +0800

    -

--- Array/566. Reshape the Matrix.py
+++ Array/566. Reshape the Matrix.py
@@ -5,12 +5,7 @@ class Solution:
         if m * n != r * c:
             return nums
         ori = [nums[i][j] for i in range(m) for j in range(n)]
-        k = 0
         ans = []
-        for i in range(r):
-            tmp = []
-            for j in range(c):
-                tmp.append(ori[k])
-                k += 1
-            ans.append(tmp)
+        for i in range(0, len(ori), c):
+            ans.append(ori[i:i+c])
         return ans
