8143782461e77f4210483275c6b38883a9444eb5
chopchap <13806680065@163.com>
2020-11-17 11:39:08 +0800

    -

--- Array/219. Contains Duplicate II.py
+++ Array/219. Contains Duplicate II.py
@@ -2,7 +2,7 @@ class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         d = {}
         for i in range(len(nums)):
-            if nums[i] in d and d[nums[i]] >= i-k:
+            if nums[i] in d and i-d[nums[i]] <= k:
                 return True
             d[nums[i]] = i
         return False
