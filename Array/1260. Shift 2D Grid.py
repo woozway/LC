536a88d803ded2a619710bd0571544a1d061fe9f
chopchap <13806680065@163.com>
2020-11-19 10:43:11 +0800

    -

--- Array/1260. Shift 2D Grid.py
+++ Array/1260. Shift 2D Grid.py
@@ -2,7 +2,7 @@ class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         m = len(grid)
         n = len(grid[0])
-        k %= (m*n)
+        k %= m*n
         a = [grid[i][j] for i in range(m) for j in range(n)]
         b = list(reversed(a[:m*n-k])) + list(reversed(a[m*n-k:]))
         b.reverse()
