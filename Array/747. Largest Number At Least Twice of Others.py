4300c8f39eb4aed4e97a0a58ecdc375a745ea074
chopchap <13806680065@163.com>
2020-11-23 17:02:28 +0800

    -

--- Array/747. Largest Number At Least Twice of Others.py
+++ Array/747. Largest Number At Least Twice of Others.py
@@ -1,5 +1,5 @@
-class Solution(object):
-  def dominantIndex(self, nums):
+class Solution:
+  def dominantIndex(self, nums: List[int]) -> int:
     m = max(nums)
     if all(m >= 2*x for x in nums if x != m):
       return nums.index(m)
