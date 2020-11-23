242ded48772d4c50b6b948686656e24dee0eac3c
chopchap <13806680065@163.com>
2020-11-23 15:42:51 +0800

    -

--- Array/1010. Pairs of Songs With Total Durations Divisible by 60.py
+++ Array/1010. Pairs of Songs With Total Durations Divisible by 60.py
@@ -1,9 +1,9 @@
 class Solution:
   def numPairsDivisibleBy60(self, time: List[int]) -> int:
-    a = [0] * 60
+    complement = [0] * 60
     ans = 0
-    for t in time:
-      t %= 60
-      ans += a[-t]
-      a[t] += 1
+    for x in time:
+      x %= 60
+      ans += complement[-x]
+      complement[x] += 1
     return ans
