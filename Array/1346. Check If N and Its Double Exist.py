8315c2bb32203f614a7064d8f6dd2a593156ce2e
chopchap <13806680065@163.com>
2020-11-23 15:18:38 +0800

    -

--- Array/1346. Check If N and Its Double Exist.py
+++ Array/1346. Check If N and Its Double Exist.py
@@ -1,19 +1,9 @@
 class Solution:
   def checkIfExist(self, arr: List[int]) -> bool:
-    """
-    T=O(nlgn), S=O(n)
-    """
-    arr.sort()
-    q = 0
-    for p in range(len(arr)):
-      while q < len(arr) and arr[p] * 2 > arr[q]:
-        q += 1
-      if q != len(arr) and p != q and arr[p] * 2 == arr[q]:
+    counter = collections.Counter(arr)
+    for n in arr:
+      if n != 0 and counter[2*n] >= 1:
         return True
-    q = len(arr) - 1
-    for p in range(len(arr) - 1, -1, -1):
-      while q > -1 and arr[p] * 2 < arr[q]:
-        q -= 1
-      if q != -1 and p != q and arr[p] * 2 == arr[q]:
+      if n == 0 and counter[2*n] >= 2:
         return True
     return False
