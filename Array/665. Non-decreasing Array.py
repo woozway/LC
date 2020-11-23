937a0ad8da9329872ba443d75f1a1bbc44bc43ea
chopchap <13806680065@163.com>
2020-11-23 22:05:13 +0800

    -

--- Array/665. Non-decreasing Array.py
+++ Array/665. Non-decreasing Array.py
@@ -2,10 +2,10 @@ class Solution:
   def checkPossibility(self, nums: List[int]) -> bool:
     count = 0
     for i in range(1, len(nums)):
-      if nums[i] < nums[i - 1]:
+      if nums[i] < nums[i-1]:
         count += 1
         if count == 2:
           return False
-        if i - 2 >= 0 and nums[i] < nums[i - 2]:
-          nums[i] = nums[i - 1]
+        if i-2 >= 0 and nums[i] < nums[i-2]:
+          nums[i] = nums[i-1]
     return True
