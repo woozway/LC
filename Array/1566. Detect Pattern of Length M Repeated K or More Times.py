210799716025e4cb59ff84fd77d28e68a3025781
chopchop <chopchop@darwin.local>
2020-11-24 22:48:39 +0800

    -

--- Array/1566. Detect Pattern of Length M Repeated K or More Times.py
+++ Array/1566. Detect Pattern of Length M Repeated K or More Times.py
@@ -1,16 +1,3 @@
 class Solution:
   def containsPattern(self, arr: List[int], m: int, k: int) -> bool:
-    """
-    inside slice [i, i+m*k) of length m*k
-    check to see if every segement [i, i+m) is the same with each other
-    """
-    n = len(arr)
-    if n < m*k:
-      return False
-    for i in range(0, n-m*k+1):
-      flag = 1
-      for j in range(i+m, i+m*k):
-        if arr[j] != arr[j-m]:
-          flag = 0
-      if flag == 1: return True
-    return False
+    return any([arr[i:i+m] * k == arr[i:i+m*k] for i in range(len(arr))])
