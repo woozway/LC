102566253a5f95ae52cf339120fa7a8e6db9d481
chopchap <13806680065@163.com>
2020-11-27 12:09:09 +0800

    -

--- String/48. Rotate Image.py
+++ String/48. Rotate Image.py
@@ -1,10 +1,8 @@
 class Solution:
   def rotate(self, matrix: List[List[int]]) -> None:
-    n = len(matrix[0])        
-    for i in range(n//2 + n%2):
-      for j in range(n//2):
-        tmp = matrix[n-1-j][i]
-        matrix[n-1-j][i] = matrix[n-1-i][n-j-1]
-        matrix[n-1-i][n-j-1] = matrix[j][n-1-i]
-        matrix[j][n-1-i] = matrix[i][j]
-        matrix[i][j] = tmp
+    n = len(matrix[0])
+    for i in range(n):
+      for j in range(i, n):
+        matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+    for i in range(n):
+      matrix[i].reverse()
