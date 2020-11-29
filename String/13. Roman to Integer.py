e3191630847c11e4e4b041e705c208abcc5c7e5d
chopchap <13806680065@163.com>
2020-11-29 17:03:28 +0800

    -

--- String/13. Roman to Integer.py
+++ String/13. Roman to Integer.py
@@ -1,4 +1,10 @@
 class Solution:
   def romanToInt(self, s: str) -> int:
-    d = {'I':1,'IV':3,'V':5,'IX':8,'X':10,'XL':30,'L':50,'XC':80,'C':100,'CD':300,'D':500,'CM':800,'M':1000}
-    return sum(d.get(s[max(i-1, 0):i+1], d[n]) for i, n in enumerate(s))
+    a = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}        
+    ans, n = 0, len(s)
+    for i in range(n):
+      if i < n-1 and a[s[i]] < a[s[i+1]]:
+        ans -= a[s[i]]
+      else:
+        ans += a[s[i]]
+    return ans
