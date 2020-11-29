c88dbedfe0ae25a62ebf6a00af15a5fdc486fccd
chopchap <13806680065@163.com>
2020-11-29 17:04:28 +0800

    -

--- String/13. Roman to Integer.py
+++ String/13. Roman to Integer.py
@@ -1,10 +1,10 @@
 class Solution:
   def romanToInt(self, s: str) -> int:
-    a = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}        
+    d = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}        
     ans, n = 0, len(s)
     for i in range(n):
-      if i < n-1 and a[s[i]] < a[s[i+1]]:
-        ans -= a[s[i]]
+      if i < n-1 and d[s[i]] < d[s[i+1]]:
+        ans -= d[s[i]]
       else:
-        ans += a[s[i]]
+        ans += d[s[i]]
     return ans
