5371c2f06cc8620e324b5a603102abcc24957fd3
chopchap <13806680065@163.com>
2020-12-01 09:47:36 +0800

    -

--- String/1071. Greatest Common Divisor of Strings.py
+++ String/1071. Greatest Common Divisor of Strings.py
@@ -1,7 +1,3 @@
 class Solution:
   def gcdOfStrings(self, str1: str, str2: str) -> str:
-    candidate_len = math.gcd(len(str1), len(str2))
-    candidate = str1[:candidate_len]
-    if str1 + str2 == str2 + str1:
-      return candidate
-    return ''
+    return str1[:math.gcd(len(str1), len(str2))] if str1 + str2 == str2 + str1 else ''
