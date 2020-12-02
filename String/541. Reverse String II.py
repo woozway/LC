7dd75df1e47ccc0b552d937ef741ea1ba970e1fa
chopchap <13806680065@163.com>
2020-12-02 09:21:17 +0800

    -

---
+++ String/541. Reverse String II.py
@@ -0,0 +1,6 @@
+class Solution:
+  def reverseStr(self, s: str, k: int) -> str:
+    ans = []
+    for i in range(0, len(s), 2*k):
+      ans.extend(list(reversed(s[i:i+k])) + list(s[i+k:i+2*k]))
+    return ''.join(ans)
