2fb2b85ab32baf27add489baab141d3d49a25ff0
chopchap <13806680065@163.com>
2020-12-04 16:58:46 +0800

    -

--- Two Pointers/845. Longest Mountain in Array.py
+++ Two Pointers/845. Longest Mountain in Array.py
@@ -1,23 +1,16 @@
 class Solution:
   def longestMountain(self, A: List[int]) -> int:
+    if not A:
+      return 0
     n = len(A)
-    v1, v2 = [0]*n, [0]*n
+    left = [0] * n
+    for i in range(1, n):
+      left[i] = (left[i-1] + 1 if A[i-1] < A[i] else 0)   
+    right = [0] * n
+    for i in range(n-2, -1, -1):
+      right[i] = (right[i+1] + 1 if A[i+1] < A[i] else 0)
+    ans = 0
     for i in range(n):
-      if i == 0:
-        v1[i] = 1
-      else:
-        v1[i] = v1[i-1]+1 if A[i]>A[i-1] else 1
-    for i in range(n-1, -1, -1):
-      if i == n-1:
-        v2[i] = 1
-      else:
-        v2[i] = v2[i+1]+1 if A[i]>A[i+1] else 1
-    maxn, index = -1, -1
-    for i in range(n):
-      tot = v1[i]+v2[i]-1
-      if maxn < tot:
-        index = i
-        maxn = tot
-      elif maxn==tot and v1[i]>1 and v2[i]>1:
-        index = i
-    return maxn if index!=-1 and v1[index]>1 and v2[index]>1 and maxn>=3 else 0
+      if left[i] > 0 and right[i] > 0:
+        ans = max(ans, left[i] + right[i] + 1)
+    return ans
