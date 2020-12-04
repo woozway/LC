80c162938219d6bd60ceb5d5d94216b086093dc8
chopchap <13806680065@163.com>
2020-12-04 16:33:13 +0800

    -

--- Dynamic Programming/1024. Video Stitching.py
+++ Dynamic Programming/1024. Video Stitching.py
@@ -1,17 +1,17 @@
 class Solution:
-    def videoStitching(self, clips: List[List[int]], T: int) -> int:
-        index, cnt, maxn = 0, 0, 0
-        while index < T:
-            flag = 0
-            for l in clips:
-                if l[0] <= index:
-                    tmp = maxn
-                    maxn = max(maxn, l[1])
-                    if maxn != tmp:
-                        flag = 1
-            if flag == 0:
-                return -1
-            else:
-                index = maxn
-                cnt += 1
-        return cnt
+  def videoStitching(self, clips: List[List[int]], T: int) -> int:
+    index, cnt, maxn = 0, 0, 0
+    while index < T:
+      flag = 0
+      for l in clips:
+        if l[0] <= index:
+          tmp = maxn
+          maxn = max(maxn, l[1])
+          if maxn != tmp:
+            flag = 1
+      if flag == 0:
+        return -1
+      else:
+        index = maxn
+        cnt += 1
+    return cnt
