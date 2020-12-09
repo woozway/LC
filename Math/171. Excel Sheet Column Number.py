0438713f17f90661aa938c10c23d13a367aa873d
chopchap <13806680065@163.com>
2020-12-09 10:30:36 +0800

    -

--- Math/171. Excel Sheet Column Number.py
+++ Math/171. Excel Sheet Column Number.py
@@ -2,6 +2,5 @@ class Solution:
   def titleToNumber(self, s: str) -> int:
     ans = 0
     for ch in s:
-      ans *= 26
-      ans += ord(ch)-ord('A')+1
+      ans = ans*26 + ord(ch)-ord('A') + 1
     return ans
