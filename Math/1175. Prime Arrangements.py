239691c5845ac555c6b540f8b651507895c6e6b9
chopchap <13806680065@163.com>
2020-12-12 10:55:33 +0800

    -

---
+++ Math/1175. Prime Arrangements.py
@@ -0,0 +1,18 @@
+class Solution:
+  def numPrimeArrangements(self, n: int) -> int:
+    def cntPrimes(n):
+      isPrime = [1]*(n+1)
+      cnt = 0
+      for i in range(2, n+1):
+        if isPrime[i]:
+          cnt += 1
+          if i*i < n+1:
+            for j in range(i*i, n+1, i):
+              isPrime[j] = 0
+      return cnt
+    
+    def numOfPermu(n):
+      return reduce(int.__mul__, [i for i in range(1, n+1)], 1)
+    
+    cnt = cntPrimes(n)
+    return numOfPermu(cnt) * numOfPermu(n-cnt) % (10**9+7)
