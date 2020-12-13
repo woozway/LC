3423a37edc792bd5bc40e0ef2b71a2d54f179454
chopchap <13806680065@163.com>
2020-12-13 10:24:23 +0800

    -

--- Math/645. Set Mismatch.py
+++ Math/645. Set Mismatch.py
@@ -2,4 +2,4 @@ class Solution:
   def findErrorNums(self, nums: List[int]) -> List[int]:
     S = sum(set(nums))
     n = len(nums)
-    return [sum(nums)-S, n*(n+1)//2-S]
+    return [sum(nums) - S, n*(n+1)//2 - S]
