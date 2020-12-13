b29633699630145e80e0c1c926840ca1de48715c
chopchap <13806680065@163.com>
2020-12-13 10:39:29 +0800

    -

--- Math/812. Largest Triangle Area.py
+++ Math/812. Largest Triangle Area.py
@@ -1,5 +1,8 @@
 class Solution:
   def largestTriangleArea(self, points: List[List[int]]) -> float:
+    """
+    Shoelace Formula to get the area
+    """
     def area(p, q, r):
       return .5 * abs(p[0]*q[1] + q[0]*r[1] + r[0]*p[1] - p[1]*q[0] - q[1]*r[0] - r[1]*p[0])
     return max(area(*triangle) for triangle in combinations(points, 3))
