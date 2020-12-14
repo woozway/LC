5739059fd9815f41fd78ce721553269c52dd3443
chopchap <13806680065@163.com>
2020-12-14 11:14:08 +0800

    -

--- Math/892. Surface Area of 3D Shapes.py
+++ Math/892. Surface Area of 3D Shapes.py
@@ -7,9 +7,6 @@ class Solution:
         if grid[r][c]:
           ans += 2
           for nr, nc in ((r-1, c), (r+1, c), (r, c-1), (r, c+1)):
-            if 0 <= nr < N and 0 <= nc < N:
-              nval = grid[nr][nc]
-            else:
-              nval = 0
+            nval = grid[nr][nc] if 0 <= nr < N and 0 <= nc < N else 0
             ans += max(grid[r][c] - nval, 0)
     return ans
