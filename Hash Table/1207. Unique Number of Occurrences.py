1c9008ba3972d967579a435257a6cd2dc2ae3147
chopchap <13806680065@163.com>
2020-12-15 19:01:34 +0800

    -

--- Hash Table/1207. Unique Number of Occurrences.py
+++ Hash Table/1207. Unique Number of Occurrences.py
@@ -2,8 +2,8 @@ class Solution:
   def uniqueOccurrences(self, arr: List[int]) -> bool:
     d = {}
     for x in arr:
-      d[x] = 1 if not d.get(x) else d[x]+1
+      d[x] = d.get(x, 0) + 1
     s = set()
     for x in d.values():
       s.add(x)
-    return True if len(s)==len(d) else False
+    return True if len(s) == len(d) else False
