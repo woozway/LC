02af696dae60e1f553a035be704883a79f80b519
chopchap <13806680065@163.com>
2020-12-19 11:19:57 +0800

    -

---
+++ Tree/589. N-ary Tree Preorder Traversal.py
@@ -0,0 +1,10 @@
+class Solution:
+  def preorder(self, root: 'Node') -> List[int]:
+    if root is None:
+      return []
+    stack, ans = [root], []            
+    while stack:
+      root = stack.pop()
+      ans.append(root.val)
+      stack.extend(root.children[::-1])    
+    return ans
