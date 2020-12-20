51cda430189685a8957b03f6a33a81adab26facb
chopchap <13806680065@163.com>
2020-12-20 10:58:00 +0800

    -

---
+++ Tree/653. Two Sum IV - Input is a BST.py
@@ -0,0 +1,12 @@
+class Solution:
+  def findTarget(self, root: TreeNode, k: int) -> bool:
+    S = set()
+    def dfs(root):
+      if root is None:
+        return False
+      if k - root.val in S:
+        return True
+      S.add(root.val)
+      return dfs(root.left) or dfs(root.right)
+
+    return dfs(root)
