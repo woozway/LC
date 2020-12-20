4ea546b88fbd57aa0293485bea7962a8877f5975
chopchap <13806680065@163.com>
2020-12-20 20:46:11 +0800

    -

---
+++ Tree/700. Search in a Binary Search Tree.py
@@ -0,0 +1,5 @@
+class Solution:
+  def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+    if root is None or val == root.val:
+      return root
+    return self.searchBST(root.left, val) if val < root.val else self.searchBST(root.right, val)
