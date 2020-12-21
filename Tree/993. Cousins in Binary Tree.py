a1167c93c78ffcb98d47f73210e0aa845d3598a2
chopchap <13806680065@163.com>
2020-12-21 09:40:33 +0800

    -

--- Tree/993. Cousins in Binary Tree.py
+++ Tree/993. Cousins in Binary Tree.py
@@ -1,7 +1,7 @@
 class Solution:
   def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
     parent, depth = {}, {}
-    def dfs(node, par = None):
+    def dfs(node, par=None):
       if node:
         depth[node.val] = 1 + depth[par.val] if par else 0
         parent[node.val] = par
