c373da35350554bc18ea5f75cc399680cbe060a1
chopchap <13806680065@163.com>
2020-12-22 10:09:00 +0800

    -

--- Tree/110. Balanced Binary Tree.py
+++ Tree/110. Balanced Binary Tree.py
@@ -2,5 +2,4 @@ class Solution:
   def isBalanced(self, root: TreeNode) -> bool:
     def height(root):
       return 0 if not root else max(height(root.left), height(root.right)) + 1
-
     return True if not root else abs(height(root.left) - height(root.right)) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
