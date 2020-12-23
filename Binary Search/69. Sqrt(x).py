0149ad7c3244e7b40bed56e6cb6182c429c55164
chopchap <13806680065@163.com>
2020-12-23 10:16:10 +0800

    -

--- Binary Search/69. Sqrt(x).py
+++ Binary Search/69. Sqrt(x).py
@@ -1,3 +1,11 @@
 class Solution:
   def mySqrt(self, x: int) -> int:
-    return int(x**0.5)
+    l, r, ans = 0, x, -1
+    while l <= r:
+      mid = (l + r) // 2
+      if mid * mid <= x:
+        ans = mid
+        l = mid + 1
+      else:
+        r = mid - 1
+    return ans
