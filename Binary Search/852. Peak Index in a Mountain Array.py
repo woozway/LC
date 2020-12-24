60c5b2e00f09b8ee955feabe1a63cc9a1b5a17bf
chopchap <13806680065@163.com>
2020-12-24 09:45:37 +0800

    -

--- Binary Search/852. Peak Index in a Mountain Array.py
+++ Binary Search/852. Peak Index in a Mountain Array.py
@@ -2,7 +2,7 @@ class Solution:
   def peakIndexInMountainArray(self, arr: List[int]) -> int:
     lo, hi = 0, len(arr) - 1
     while lo < hi:
-      mi = (lo + hi) // 2
+      mi = lo + (hi - lo) // 2
       if arr[mi] < arr[mi + 1]:
         lo = mi + 1
       else:
