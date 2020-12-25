dcd72c94725249e243fea91a24d55641f235fb33
chopchap <13806680065@163.com>
2020-12-25 11:02:44 +0800

    -

--- Stack/225. Implement Stack using Queues.py
+++ Stack/225. Implement Stack using Queues.py
@@ -1,11 +1,9 @@
 class MyStack:
-
   def __init__(self):
     """
     Initialize your data structure here.
     """
     self.queue = collections.deque()
-
 
   def push(self, x: int) -> None:
     """
@@ -16,20 +14,17 @@ class MyStack:
     for _ in range(n):
       self.queue.append(self.queue.popleft())
 
-
   def pop(self) -> int:
     """
     Removes the element on top of the stack and returns that element.
     """
     return self.queue.popleft()
 
-
   def top(self) -> int:
     """
     Get the top element.
     """
     return self.queue[0]
-
 
   def empty(self) -> bool:
     """
