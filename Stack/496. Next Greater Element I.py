ff610e383d34b68b53985d04ca7b623ab306ea22
chopchap <13806680065@163.com>
2020-12-25 12:25:38 +0800

    -

--- Stack/496. Next Greater Element I.py
+++ Stack/496. Next Greater Element I.py
@@ -1,13 +1,9 @@
 class Solution:
   def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-    stack, ans = [], []
+    stack = []
     nextGreater = {}
     for num in nums2:
       while stack and num > stack[-1]:
         nextGreater[stack.pop()] = num
       stack.append(num)
-    while stack:
-      nextGreater[stack.pop()] = -1
-    for num in nums1:
-      ans.append(nextGreater[num])
-    return ans
+    return [nextGreater.get(num, -1) for num in nums1]
