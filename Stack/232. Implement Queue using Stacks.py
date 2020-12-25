9a20266f6a1eb742086d39b148a0055f7cd68f8c
chopchap <13806680065@163.com>
2020-12-25 11:39:34 +0800

    -

---
+++ Stack/232. Implement Queue using Stacks.py
@@ -0,0 +1,22 @@
+class MyQueue:
+  def __init__(self):
+    self.s1 = []
+    self.s2 = []
+    self.front = None
+
+  def push(self, x: int) -> None:
+    if not self.s1: self.front = x
+    self.s1.append(x)
+
+  def pop(self) -> int:
+    if not self.s2:
+      while self.s1:
+        self.s2.append(self.s1.pop())
+      self.front = None
+    return self.s2.pop()
+
+  def peek(self) -> int:
+    return self.s2[-1] if self.s2 else self.front
+
+  def empty(self) -> bool:
+    return True if not self.s1 and not self.s2 else False
