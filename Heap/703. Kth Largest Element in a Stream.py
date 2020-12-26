532e236a590935cdfcbad5caf17875fe1bf27493
chopchap <13806680065@163.com>
2020-12-26 11:44:40 +0800

    -

--- Heap/703. Kth Largest Element in a Stream.py
+++ Heap/703. Kth Largest Element in a Stream.py
@@ -1,14 +1,14 @@
 class KthLargest:
   def __init__(self, k: int, nums: List[int]):
     self._nums = nums
-    heapq.heapify(self._nums)
+    heapify(self._nums)
     self._k = k
     while len(self._nums) > k:
-      heapq.heappop(self._nums)
+      heappop(self._nums)
 
   def add(self, val: int) -> int:
     if len(self._nums) < self._k:
-      heapq.heappush(self._nums, val)
+      heappush(self._nums, val)
     elif val > self._nums[0]:
-      heapq.heapreplace(self._nums, val)
+      heapreplace(self._nums, val)
     return self._nums[0]
