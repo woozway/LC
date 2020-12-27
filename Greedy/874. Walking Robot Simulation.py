f785114d82e791f735667cc90f4e731264ba8627
chopchap <13806680065@163.com>
2020-12-27 08:39:42 +0800

    -

--- Greedy/874. Walking Robot Simulation.py
+++ Greedy/874. Walking Robot Simulation.py
@@ -2,9 +2,8 @@ class Solution:
   def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
-    x = y = di = 0
+    x = y = di = ans = 0
     obstacleSet = set(map(tuple, obstacles))
-    ans = 0
     for cmd in commands:
       if cmd == -2:
         di = (di - 1) % 4
