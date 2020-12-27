9f48a91af894d65a982c8270ebc66930a6a4a3a6
chopchap <13806680065@163.com>
2020-12-27 09:18:56 +0800

    -

--- Greedy/944. Delete Columns to Make Sorted.py
+++ Greedy/944. Delete Columns to Make Sorted.py
@@ -1,3 +1,3 @@
 class Solution:
   def minDeletionSize(self, A: List[str]) -> int:
-    return sum(1 for s in [list(s) for s in zip(*A)] if sorted(s) != s)
+    return sum(1 for col in zip(*A) if any(col[i] > col[i+1] for i in range(len(col) - 1)))
