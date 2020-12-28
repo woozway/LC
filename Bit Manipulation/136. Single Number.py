ec6283c8c44b23d90ad5ecd3c67a37dd926c01f2
chopchap <13806680065@163.com>
2020-12-28 08:57:37 +0800

    -

--- Bit Manipulation/136. Single Number.py
+++ Bit Manipulation/136. Single Number.py
@@ -1,3 +1,6 @@
 class Solution:
   def singleNumber(self, nums: List[int]) -> int:
+    """ a ^ 0 = a, a ^ a = 0, 
+        a ^ b ^ a = b ^ a ^ a = b ^ (a ^ a) = b ^ 0 = b
+    """
     return reduce(lambda x, y: x ^ y, nums)
