cf57c21ac28dfaea5ed92ac38e9413e01cf0022c
chopchap <13806680065@163.com>
2020-12-28 20:13:11 +0800

    -

--- Array/1. Two Sum.py
+++ Array/1. Two Sum.py
@@ -1,8 +1,7 @@
 class Solution:
-  def twoSum(self, nums: List[int], target: int) -> List[int]:
-    d = {}
-    for i, x in enumerate(nums):
-      if target-x in d:
-        return [d[target-x], i]
-      d[x] = i
-    return []
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict()
+        for i, x in enumerate(nums):
+            if target - x in d:
+                return [d[target - x], i]
+            d[x] = i
