7796aebe734dc01d85a1f44a79b0f1a48e7e3df0
chopchap <13806680065@163.com>
2020-12-29 09:54:52 +0800

    -

--- Bit Manipulation/405. Convert a Number to Hexadecimal.py
+++ Bit Manipulation/405. Convert a Number to Hexadecimal.py
@@ -2,8 +2,7 @@ class Solution:
     def toHex(self, num: int) -> str:
         if num == 0:
             return '0'
-        hex = '0123456789abcdef'
-        ans = ''
+        ans, hex = '', '0123456789abcdef'
         while num and len(ans) < 8:
             ans = hex[num & 0xf] + ans
             num >>= 4
