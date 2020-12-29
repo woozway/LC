1432f98103f757330550926db66ed5a4f418fcbc
chopchap <13806680065@163.com>
2020-12-29 20:10:10 +0800

    -

--- Backtracking/37. Sudoku Solver.py
+++ Backtracking/37. Sudoku Solver.py
@@ -22,6 +22,6 @@ class Solution:
             if board[i][col] != '.' and board[i][col] == c: return False
             if board[row][i] != '.' and board[row][i] == c: return False
             if board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] != '.'\
-              and board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == c:
+                    and board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == c:
                 return False
         return True
