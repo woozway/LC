40a3340791072e8e7a0f1938a124a05b452394a0
chopchap <13806680065@163.com>
2020-12-31 00:08:33 +0800

    -

--- Trie/212. Word Search II.py
+++ Trie/212. Word Search II.py
@@ -7,18 +7,14 @@ class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         if not board or not board[0]: return []
         if not words: return []
-        
         self.result = set()
-        
         root = collections.defaultdict()
         for word in words:
             node = root
             for char in word:
                 node = node.setdefault(char, collections.defaultdict())
             node[END_OF_WORD] = END_OF_WORD
-        
         self.m, self.n = len(board), len(board[0])
-        
         for i in range(self.m):
             for j in range(self.n):
                 if board[i][j] in root:
@@ -28,10 +24,8 @@ class Solution:
     def _dfs(self, board, i, j, cur_word, cur_dict):
         cur_word += board[i][j]
         cur_dict = cur_dict[board[i][j]]
-
         if END_OF_WORD in cur_dict:
             self.result.add(cur_word)
-
         tmp, board[i][j] = board[i][j], '@'
         for k in range(4):
             x, y = i + dx[k], j + dy[k]
@@ -39,4 +33,3 @@ class Solution:
                 and board[x][y] != '@' and board[x][y] in cur_dict:
                 self._dfs(board, x, y, cur_word, cur_dict)
         board[i][j] = tmp
-            
