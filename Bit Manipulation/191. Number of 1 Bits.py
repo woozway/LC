7f348f22e8a5fef30abebf1f51c81ef0ae0890eb
chopchap <13806680065@163.com>
2020-12-31 14:22:23 +0800

    -

--- Bit Manipulation/191. Number of 1 Bits.py
+++ Bit Manipulation/191. Number of 1 Bits.py
@@ -1,8 +1,10 @@
 class Solution:
-  def hammingWeight(self, n: int) -> int:
-    """ Brian Kernighan Algorithm """
-    cnt = 0
-    while n:
-      n &= n-1
-      cnt += 1
-    return cnt
+    def hammingWeight(self, n: int) -> int:
+        """ Brian Kernighan Algorithm """
+        rst = 0
+        mask = 1
+        for i in range(32):
+            if n & mask:
+                rst += 1
+            mask <<= 1
+        return rst
