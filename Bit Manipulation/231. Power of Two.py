f24f89a7bbdeea5aca21806eec6d4fcfacf21444
chopchap <13806680065@163.com>
2020-12-31 14:29:36 +0800

    -

--- Bit Manipulation/231. Power of Two.py
+++ Bit Manipulation/231. Power of Two.py
@@ -1,8 +1,9 @@
 class Solution:
-  def isPowerOfTwo(self, n: int) -> bool:
-    """
-    T=O(1), S=O(1)
-    get the rightmost 1-bit in n: x & (-x)
-    set the rightmost 1-bit to 0-bit in n: x & (x - 1)
-    """
-    return False if n == 0 else n & (n - 1) == 0
+    def isPowerOfTwo(self, n: int) -> bool:
+        """
+        T=O(1), S=O(1)
+        get the rightmost 1-bit in n: x & (-x)
+        set the rightmost 1-bit to 0-bit in n: x & (x - 1)
+        """
+        if n == 0: return False
+        return n & (n - 1) == 0
