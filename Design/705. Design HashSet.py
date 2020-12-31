4572bb5a88de799bf2ce4d38917cab9221c5339a
chopchap <13806680065@163.com>
2020-12-31 08:57:20 +0800

    -

---
+++ Design/705. Design HashSet.py
@@ -0,0 +1,92 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.keyRange = 769
+        self.bucketArray = [Bucket() for i in range(self.keyRange)]
+
+    def _hash(self, key) -> int:
+        return key % self.keyRange
+
+    def add(self, key: int) -> None:
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].insert(key)
+
+    def remove(self, key: int) -> None:
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].delete(key)
+
+    def contains(self, key: int) -> bool:
+        bucketIndex = self._hash(key)
+        return self.bucketArray[bucketIndex].exists(key)
+
+class Bucket:
+    def __init__(self):
+        self.tree = BSTree()
+
+    def insert(self, value):
+        self.tree.root = self.tree.insertIntoBST(self.tree.root, value)
+
+    def delete(self, value):
+        self.tree.root = self.tree.deleteNode(self.tree.root, value)
+
+    def exists(self, value):
+        return (self.tree.searchBST(self.tree.root, value) is not None)
+
+class TreeNode:
+    def __init__(self, value):
+        self.val = value
+        self.left = None
+        self.right = None
+
+class BSTree:
+    def __init__(self):
+        self.root = None
+
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root is None or val == root.val:
+            return root
+
+        return self.searchBST(root.left, val) if val < root.val \
+            else self.searchBST(root.right, val)
+
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if not root:
+            return TreeNode(val)
+
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        elif val == root.val:
+            return root
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
+
+    def successor(self, root):
+        root = root.right
+        while root.left:
+            root = root.left
+        return root.val
+
+    def predecessor(self, root):
+        root = root.left
+        while root.right:
+            root = root.right
+        return root.val
+
+    def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
+        if not root:
+            return None
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not (root.left or root.right):
+                root = None
+            elif root.right:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
+            else:
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
+        return root
