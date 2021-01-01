ea771642e35d4fe7ada17d43d127bda70feab411
chopchap <13806680065@163.com>
2021-01-01 14:49:31 +0800

    -

--- Dynamic Programming/72. Edit Distance.py
+++ Dynamic Programming/72. Edit Distance.py
@@ -1,7 +1,7 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         m, n = len(word1), len(word2)
-        dp= [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
         
         for i in range(m + 1): dp[i][0] = i
         for j in range(n + 1): dp[0][j] = j
