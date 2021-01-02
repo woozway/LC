21a490fa40cf6f9bb6c8bddce83687b02551645e
chopchap <13806680065@163.com>
2021-01-02 10:08:30 +0800

    -

--- Hash Table/599. Minimum Index Sum of Two Lists.py
+++ Hash Table/599. Minimum Index Sum of Two Lists.py
@@ -3,5 +3,5 @@ class Solution:
         index1 = {list1[i]: i for i in range(len(list1))}
         index2 = {list2[i]: i for i in range(len(list2))}
         agreements = set(list1) & set(list2)
-        sum_index = {s: index1[s]+index2[s] for s in agreements}
+        sum_index = {s : index1[s] + index2[s] for s in agreements}
         return [s for s in agreements if sum_index[s] == min(sum_index.values())]
