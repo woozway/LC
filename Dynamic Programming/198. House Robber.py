dc1042d6943498edff13ac2840f652cd3269b01b
chopchap <13806680065@163.com>
2021-01-04 10:55:13 +0800

    -

--- Dynamic Programming/198. House Robber.py
+++ Dynamic Programming/198. House Robber.py
@@ -1,11 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        if not nums: return 0
 
         n = len(nums)
-        if n == 1:
-            return nums[0]
+        if n == 1: return nums[0]
         
         first, second = nums[0], max(nums[0], nums[1])
         for i in range(2, n):
