6e808b66c1f04af60b8c488845f6a5c3e07b3db9
chopchap <13806680065@163.com>
2021-01-12 19:01:19 +0800

    -

--- Linked List/141. Linked List Cycle.py
+++ Linked List/141. Linked List Cycle.py
@@ -1,3 +1,30 @@
+"""
+1. Clarification
+2. Possible solutions
+ - use set to check whether a node has been met before
+ - fast & slow pointers
+3. Coding
+4. Tests
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+# # T=O(n), S=O(n)
+# class Solution:
+#     def hasCycle(self, head: ListNode) -> bool:
+#         S = set()
+#         while head:
+#             if head in S:
+#                 return True
+#             S.add(head)
+#             head = head.next
+#         return False
+
+# T=O(n), S=O(1)
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
         fast = slow = head
