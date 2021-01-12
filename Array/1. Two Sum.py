0e6168426192e31e91d3d42b8ec46dbb9e002136
chopchap <13806680065@163.com>
2021-01-12 14:55:20 +0800

    -

--- Array/1. Two Sum.py
+++ Array/1. Two Sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = dict()
+        hash_map = dict()
         for i, x in enumerate(nums):
-            if target - x in d:
-                return [d[target - x], i]
-            d[x] = i
+            if target - x in hash_map:
+                return [hash_map[target - x], i]
+            hash_map[x] = i
