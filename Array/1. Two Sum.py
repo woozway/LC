eb14afc6e8a187cd6f9f7c3704636b628aa95733
chopchap <13806680065@163.com>
2021-01-12 15:28:34 +0800

    -

--- Array/1. Two Sum.py
+++ Array/1. Two Sum.py
@@ -1,3 +1,21 @@
+"""
+1. Clarification
+2. Possible solutions
+  - brute-force
+  - use set
+3. Coding
+4. Tests
+"""
+# # T=O(n^2), S=O(1)
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         n = len(nums)
+#         for i in range(n):
+#             for j in range(i+1, n):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+
+# T=O(n), S=O(n)
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash_map = dict()
