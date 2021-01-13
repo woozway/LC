b7a07b7703aa3e3e4e9d7c076bc9ca8850cb9f06
chopchap <13806680065@163.com>
2021-01-13 14:22:28 +0800

    -

--- Sliding Window/239. Sliding Window Maximum.py
+++ Sliding Window/239. Sliding Window Maximum.py
@@ -1,13 +1,35 @@
+"""
+1. Clarification
+2. Possible solutions
+ - use MaxHeap
+ - use monotonous Queue
+3. Coding
+4. Tests
+"""
+
+# # T=O(nlgn), S=O(n)
+# class Solution:
+#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+#         q, res = [], []
+#         for i in range(len(nums)):
+#             while q and q[0][1] <= i - k:
+#                 heapq.heappop(q)
+#             heapq.heappush(q, (-nums[i], i))
+#             if i >= k - 1:
+#                 res.append(-q[0][0])
+#         return res
+
+# T=O(n), S=O(k)
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        d = collections.deque()
-        out = []
-        for i, n in enumerate(nums):
-            while d and nums[d[-1]] < n:
-                d.pop()
-            d += i,
-            if d[0] == i - k:
-                d.popleft()
+        if not nums: return []
+        window, res = [], []
+        for i, x in enumerate(nums):
+            if i >= k and window[0] <= i - k:
+                window.pop(0)
+            while window and nums[window[-1]] <= x:
+                window.pop()
+            window.append(i)
             if i >= k - 1:
-                out += nums[d[0]],
-        return out
+                res.append(nums[window[0]])
+        return res
