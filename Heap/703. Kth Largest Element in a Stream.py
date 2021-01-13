c5e009959de4e5fab6c094f6a08bf776c6fae62a
chopchap <13806680065@163.com>
2021-01-13 11:27:39 +0800

    -

--- Heap/703. Kth Largest Element in a Stream.py
+++ Heap/703. Kth Largest Element in a Stream.py
@@ -1,15 +1,45 @@
+"""
+1. Clarification
+2. Possible solutions
+ - keep max k elements by sorting
+ - use MinHeap of size k
+3. Coding
+4. Tests
+"""
+
+# # T=O(klgk) per operation
+# class KthLargest:
+# 
+#     def __init__(self, k: int, nums: List[int]):
+#         self.k = k
+#         self.arr = sorted(nums, reverse=True)[:k]
+# 
+#     def add(self, val: int) -> int:
+#         if len(self.arr) < self.k:
+#             self.arr.append(val)
+#         elif val > self.arr[-1]:
+#             self.arr[-1] = val
+#         self.arr.sort(reverse=True)
+#         return self.arr[-1]
+
+
+# T=O(lgk) per operation
 class KthLargest:
 
     def __init__(self, k: int, nums: List[int]):
-        self.pool = nums
         self.k = k
-        heapq.heapify(self.pool)
-        while len(self.pool) > k:
-            heapq.heappop(self.pool)
-        
+        self.mhp = nums
+        heapq.heapify(self.mhp)
+        while len(self.mhp) > k:
+            heapq.heappop(self.mhp)
+
     def add(self, val: int) -> int:
-        if len(self.pool) < self.k:
-            heapq.heappush(self.pool, val)
-        elif val > self.pool[0]:
-            heapq.heapreplace(self.pool, val)
-        return self.pool[0]
+        if len(self.mhp) < self.k:
+            heapq.heappush(self.mhp, val)
+        elif val > self.mhp[0]:
+            heapq.heapreplace(self.mhp, val)
+        return self.mhp[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
