bbed99e1ce5c1e93b8dc0e4f89e5cfd8751ec9aa
chopchap <13806680065@163.com>
2021-01-13 10:03:45 +0800

    -

--- Stack/20. Valid Parentheses.py
+++ Stack/20. Valid Parentheses.py
@@ -1,7 +1,24 @@
+"""
+1. Clarification
+2. Possible solutions
+ - replace adjacent matched brackets
+ - use stack to match brackets
+3. Coding
+4. Tests
+"""
+
+# # T=O(n^2), S=O(1)
+# class Solution:
+#     def isValid(self, s: str) -> bool:
+#         while '()' in s or '[]' in s or '{}' in s:
+#             s = s.replace('()', '').replace('[]', '').replace('{}', '')
+#         return s == ''
+
+# T=O(n), S=O(n)
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        paren_map = {')':'(', ']':'[', '}':'{'}
+        paren_map = {')': '(', ']': '[', '}': '{'}
         for c in s:
             if c not in paren_map:
                 stack.append(c)
