f2882f255d3e5692a435f39059592b24a6cfd9a2
chopchap <13806680065@163.com>
2021-01-13 21:28:38 +0800

    -

--- Hash Table/242. Valid Anagram.py
+++ Hash Table/242. Valid Anagram.py
@@ -1,3 +1,18 @@
+"""
+1. Clarification
+2. Possible solutions
+ - sort string s & t
+ - hashMap
+3. Coding
+4. Tests
+"""
+
+# # T=O(nlgn), S=O(n)
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         return sorted(s) == sorted(t)
+
+# T=O(n), S=O(n)
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return collections.Counter(s) == collections.Counter(t)
