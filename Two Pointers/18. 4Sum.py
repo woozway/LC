622ea450ed6515bf0a0530d9bca1c906bd11747c
chopchap <13806680065@163.com>
2021-01-14 12:18:27 +0800

    -

--- Two Pointers/18. 4Sum.py
+++ Two Pointers/18. 4Sum.py
@@ -7,20 +7,20 @@
 4. Tests
 """
 
-# T=O(n^4), S=O(1), Time Limit Exceeded
-class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        n = len(nums)
-        if n < 4: return []
-        nums.sort()
-        res = set()
-        for i in range(n):
-            for j in range(i+1, n):
-                for k in range(j+1, n):
-                    for l in range(k+1, n):
-                        if nums[i] + nums[j] + nums[k] + nums[l] == target:
-                            res.add((nums[i], nums[j], nums[k], nums[l]))
-        return res
+# # T=O(n^4), S=O(1), Time Limit Exceeded
+# class Solution:
+#     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+#         n = len(nums)
+#         if n < 4: return []
+#         nums.sort()
+#         res = set()
+#         for i in range(n):
+#             for j in range(i+1, n):
+#                 for k in range(j+1, n):
+#                     for l in range(k+1, n):
+#                         if nums[i] + nums[j] + nums[k] + nums[l] == target:
+#                             res.add((nums[i], nums[j], nums[k], nums[l]))
+#         return res
 
 # T=O(n^3), S=O(1)
 class Solution:
