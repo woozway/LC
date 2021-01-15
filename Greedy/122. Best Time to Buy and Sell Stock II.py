cf8aa96fae416897c3489804f0fd46feff347fd2
chopchap <13806680065@163.com>
2021-01-15 14:04:22 +0800

    -

--- Greedy/122. Best Time to Buy and Sell Stock II.py
+++ Greedy/122. Best Time to Buy and Sell Stock II.py
@@ -1,8 +1,45 @@
+"""
+1. Clarification
+2. Possible solutions
+ - brute force, dfs
+ - greedy algo
+ - dp
+3. Coding
+4. Tests
+"""
+
+# # T=O(2^n), S=O(n), Time Limit Exceeded
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         return self.calculate(prices, 0)
+#
+#     def calculate(self, prices, index):
+#         if index >= len(prices): return 0
+#         maxn = 0
+#         for i in range(index, len(prices)):
+#             maxprofit = 0
+#             for j in range(i+1, len(prices)):
+#                 if prices[i] < prices[j]:
+#                     profit = self.calculate(prices, j+1) + (prices[j] - prices[i])
+#                     maxprofit = max(maxprofit, profit)
+#                 maxn = max(maxn, maxprofit)
+#         return maxn
+
+# T=O(n), S=O(1)
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        """ T=O(n), S=O(1), Greedy algo """
         profit = 0
         for i in range(1, len(prices)):
             if prices[i] > prices[i-1]:
                 profit += prices[i] - prices[i-1]
         return profit
+
+# # T=O(n), S=O(1)
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         cur_hold, cur_not_hold = -inf, 0
+#         for stock_price in prices:
+#             prev_hold, prev_not_hold = cur_hold, cur_not_hold
+#             cur_hold = max(prev_hold, prev_not_hold - stock_price)
+#             cur_not_hold = max(prev_not_hold, prev_hold + stock_price)   
+#         return cur_not_hold
