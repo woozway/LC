b26535b7eaff7938f1633836765058daf1a39864
chopchap <13806680065@163.com>
2021-01-17 20:22:27 +0800

    -

---
+++ Bit Manipulation/1720. Decode XORed Array.py
@@ -0,0 +1,15 @@
+"""
+1. Clarification
+2. Possible solutions
+ - simulation
+3. Coding
+4. Tests
+"""
+
+# T=O(n), S=O(n)
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        ret = [first]
+        for x in encoded:
+            ret.append(x ^ ret[-1])
+        return ret
