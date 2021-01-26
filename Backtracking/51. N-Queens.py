2e9fe5318c5c4e1916e612cea56d676fc390c8cd
chopchap <13806680065@163.com>
2021-01-26 18:47:02 +0800

    -

--- Backtracking/51. N-Queens.py
+++ Backtracking/51. N-Queens.py
@@ -1,3 +1,13 @@
+"""
+1. Clarification
+2. Possible solutions
+ - brute force
+ - backtracking
+3. Coding
+4. Tests
+"""
+
+# T=O(n!), S=O(n)
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         if n < 1: return []
@@ -5,10 +15,10 @@ class Solution:
         self.cols = set(); self.pie = set(); self.na = set()
         self.dfs(n, 0, [])
         return self._generate_result(n)
-    
-    def dfs(self, n, row, cur_state):
+
+    def dfs(self, n, row, cur_list):
         if row >= n:
-            self.result.append(cur_state)
+            self.result.append(cur_list)
             return
         for col in range(n):
             if col in self.cols or row + col in self.pie or row - col in self.na:
@@ -16,11 +26,11 @@ class Solution:
             self.cols.add(col)
             self.pie.add(row + col)
             self.na.add(row - col)
-            self.dfs(n, row + 1, cur_state + [col])
+            self.dfs(n, row + 1, cur_list + [col])
             self.cols.remove(col)
             self.pie.remove(row + col)
             self.na.remove(row - col)
-            
+
     def _generate_result(self, n):
         board = []
         for res in self.result:
