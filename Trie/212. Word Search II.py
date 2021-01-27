aaf70431738fe703cadba636e4e0a44ac035d748
chopchap <13806680065@163.com>
2021-01-27 19:06:11 +0800

    -

--- Trie/212. Word Search II.py
+++ Trie/212. Word Search II.py
@@ -1,3 +1,13 @@
+"""
+1. Clarification
+2. Possible solutions
+ - naive dfs
+ - trie + dfs + backtracking
+3. Coding
+4. Tests
+"""
+
+# trie + dfs + backtracking
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
@@ -5,38 +15,29 @@ END_OF_WORD = '#'
 
 class Solution:
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        if not board or not board[0]: return []
-        if not words: return []
-        
+        if not board or not board[0] or not words: return []
         self.result = set()
-        
-        root = collections.defaultdict()
+        root = dict()
         for word in words:
             node = root
             for char in word:
-                node = node.setdefault(char, collections.defaultdict())
+                node = node.setdefault(char, {})
             node[END_OF_WORD] = END_OF_WORD
-            
-        self.m, self.n = len(board), len(board[0])
-        
-        for i in range(self.m):
-            for j in range(self.n):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
                 if board[i][j] in root:
-                    self._dfs(board, i, j, '', root)
-                    
+                    self.dfs(board, i, j, '', root)
         return list(self.result)
-                    
-    def _dfs(self, board, i, j, cur_word, cur_dict):
-        cur_word += board[i][j]
-        cur_dict = cur_dict[board[i][j]]
-        
+
+    def dfs(self, board, row, col, cur_word, cur_dict):
+        cur_word += board[row][col]
+        cur_dict = cur_dict[board[row][col]]
         if END_OF_WORD in cur_dict:
             self.result.add(cur_word)
-            
-        tmp, board[i][j] = board[i][j], '@'
-        for k in range(4):
-            x, y = i + dx[k], j + dy[k]
-            if 0 <= x < self.m and 0 <= y < self.n \
-                and board[x][y] != '@' and board[x][y] in cur_dict:
-                self._dfs(board, x, y, cur_word, cur_dict)
-        board[i][j] = tmp
+        tmp, board[row][col] = board[row][col], '@'
+        m, n = len(board), len(board[0])
+        for i in range(4):
+            x, y = row + dx[i], col + dy[i]
+            if 0 <= x < m and 0 <= y < n and board[x][y] != '@' and board[x][y] in cur_dict:
+                self.dfs(board, x, y, cur_word, cur_dict)
+        board[row][col] = tmp
