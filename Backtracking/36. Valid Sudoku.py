3db0c596f130adea4e05ae922a9738ce381e7157
chopchap <13806680065@163.com>
2021-01-27 12:14:56 +0800

    -

---
+++ Backtracking/36. Valid Sudoku.py
@@ -0,0 +1,29 @@
+"""
+1. Clarification
+2. Possible solutions
+ - simple backtracking + naive dfs
+3. Coding
+4. Tests
+"""
+
+# T=O(1), S=O(1)
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        if not board: return False
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == '.':
+                    continue
+                c = board[i][j]
+                board[i][j] = '.'
+                if self.isValid(board, i, j, c) == False:
+                    return False
+                board[i][j] = c
+        return True
+
+    def isValid(self, board, row, col, c):
+        for i in range(len(board)):
+            if board[row][i] == c: return False
+            if board[i][col] == c: return False
+            if board[3*(row//3) + i//3][3*(col//3) + i%3] == c: return False
+        return True
