1d9b7361ff8e1ac72d68aeee41fcdbeb6895dff1
chopchap <13806680065@163.com>
2021-01-27 12:16:26 +0800

    -

--- Backtracking/36. Valid Sudoku.py
+++ Backtracking/36. Valid Sudoku.py
@@ -12,13 +12,12 @@ class Solution:
         if not board: return False
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] == '.':
-                    continue
-                c = board[i][j]
-                board[i][j] = '.'
-                if self.isValid(board, i, j, c) == False:
-                    return False
-                board[i][j] = c
+                if board[i][j] != '.':
+                    c = board[i][j]
+                    board[i][j] = '.'
+                    if not self.isValid(board, i, j, c):
+                        return False
+                    board[i][j] = c
         return True
 
     def isValid(self, board, row, col, c):
