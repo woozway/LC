76600d2ce984d7a23eb72cf41593db27d6b1e1da
chopchap <13806680065@163.com>
2021-01-28 10:40:16 +0800

    -

--- Bit Manipulation/231. Power of Two.py
+++ Bit Manipulation/231. Power of Two.py
@@ -1,8 +1,35 @@
+"""
+1. Clarification
+2. Possible solutions
+ - mod
+ - log2
+ - bit manipulation (2 versions)
+3. Coding
+4. Tests
+"""
+
+# # T=O(lgn), S=O(1)
+# class Solution:
+#     def isPowerOfTwo(self, n: int) -> bool:
+#         if n <= 0: return False
+#         while n & 1 == 0:
+#             n >>= 1
+#         return n == 1
+
+# # T=O(1), S=O(1)
+# class Solution:
+#     def isPowerOfTwo(self, n: int) -> bool:
+#         if n <= 0: return False
+#         return math.log2(n) % 1 == 0
+
+# # T=O(1), S=O(1)
+# class Solution:
+#     def isPowerOfTwo(self, n: int) -> bool:
+#         if n <= 0: return False
+#         return n & (-n) == n
+
+# T=O(1), S=O(1)
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        """
-        T=O(1), S=O(1)
-        get the rightmost 1-bit in n: x & (-x)
-        set the rightmost 1-bit to 0-bit in n: x & (x - 1)
-        """
-        return n > 0 and not (n & n-1)
+        if n <= 0: return False
+        return (n & (n - 1)) == 0
