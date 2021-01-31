191973afe7878239e561a04de00bd2aab7b0e987
chopchap <13806680065@163.com>
2021-01-31 20:59:28 +0800

    -

--- Dynamic Programming/152. Maximum Product Subarray.py
+++ Dynamic Programming/152. Maximum Product Subarray.py
@@ -13,7 +13,7 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         if nums is None: return 0
         dp = [[0] * 2 for _ in range(2)]
-        dp[0][0], dp[0][1], res = nums[0], nums[0], nums[0]
+        dp[0][0] = dp[0][1] = res = nums[0]
         for i in range(1, len(nums)):
             x, y = i % 2, (i - 1) % 2
             dp[x][0] = max(dp[y][0] * nums[i], dp[y][1] * nums[i], nums[i])
