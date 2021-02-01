f2b2316bd16aff2e5310350d8420e6db7866c96a
chopchap <13806680065@163.com>
2021-02-01 10:52:24 +0800

    -

--- Dynamic Programming/121. Best Time to Buy and Sell Stock.py
+++ Dynamic Programming/121. Best Time to Buy and Sell Stock.py
@@ -1,15 +1,35 @@
+"""
+1. Clarification
+2. Possible solutions
+ - one pass
+ - dynamic programming
+3. Coding
+4. Tests
+"""
+
+
+# # T=O(n), S=O(1)
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         inf = int(1e9)
+#         minPrice = inf
+#         maxProf = 0
+#         for price in prices:
+#             maxProf = max(price - minPrice, maxProf)
+#             minPrice = min(price, minPrice)
+#         return maxProf
+
+
+# T=O(n), S=O(n)
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if not prices: return 0
-        
         res = 0
-        profit = [[0 for i in range(3)] for i in range(len(prices))]
+        profit = [[0] * 3 for i in range(len(prices))]
         profit[0][0], profit[0][1], profit[0][2] = 0, -prices[0], 0
-        
         for i in range(1, len(prices)):
-            profit[i][0] = profit[i-1][0]
-            profit[i][1] = max(profit[i-1][1], profit[i-1][0] - prices[i])
-            profit[i][2] = profit[i-1][1] + prices[i]
+            profit[i][0] = profit[i - 1][0]
+            profit[i][1] = max(profit[i - 1][1], profit[i - 1][0] - prices[i])
+            profit[i][2] = profit[i - 1][1] + prices[i]
             res = max(res, profit[i][0], profit[i][1], profit[i][2])
-            
         return res
