6c5b038cdc2acca6e81b0cf5213136dfa4d8397e
chopchap <13806680065@163.com>
2021-02-01 12:10:17 +0800

    -

--- Dynamic Programming/123. Best Time to Buy and Sell Stock III.py
+++ Dynamic Programming/123. Best Time to Buy and Sell Stock III.py
@@ -1,20 +1,25 @@
+"""
+1. Clarification
+2. Possible solutions
+ - dp
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n), S=O(n)
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if not prices: return 0
-        
-        profit = [[[0 for _ in range(2)] for _ in range(3)] for _ in range(len(prices))]
-        profit[0][0][0], profit[0][0][1] = 0, -prices[0]
-        profit[0][1][0], profit[0][1][1] = -inf, -inf
-        profit[0][2][0], profit[0][2][1] = -inf, -inf
-        
-        for i in range(1, len(prices)):
-            profit[i][0][0] = profit[i-1][0][0]
-            profit[i][0][1] = max(profit[i-1][0][1], profit[i-1][0][0] - prices[i])
-            
-            profit[i][1][0] = max(profit[i-1][1][0], profit[i-1][0][1] + prices[i])
-            profit[i][1][1] = max(profit[i-1][1][1], profit[i-1][1][0] - prices[i])
-            
-            profit[i][2][0] = max(profit[i-1][2][0], profit[i-1][1][1] + prices[i])
-            
-        end = len(prices) - 1
-        return max(profit[end][0][0], profit[end][1][0], profit[end][2][0])
+        n = len(prices)
+        dp = [[[0 for _ in range(2)] for _ in range(3)] for _ in range(n)]
+        dp[0][0][0], dp[0][0][1] = 0, -prices[0]
+        dp[0][1][0], dp[0][1][1] = -inf, -inf
+        dp[0][2][0], dp[0][2][1] = -inf, -inf
+        for i in range(1, n):
+            dp[i][0][0] = dp[i - 1][0][0]
+            dp[i][0][1] = max(dp[i - 1][0][1], dp[i - 1][0][0] - prices[i])
+            dp[i][1][0] = max(dp[i - 1][1][0], dp[i - 1][0][1] + prices[i])
+            dp[i][1][1] = max(dp[i - 1][1][1], dp[i - 1][1][0] - prices[i])
+            dp[i][2][0] = max(dp[i - 1][2][0], dp[i - 1][1][1] + prices[i])
+        return max(dp[n-1][0][0], dp[n-1][1][0], dp[n-1][2][0])
