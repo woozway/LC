15a1a7c6694df0905237e25c543e9a1ba8883305
chopchap <13806680065@163.com>
2021-02-01 10:54:57 +0800

    -

--- Dynamic Programming/121. Best Time to Buy and Sell Stock.py
+++ Dynamic Programming/121. Best Time to Buy and Sell Stock.py
@@ -25,11 +25,11 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if not prices: return 0
         res = 0
-        profit = [[0] * 3 for i in range(len(prices))]
-        profit[0][0], profit[0][1], profit[0][2] = 0, -prices[0], 0
+        dp = [[0] * 3 for i in range(len(prices))]
+        dp[0][0], dp[0][1], dp[0][2] = 0, -prices[0], 0
         for i in range(1, len(prices)):
-            profit[i][0] = profit[i - 1][0]
-            profit[i][1] = max(profit[i - 1][1], profit[i - 1][0] - prices[i])
-            profit[i][2] = profit[i - 1][1] + prices[i]
-            res = max(res, profit[i][0], profit[i][1], profit[i][2])
+            dp[i][0] = dp[i - 1][0]
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+            dp[i][2] = dp[i - 1][1] + prices[i]
+            res = max(res, dp[i][0], dp[i][1], dp[i][2])
         return res
