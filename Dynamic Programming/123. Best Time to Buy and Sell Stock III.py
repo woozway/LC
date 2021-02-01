4e93044df72e86170fd8aa1c5dd524266fce9841
chopchap <13806680065@163.com>
2021-02-01 15:59:17 +0800

    -

--- Dynamic Programming/123. Best Time to Buy and Sell Stock III.py
+++ Dynamic Programming/123. Best Time to Buy and Sell Stock III.py
@@ -19,11 +19,7 @@ class Solution:
             dp[0][j][0], dp[0][j][1] = 0, -prices[0]
         for i in range(1, n):
             for j in range(0, 2 + 1):
-                if j == 0:
-                    dp[i][j][0] = dp[i - 1][j][0]
-                    dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j][0] - prices[i])
-                    continue
-                dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j - 1][1] + prices[i])
+                dp[i][j][0] = dp[i - 1][j][0] if j == 0 else max(dp[i - 1][j][0], dp[i - 1][j - 1][1] + prices[i])
                 dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j][0] - prices[i])
         return max(dp[n - 1][0][0], dp[n - 1][1][0], dp[n - 1][2][0], dp[n - 1][2][1])
 
