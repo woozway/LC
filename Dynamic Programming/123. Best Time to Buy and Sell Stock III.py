d150fbe4b65f49be187a6c7f2372c7b4fe3039b9
chopchap <13806680065@163.com>
2021-02-01 12:12:06 +0800

    -

--- Dynamic Programming/123. Best Time to Buy and Sell Stock III.py
+++ Dynamic Programming/123. Best Time to Buy and Sell Stock III.py
@@ -1,7 +1,7 @@
 """
 1. Clarification
 2. Possible solutions
- - dp
+ - dp1 & dp2
 3. Coding
 4. Tests
 """
@@ -23,3 +23,17 @@ class Solution:
             dp[i][1][1] = max(dp[i - 1][1][1], dp[i - 1][1][0] - prices[i])
             dp[i][2][0] = max(dp[i - 1][2][0], dp[i - 1][1][1] + prices[i])
         return max(dp[n-1][0][0], dp[n-1][1][0], dp[n-1][2][0])
+
+
+# # T=O(n), S=O(1)
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         n = len(prices)
+#         buy1 = buy2 = -prices[0]
+#         sell1 = sell2 = 0
+#         for i in range(1, n):
+#             buy1 = max(buy1, -prices[i])
+#             sell1 = max(sell1, buy1 + prices[i])
+#             buy2 = max(buy2, sell1 - prices[i])
+#             sell2 = max(sell2, buy2 + prices[i])
+#         return sell2
