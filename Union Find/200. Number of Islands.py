deaedecb03fa84172637a75fac94ef0d53742c2e
chopchap <13806680065@163.com>
2021-02-02 17:09:29 +0800

    -

--- Union Find/200. Number of Islands.py
+++ Union Find/200. Number of Islands.py
@@ -11,7 +11,7 @@
 # # T=O(mn), S=O(mn), flood fill
 # dx = [-1, 1, 0, 0]
 # dy = [0, 0, -1, 1]
-#
+# 
 # class Solution:
 #     def numIslands(self, grid: List[List[str]]) -> int:
 #         if not grid or not grid[0]: return 0
@@ -19,16 +19,32 @@
 #         self.max_y = len(grid[0]);
 #         self.grid = grid
 #         self.visited = set()
-#         return sum([self.floodfill_dfs(i, j) for i in range(self.max_x) for j in range(self.max_y)])
-#
-#     def floodfill_dfs(self, x, y):
+#         # return sum([self.floodfill_dfs(i, j) for i in range(self.max_x) for j in range(self.max_y)])
+#         return sum([self.floodfill_bfs(i, j) for i in range(self.max_x) for j in range(self.max_y)])
+# 
+#     # def floodfill_dfs(self, x, y):
+#     #     if not self._is_valid(x, y):
+#     #         return 0
+#     #     self.visited.add((x, y))
+#     #     for k in range(4):
+#     #         self.floodfill_dfs(x + dx[k], y + dy[k])
+#     #     return 1
+# 
+#     def floodfill_bfs(self, x, y):
 #         if not self._is_valid(x, y):
 #             return 0
 #         self.visited.add((x, y))
-#         for k in range(4):
-#             self.floodfill_dfs(x + dx[k], y + dy[k])
+#         queue = collections.deque()
+#         queue.append((x, y))
+#         while queue:
+#             cur_x, cur_y = queue.popleft()
+#             for i in range(4):
+#                 new_x, new_y = cur_x + dx[i], cur_y + dy[i]
+#                 if self._is_valid(new_x, new_y):
+#                     self.visited.add((new_x, new_y))
+#                     queue.append((new_x, new_y))
 #         return 1
-#
+# 
 #     def _is_valid(self, x, y):
 #         if x < 0 or x >= self.max_x or y < 0 or y >= self.max_y:
 #             return False
