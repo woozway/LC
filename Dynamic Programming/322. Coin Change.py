1d6788e0f03d68a083a453e4cc3fc2eb7e1d2b07
chopchap <13806680065@163.com>
2021-02-02 12:25:25 +0800

    -

--- Dynamic Programming/322. Coin Change.py
+++ Dynamic Programming/322. Coin Change.py
@@ -1,3 +1,14 @@
+"""
+1. Clarification
+2. Possible solutions
+ - brute force, recursion
+ - dp
+3. Coding
+4. Tests
+"""
+
+
+# T=O(nk), S=O(k), k is len(coins), dp[i]: min coin num to reach i
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         Max = amount + 1
