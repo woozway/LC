7a1efa27b2d293bf043c0695c4b3369f2e53c90e
chopchap <13806680065@163.com>
2021-02-02 11:43:21 +0800

    -

--- Dynamic Programming/300. Longest Increasing Subsequence.py
+++ Dynamic Programming/300. Longest Increasing Subsequence.py
@@ -15,9 +15,13 @@ class Solution:
         if not nums: return 0
         n = len(nums)
         dp = [1] * n
+        res = 1
         for i in range(1, n):
-            dp[i] = 1 + max([dp[j] for j in range(i) if nums[j] < nums[i]] + [0])
-        return max(dp)
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+            res = max(res, dp[i])
+        return res
 
 
 # # T=O(nlgn), S=O(n), binary search
