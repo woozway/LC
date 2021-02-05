6b9669db8f87f3758e697f62cdec42190335c261
chopchap <13806680065@163.com>
2021-02-05 15:00:28 +0800

    -

--- Array/283. Move Zeroes.py
+++ Array/283. Move Zeroes.py
@@ -1,16 +1,20 @@
+"""
+1. Clarification
+2. Possible solutions
+ - two pointers
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n), S=O(1)
 class Solution:
-  def moveZeroes(self, nums: List[int]) -> None:
-    """
-    in-place implementation
-    invariant: num[0...i] as the non-zero part
-    """
-    n = len(nums)
-    i = -1
-    j = 0
-    while j < n:
-      if nums[j] != 0:
-        i += 1
-        nums[i] = nums[j]
-      j += 1
-    for k in range(i+1, n):
-      nums[k] = 0
+    def moveZeroes(self, nums: List[int]) -> None:
+        if not nums: return
+        i, j, n = 0, 0, len(nums)
+        for i in range(n):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+        for k in range(j, n):
+            nums[k] = 0
