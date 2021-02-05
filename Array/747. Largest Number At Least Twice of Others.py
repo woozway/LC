259b7e52155ce4a3c88fff5daddec0295b3d5a95
chopchap <13806680065@163.com>
2021-02-05 19:07:02 +0800

    -

--- Array/747. Largest Number At Least Twice of Others.py
+++ Array/747. Largest Number At Least Twice of Others.py
@@ -1,6 +1,17 @@
+"""
+1. Clarification
+2. Possible solutions
+ - linear scan
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n), S=O(1)
 class Solution:
-  def dominantIndex(self, nums: List[int]) -> int:
-    m = max(nums)
-    if all(m >= 2*x for x in nums if x != m):
-      return nums.index(m)
-    return -1
+    def dominantIndex(self, nums: List[int]) -> int:
+        if not nums: return -1
+        m = max(nums)
+        if all(m >= 2*x for x in nums if x != m):
+            return nums.index(m)
+        return -1
