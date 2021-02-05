2b9fba5163cfdb4bcb2aba5a74c68e0f16f956ad
chopchap <13806680065@163.com>
2021-02-05 16:00:25 +0800

    -

--- Array/448. Find All Numbers Disappeared in an Array.py
+++ Array/448. Find All Numbers Disappeared in an Array.py
@@ -1,15 +1,37 @@
+"""
+1. Clarification
+2. Possible solutions
+ - hashMap
+ - in-place
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n), S=O(n)
 class Solution:
-  def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-    """
-    T=O(n), S=O(1)
-    """
-    n = len(nums)
-    for i in range(n):
-      new_index = abs(nums[i]) - 1
-      if nums[new_index] > 0:
-        nums[new_index] *= -1
-    ans = []    
-    for i in range(1, n+1):
-      if nums[i-1] > 0:
-        ans.append(i)
-    return ans
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        if not nums: return []
+        hash_table = {}
+        for num in nums:
+            hash_table[num] = 1
+        result = []
+        for num in range(1, len(nums) + 1):
+            if num not in hash_table:
+                result.append(num)
+        return result
+
+
+# # T=O(n), S=O(1)
+# class Solution:
+#     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+#         if not nums: return []
+#         for i in range(len(nums)):
+#             new_index = abs(nums[i]) - 1
+#             if nums[new_index] > 0:
+#                 nums[new_index] *= -1
+#         result = []
+#         for i in range(1, len(nums) + 1):
+#             if nums[i - 1] > 0:
+#                 result.append(i)
+#         return result
