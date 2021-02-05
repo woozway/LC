9d9e12b7fbb03651507fca117fa2083471bf1a8f
chopchap <13806680065@163.com>
2021-02-05 15:35:59 +0800

    -

--- Array/1051. Height Checker.py
+++ Array/1051. Height Checker.py
@@ -1,3 +1,19 @@
+"""
+1. Clarification
+2. Possible solutions
+ - sort
+3. Coding
+4. Tests
+"""
+
+
+# T=O(nlgn), S=O(n)
 class Solution:
-  def heightChecker(self, heights: List[int]) -> int:
-    return sum(a != b for a, b in zip(heights, sorted(heights)))
+    def heightChecker(self, heights: List[int]) -> int:
+        if not heights: return 0
+        a = sorted(heights)
+        count, n = 0, len(heights)
+        for i in range(n):
+            if heights[i] != a[i]:
+                count += 1
+        return count
