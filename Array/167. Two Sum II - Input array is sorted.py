5fac37db48285d67580d5722aeadb3818f4e3a37
chopchap <13806680065@163.com>
2021-02-07 14:13:26 +0800

    -

--- Array/167. Two Sum II - Input array is sorted.py
+++ Array/167. Two Sum II - Input array is sorted.py
@@ -1,12 +1,43 @@
+"""
+1. Clarification
+2. Possible solutions
+ - binary search
+ - two pointers
+3. Coding
+4. Tests
+"""
+
+
+# # T=O(nlgn), S=O(1)
+# class Solution:
+#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#         n = len(numbers)
+#         if n < 2: return [-1, -1]
+#         for i in range(n):
+#             low, high = i + 1, n - 1
+#             while low <= high:
+#                 mid = (low + high) // 2
+#                 if numbers[mid] == target - numbers[i]:
+#                     return [i + 1, mid + 1]
+#                 elif numbers[mid] > target - numbers[i]:
+#                     high = mid - 1
+#                 else:
+#                     low = mid + 1
+#         return [-1, -1]
+
+
+# T=O(n), S=O(1)
 class Solution:
-  def twoSum(self, numbers: List[int], target: int) -> List[int]:
-    i = 0
-    j = len(numbers)-1
-    while i < j:
-      if numbers[i] + numbers[j] == target:
-        return [i+1, j+1]
-      elif numbers[i] + numbers[j] < target:
-        i += 1
-      else:
-        j -= 1
-    return []
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n = len(numbers)
+        if n < 2: return [-1, -1]
+        low, high = 0, n - 1
+        while low < high:
+            total = numbers[low] + numbers[high]
+            if total == target:
+                return [low + 1, high + 1]
+            elif total < target:
+                low += 1
+            else:
+                high -= 1
+        return [-1, -1]
