0f5d1ed4e0b03f3e894ca597d770243f97ef9064
chopchap <13806680065@163.com>
2021-02-07 13:23:57 +0800

    -

--- String/344. Reverse String.py
+++ String/344. Reverse String.py
@@ -1,7 +1,30 @@
+"""
+1. Clarification
+2. Possible solutions
+ - recursion
+ - two pointers
+3. Coding
+4. Tests
+"""
+
+
+# # T=O(n), S=O(n)
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         def helper(left, right):
+#             if left < right:
+#                 s[left], s[right] = s[right], s[left]
+#                 helper(left + 1, right - 1)
+# 
+#         if not s: return
+#         helper(0, len(s) - 1)
+
+
+# T=O(n), S=O(1)
 class Solution:
-  def reverseString(self, s: List[str]) -> None:
-    i, j = 0, len(s)-1
-    while i < j:
-      s[i], s[j] = s[j], s[i]
-      i += 1
-      j -= 1
+    def reverseString(self, s: List[str]) -> None:
+        if not s: return
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
