691711ecf4c786774463825f3772c8b73a60237f
chopchap <13806680065@163.com>
2021-02-08 11:17:02 +0800

    -

--- Binary Search/704. Binary Search.py
+++ Binary Search/704. Binary Search.py
@@ -1,4 +1,23 @@
+"""
+1. Clarification
+2. Possible solutions
+ - binary search
+3. Coding
+4. Tests
+"""
+
+
+# T=O(lgn), S=O(1)
 class Solution:
-  def search(self, nums: List[int], target: int) -> int:
-    idx = bisect_left(nums, target)
-    return idx if 0 <= idx < len(nums) and nums[idx] == target else -1
+    def search(self, nums: List[int], target: int) -> int:
+        if not nums: return -1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            pivot = left + (right - left) // 2
+            if nums[pivot] == target:
+                return pivot
+            elif target < nums[pivot]:
+                right = pivot - 1
+            else:
+                left = pivot + 1
+        return -1
