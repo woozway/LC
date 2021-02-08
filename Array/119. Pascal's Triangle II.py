3510f6572b66a5372cc618708b62304817f97108
chopchap <13806680065@163.com>
2021-02-08 09:36:56 +0800

    -

--- Array/119. Pascal's Triangle II.py
+++ Array/119. Pascal's Triangle II.py
@@ -1,7 +1,21 @@
+"""
+1. Clarification
+2. Possible solutions
+ - dynamic programming
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n^2), S=O(1)
 class Solution:
-  def getRow(self, rowIndex: int) -> List[int]:
-    ans = [[0]*(i+1) for i in range(rowIndex+1)]
-    for i in range(0, rowIndex+1):
-      for j in range(i+1):
-        ans[i][j] = 1 if j == 0 or j == i else ans[i-1][j-1] + ans[i-1][j]
-    return ans[rowIndex]
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex < 0: return []
+        triangle = []
+        for row_num in range(rowIndex + 1):
+            row = [None for _ in range(row_num + 1)]
+            row[0], row[-1] = 1, 1
+            for j in range(1, len(row) - 1):
+                row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
+            triangle.append(row)
+        return triangle[rowIndex]
