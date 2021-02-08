7f4a2978549c40cf790063930c61fa8c73b84172
chopchap <13806680065@163.com>
2021-02-08 22:53:33 +0800

    -

--- Binary Search/704. Binary Search.py
+++ Binary Search/704. Binary Search.py
@@ -2,6 +2,8 @@
 1. Clarification
 2. Possible solutions
  - binary search I
+ - binary search II
+ - binary search III
 3. Coding
 4. Tests
 """
@@ -21,3 +23,39 @@ class Solution:
             else:
                 left = pivot + 1
         return -1
+
+
+# # T=O(lgn), S=O(1)
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         if not nums: return -1
+#         left, right = 0, len(nums)
+#         while left < right:
+#             mid = (left + right) // 2
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[mid] < target:
+#                 left = mid + 1
+#             else:
+#                 right = mid
+#         if left != len(nums) and nums[left] == target:
+#             return left
+#         return -1
+
+
+# # T=O(lgn), S=O(1)
+# class Solution:
+#     def search(self, nums: List[int], target: int) -> int:
+#         if not nums: return -1
+#         left, right = 0, len(nums) - 1
+#         while left + 1 < right:
+#             mid = (left + right) // 2
+#             if nums[mid] == target:
+#                 return mid
+#             elif nums[mid] < target:
+#                 left = mid
+#             else:
+#                 right = mid
+#         if nums[left] == target: return left
+#         if nums[right] == target: return right
+#         return -1
