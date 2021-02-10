665fccf25053933d1672b9cd9f8175a71495ecb8
chopchap <13806680065@163.com>
2021-02-10 14:46:20 +0800

    -

--- Sort/350. Intersection of Two Arrays II.py
+++ Sort/350. Intersection of Two Arrays II.py
@@ -1,3 +1,14 @@
+"""
+1. Clarification
+2. Possible solutions
+ - built-in set intersection
+3. Coding
+4. Tests
+"""
+
+
+# T=O(m+n), S=O(m+n)
 class Solution:
-  def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-    return list((Counter(nums1) & Counter(nums2)).elements())
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if not nums1 or not nums2: return []
+        return (collections.Counter(nums1) & collections.Counter(nums2)).elements()
