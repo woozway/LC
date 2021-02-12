67f394b98933d4e6ca83d0fdd5097633278f71a5
chopchap <13806680065@163.com>
2021-02-12 09:50:12 +0800

    -

--- Depth-first Search/104. Maximum Depth of Binary Tree.py
+++ Depth-first Search/104. Maximum Depth of Binary Tree.py
@@ -7,6 +7,7 @@
 4. Tests
 """
 
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -20,6 +21,7 @@ class Solution:
         if not root: return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
+    
 # # T=O(n), S=O(n)
 # class Solution:
 #     def maxDepth(self, root: TreeNode) -> int:
