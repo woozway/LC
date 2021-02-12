3e797e09de79f16c2cf4ee49e81ddca863e66b89
chopchap <13806680065@163.com>
2021-02-12 11:43:48 +0800

    -

--- Tree/101. Symmetric Tree.py
+++ Tree/101. Symmetric Tree.py
@@ -1,8 +1,51 @@
-class Solution:
-  def isSymmetric(self, root: TreeNode) -> bool:
-    def check(p, q):
-      if not p and not q: return True
-      if not p or not q: return False
-      return p.val == q.val and check(p.left, q.right) and check(p.right, q.left)
+"""
+1. Clarification
+2. Possible solutions
+ - recursively
+ - iteratively
+3. Coding
+4. Tests
+"""
 
-    return check(root, root)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# T=O(n), S=O(n)
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def check(p, q):
+            if not p and not q: return True
+            if not p or not q: return False
+            return p.val == q.val and check(p.right, q.left) and check(p.left, q.right)
+
+        if not root: return True
+        return check(root, root)
+
+
+# # T=O(n), S=O(n)
+# class Solution:
+#     def isSymmetric(self, root: TreeNode) -> bool:
+#         def check(u, v):
+#             q = collections.deque()
+#             q.append(u)
+#             q.append(v)
+#             while q:
+#                 u = q.popleft()
+#                 v = q.popleft()
+#                 if not u and not v:
+#                     continue
+#                 if (not u or not v) or (u.val != v.val):
+#                     return False
+#                 q.append(u.left)
+#                 q.append(v.right)
+#                 q.append(u.right)
+#                 q.append(v.left)
+#             return True
+# 
+#         if not root: return True
+#         return check(root, root)
