4434b171e78c1033b99e41003d0574ea46662d83
chopchap <13806680065@163.com>
2021-02-13 20:07:34 +0800

    -

--- Heap/703. Kth Largest Element in a Stream.py
+++ Heap/703. Kth Largest Element in a Stream.py
@@ -7,6 +7,7 @@
 4. Tests
 """
 
+
 # # T=O(klgk) per operation
 # class KthLargest:
 # 
@@ -39,6 +40,7 @@ class KthLargest:
             heapq.heapreplace(self.mhp, val)
         return self.mhp[0]
 
+       
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
 # param_1 = obj.add(val)
