b5da9907e46d09ac92cd8f920d93f836f71e1df6
chopchap <13806680065@163.com>
2021-02-13 11:00:37 +0800

    -

--- Tree/98. Validate Binary Search Tree.py
+++ Tree/98. Validate Binary Search Tree.py
@@ -7,6 +7,7 @@
 4. Tests
 """
 
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -27,6 +28,7 @@ class Solution:
         self.prev = root
         return self.isValid(root.right)
 
+       
 # # T=O(n), S=O(n)
 # class Solution:
 #     def isValidBST(self, root: TreeNode) -> bool:
