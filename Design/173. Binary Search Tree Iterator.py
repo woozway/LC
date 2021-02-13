c6a0cc5dc018edf0f71251e809323abfaac5bff5
chopchap <13806680065@163.com>
2021-02-13 11:30:03 +0800

    -

--- Design/173. Binary Search Tree Iterator.py
+++ Design/173. Binary Search Tree Iterator.py
@@ -37,26 +37,26 @@ class BSTIterator:
         return self.index + 1 < len(self.nodes_sorted)
 
 
-# next: T=O(1), hasNext: T=O(1), S=O(h)
-class BSTIterator:
+# # next: T=O(1), hasNext: T=O(1), S=O(h)
+# class BSTIterator:
 
-    def __init__(self, root: TreeNode):
-        self.stack = []
-        self._leftmost_inorder(root)
+#     def __init__(self, root: TreeNode):
+#         self.stack = []
+#         self._leftmost_inorder(root)
 
-    def _leftmost_inorder(self, root):
-        while root:
-            self.stack.append(root)
-            root = root.left
+#     def _leftmost_inorder(self, root):
+#         while root:
+#             self.stack.append(root)
+#             root = root.left
 
-    def next(self) -> int:
-        topmost_node = self.stack.pop()
-        if topmost_node.right:
-            self._leftmost_inorder(topmost_node.right)
-        return topmost_node.val
+#     def next(self) -> int:
+#         topmost_node = self.stack.pop()
+#         if topmost_node.right:
+#             self._leftmost_inorder(topmost_node.right)
+#         return topmost_node.val
 
-    def hasNext(self) -> bool:
-        return len(self.stack) > 0
+#     def hasNext(self) -> bool:
+#         return len(self.stack) > 0
 
 
 # Your BSTIterator object will be instantiated and called as such:
