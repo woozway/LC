6ecfb2489245e2a52c2b9ef16d641d60f06193ea
chopchap <13806680065@163.com>
2021-02-18 18:28:53 +0800

    -

--- Stack/155. Min Stack.py
+++ Stack/155. Min Stack.py
@@ -1,18 +1,37 @@
+"""
+1. Clarification
+2. Possible solutions
+ - naive
+3. Coding
+4. Tests
+"""
+
+
+# T=O(1), S=O(n)
 class MinStack:
-  def __init__(self):
-    self.stack = []
-    self.min_stack = [math.inf]
 
-  def push(self, x: int) -> None:
-    self.stack.append(x)
-    self.min_stack.append(min(x, self.min_stack[-1]))
+    def __init__(self):
+        self.stack = []
+        self.min_stack = [math.inf]
 
-  def pop(self) -> None:
-    self.stack.pop()
-    self.min_stack.pop()
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        self.min_stack.append(min(x, self.min_stack[-1]))
 
-  def top(self) -> int:
-    return self.stack[-1]
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
 
-  def getMin(self) -> int:
-    return self.min_stack[-1]
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
