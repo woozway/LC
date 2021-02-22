97bc1fde0713d6b300418246633cd493d77281bf
chopchap <13806680065@163.com>
2021-02-22 18:18:53 +0800

    -

--- Linked List/19. Remove Nth Node From End of List.py
+++ Linked List/19. Remove Nth Node From End of List.py
@@ -1,14 +1,68 @@
+"""
+1. Clarification
+2. Possible solutions
+     - Two pass algorithm
+     - Stack
+     - Two pointers
+3. Coding
+4. Tests
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# # T=O(l), S=O(1)
+# class Solution:
+#     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+#         def getLength(head: ListNode) -> int:
+#             length = 0
+#             while head:
+#                 length += 1
+#                 head = head.next
+#             return length
+#
+#         if not head: return None
+#         dummy = ListNode(0, head)
+#         length = getLength(head)
+#         cur = dummy
+#         for i in range(1, length - n + 1):
+#             cur = cur.next
+#         cur.next = cur.next.next
+#         return dummy.next
+
+
+# # T=O(l), S=O(l)
+# class Solution:
+#     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+#         if not head: return None
+#         dummy = ListNode(0, head)
+#         stack = list()
+#         cur = dummy
+#         while cur:
+#             stack.append(cur)
+#             cur = cur.next
+#         for i in range(n):
+#             stack.pop()
+#         prev = stack[-1]
+#         prev.next = prev.next.next
+#         return dummy.next
+
+
+# T=O(l), S=O(1)
 class Solution:
-  def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-    """
-    two pointers, runner and chaser
-    singly-linked list traversal in one pass
-    """
-    dummy = ListNode(-1, head)
-    runner = chaser = dummy
-    for i in range(n):
-      runner = runner.next
-    while runner.next:
-      runner, chaser = runner.next, chaser.next
-    chaser.next = chaser.next.next
-    return dummy.next
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        if not head: return None
+        dummy = ListNode(0, head)
+        first = head
+        second = dummy
+        for i in range(n):
+            first = first.next
+        while first:
+            first = first.next
+            second = second.next
+        second.next = second.next.next
+        return dummy.next
