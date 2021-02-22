ed135389677c8019e348ee09f128a4fa14cc605a
chopchap <13806680065@163.com>
2021-02-22 19:37:48 +0800

    -

---
+++ Linked List/138. Copy List with Random Pointer.py
@@ -0,0 +1,84 @@
+"""
+1. Clarification
+2. Possible solutions
+     - backtrack
+     - iterative v1
+     - iterative v2
+3. Coding
+4. Tests
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+# T=O(n), S=O(n)
+class Solution:
+    def __init__(self):
+        self.visitedHash = {}
+
+    def copyRandomList(self, head: 'Node') -> 'Node':
+        if head == None: return head
+        if head in self.visitedHash: return self.visitedHash[head]
+        node = Node(head.val, None, None)
+        self.visitedHash[head] = node
+        node.next = self.copyRandomList(head.next)
+        node.random = self.copyRandomList(head.random)
+        return node
+
+
+# # T=O(n), S=O(n)
+# class Solution:
+#     def __init__(self):
+#         self.visited = {}
+#
+#     def getClonedNode(self, node):
+#         if node:
+#             if node in self.visited:
+#                 return self.visited[node]
+#             else:
+#                 self.visited[node] = Node(node.val, None, None)
+#                 return self.visited[node]
+#         return None
+#
+#     def copyRandomList(self, head: 'Node') -> 'Node':
+#         if not head: return head
+#         old_node = head
+#         new_node = Node(old_node.val, None, None)
+#         self.visited[old_node] = new_node
+#         while old_node != None:
+#             new_node.random = self.getClonedNode(old_node.random)
+#             new_node.next = self.getClonedNode(old_node.next)
+#             old_node = old_node.next
+#             new_node = new_node.next
+#         return self.visited[head]
+
+
+# # T=O(n), S=O(1)
+# class Solution:
+#     def copyRandomList(self, head: 'Node') -> 'Node':
+#         if not head: return head
+#         ptr = head
+#         while ptr:
+#             new_node = Node(ptr.val, None, None)
+#             new_node.next = ptr.next
+#             ptr.next = new_node
+#             ptr = new_node.next
+#         ptr = head
+#         while ptr:
+#             ptr.next.random = ptr.random.next if ptr.random else None
+#             ptr = ptr.next.next
+#         ptr_old_list = head
+#         ptr_new_list = head.next
+#         head_old = head.next
+#         while ptr_old_list:
+#             ptr_old_list.next = ptr_old_list.next.next
+#             ptr_new_list.next = ptr_new_list.next.next if ptr_new_list.next else None
+#             ptr_old_list = ptr_old_list.next
+#             ptr_new_list = ptr_new_list.next
+#         return head_old
