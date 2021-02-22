7aeb26afc34052aa8caac2968dd17a7dbbed42fb
chopchap <13806680065@163.com>
2021-02-22 18:25:39 +0800

    -

--- Linked List/203. Remove Linked List Elements.py
+++ Linked List/203. Remove Linked List Elements.py
@@ -1,11 +1,29 @@
+"""
+1. Clarification
+2. Possible solutions
+     - set sentinel
+3. Coding
+4. Tests
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# T=O(n), S=O(1)
 class Solution:
     def removeElements(self, head: ListNode, val: int) -> ListNode:
-        dummy = ListNode(None)
-        dummy.next = head
-        p = dummy
-        while p.next:
-            if p.next.val != val:
-                p = p.next
+        if not head: return None
+        sentinel = ListNode(0)
+        sentinel.next = head
+        prev, curr = sentinel, head
+        while curr:
+            if curr.val == val:
+                prev.next = curr.next
             else:
-                p.next = p.next.next
-        return dummy.next
+                prev = curr
+            curr = curr.next
+        return sentinel.next
