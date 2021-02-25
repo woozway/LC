58b629fc301ae8d8b0859731681599351bded91a
chopchap <13806680065@163.com>
2021-02-25 15:57:06 +0800

    -

---
+++ Backtracking/46. Permutations.py
@@ -0,0 +1,33 @@
+"""
+1. Clarification
+2. Possible solutions
+     - backtracking
+     - pythonic
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n*n!), S=O(n)
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(first=0):
+            if first == n:
+                res.append(nums[:])
+            for i in range(first, n):
+                nums[first], nums[i] = nums[i], nums[first]
+                backtrack(first + 1)
+                nums[first], nums[i] = nums[i], nums[first]
+
+        if not nums: return []
+        n = len(nums)
+        res = []
+        backtrack()
+        return res
+
+
+# # T=O(n*n!), S=O(n)
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         if not nums: return []
+#         return list(itertools.permutations(nums))
