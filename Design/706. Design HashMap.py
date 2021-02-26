53942dad1119d9a90b12706bdc14494477ae7b36
chopchap <13806680065@163.com>
2021-02-26 14:20:45 +0800

    -

--- Design/706. Design HashMap.py
+++ Design/706. Design HashMap.py
@@ -1,3 +1,13 @@
+"""
+1. Clarification
+2. Possible solutions
+     - mod + array
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n/k), S=O(k+m), k=len(buckets), m=# of already inserted number
 class Bucket:
     def __init__(self):
         self.bucket = []
@@ -24,20 +34,28 @@ class Bucket:
             if key == kv[0]:
                 del self.bucket[i]
 
-class MyHashMap:
+
+class MyHashMap(object):
 
     def __init__(self):
         self.key_space = 2069
         self.hash_table = [Bucket() for i in range(self.key_space)]
 
-    def put(self, key: int, value: int) -> None:
+    def put(self, key, value):
         hash_key = key % self.key_space
         self.hash_table[hash_key].update(key, value)
 
-    def get(self, key: int) -> int:
+    def get(self, key):
         hash_key = key % self.key_space
         return self.hash_table[hash_key].get(key)
 
-    def remove(self, key: int) -> None:
+    def remove(self, key):
         hash_key = key % self.key_space
         self.hash_table[hash_key].remove(key)
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
