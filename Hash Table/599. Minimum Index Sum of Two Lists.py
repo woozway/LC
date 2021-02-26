7c60b257c7ba43f90fee4e6ff6ed69d6fb27431b
chopchap <13806680065@163.com>
2021-02-26 15:05:17 +0800

    -

--- Hash Table/599. Minimum Index Sum of Two Lists.py
+++ Hash Table/599. Minimum Index Sum of Two Lists.py
@@ -1,3 +1,13 @@
+"""
+1. Clarification
+2. Possible solutions
+     - hashmap
+3. Coding
+4. Tests
+"""
+
+
+# T=O(l1*l2*x), S=O(l1*l2*x), x: avg len of strs
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
         index1 = {list1[i]: i for i in range(len(list1))}
