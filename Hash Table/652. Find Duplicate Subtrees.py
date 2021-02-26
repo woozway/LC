b564a7a12271cb184572a13d5a936fec74b395dc
chopchap <13806680065@163.com>
2021-02-26 15:31:20 +0800

    -

---
+++ Hash Table/652. Find Duplicate Subtrees.py
@@ -0,0 +1,51 @@
+"""
+1. Clarification
+2. Possible solutions
+     - dfs
+     - Unique Identifier or hashtable
+3. Coding
+4. Tests
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# # T=O(n^2), S=O(n^2)
+# class Solution:
+#     def findDuplicateSubtrees(self, root: TreeNode) -> List[TreeNode]:
+#         def collect(node):
+#             if not node: return "#"
+#             serial = "{},{},{}".format(node.val, collect(node.left), collect(node.right))
+#             count[serial] += 1
+#             if count[serial] == 2:
+#                 ans.append(node)
+#             return serial
+# 
+#         count = collections.Counter()
+#         ans = []
+#         collect(root)
+#         return ans
+
+
+# T=O(n), S=O(n)
+class Solution:
+    def findDuplicateSubtrees(self, root: TreeNode) -> List[TreeNode]:
+        def lookup(node):
+            if node:
+                uid = trees[node.val, lookup(node.left), lookup(node.right)]
+                count[uid] += 1
+                if count[uid] == 2:
+                    ans.append(node)
+                return uid
+
+        trees = collections.defaultdict()
+        trees.default_factory = trees.__len__
+        count = collections.Counter()
+        ans = []
+        lookup(root)
+        return ans
