2e1f3971d48abf13173a7145cf169a31b7b7b92a
chopchap <13806680065@163.com>
2021-02-26 15:00:58 +0800

    -

--- Hash Table/205. Isomorphic Strings.py
+++ Hash Table/205. Isomorphic Strings.py
@@ -1,7 +1,17 @@
+"""
+1. Clarification
+2. Possible solutions
+     - hash table
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n), S=O(len(alphabet))
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         if len(s) != len(t): return False
-        d = {}
+        d = dict()
         S = set()
         for i, x in enumerate(s):
             if d.get(x, 0) == 0:
