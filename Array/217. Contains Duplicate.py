bf4a2168f5a872ec9a1c57f29f009f2dd9671f21
chopchap <13806680065@163.com>
2021-02-26 14:31:39 +0800

    -

--- Array/217. Contains Duplicate.py
+++ Array/217. Contains Duplicate.py
@@ -1,3 +1,24 @@
+"""
+1. Clarification
+2. Possible solutions
+     - sort
+     - hashset
+3. Coding
+4. Tests
+"""
+
+
+# # T=O(nlgn), S=O(lgn)
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         nums.sort()
+#         for i in range(len(nums) - 1):
+#             if nums[i] == nums[i + 1]:
+#                 return True
+#         return False
+
+
+# T=O(n), S=O(n)
 class Solution:
-  def containsDuplicate(self, nums: List[int]) -> bool:
-    return len(set(nums)) < len(nums)
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
