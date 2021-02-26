1044a6835531eac6f58ad23e8c7d0d2bc5ecfb9f
chopchap <13806680065@163.com>
2021-02-26 15:38:01 +0800

    -

--- Hash Table/771. Jewels and Stones.py
+++ Hash Table/771. Jewels and Stones.py
@@ -1,3 +1,20 @@
+"""
+1. Clarification
+2. Possible solutions
+     - brute force
+     - hash
+3. Coding
+4. Tests
+"""
+
+
+# # T=O(mn), S=O(1)
+# class Solution:
+#     def numJewelsInStones(self, jewels: str, stones: str) -> int:
+#         return sum(s in jewels for s in stones)
+
+
+# T=O(m+n), S=O(m)
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         jewelsSet = set(jewels)
