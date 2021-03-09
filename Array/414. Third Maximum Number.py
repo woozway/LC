b1065cfe5cad00643acb7094219544b99a91ea8d
chopchap <13806680065@163.com>
2021-03-09 15:38:35 +0800

    -

--- Array/414. Third Maximum Number.py
+++ Array/414. Third Maximum Number.py
@@ -11,7 +11,7 @@
 # # T=O(nlgn), S=O(n)
 # class Solution:
 #     def thirdMax(self, nums: List[int]) -> int:
-#         if not nums: return -float('inf')
+#         if not nums: return -inf
 #         n = len(nums)
 #         distinct = set(nums)
 #         sz = len(distinct)
@@ -22,11 +22,11 @@
 # T=O(n), S=O(1)
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        if not nums: return -float('inf')
-        a = b = c = -float('inf')
+        if not nums: return -inf
+        a = b = c = -inf
         for n in nums:
             if n in (a, b, c): continue
             if n > a: n, a = a, n
             if n > b: n, b = b, n
             if n > c: n, c = c, n
-        return a if c == -float('inf') else c
+        return a if c == -inf else c
