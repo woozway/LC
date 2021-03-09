59ac8388bd282dcef85c93c26c7d5efe90d53eeb
chopchap <13806680065@163.com>
2021-03-09 14:53:30 +0800

    -

--- Array/941. Valid Mountain Array.py
+++ Array/941. Valid Mountain Array.py
@@ -10,7 +10,7 @@
 # T=O(n), S=O(1)
 class Solution:
     def validMountainArray(self, arr: List[int]) -> bool:
-        if not arr: return False
+        if len(arr) < 3: return False
         i, n = 0, len(arr)
         while i + 1 < n and arr[i] < arr[i + 1]:
             i += 1
