1c0dfc4c6273f2e66bc48ddb4c3b67469e746f6f
chopchap <13806680065@163.com>
2021-03-11 20:13:20 +0800

    -

--- Array/189. Rotate Array.py
+++ Array/189. Rotate Array.py
@@ -33,36 +33,36 @@
 #         nums[:] = a
 
 
-# # T=O(n), S=O(1)
-# class Solution:
-#     def rotate(self, nums: List[int], k: int) -> None:
-#         if not nums: return
-#         n = len(nums)
-#         k %= n
-#         start = count = 0
-#         while count < n:
-#             current, prev = start, nums[start]
-#             while True:
-#                 next_idx = (current + k) % n
-#                 nums[next_idx], prev = prev, nums[next_idx]
-#                 current = next_idx
-#                 count += 1
-#                 if start == current:
-#                     break
-#             start += 1
-
-
 # T=O(n), S=O(1)
 class Solution:
-    def reverse(self, nums: list, start: int, end: int) -> None:
-        while start < end:
-            nums[start], nums[end] = nums[end], nums[start]
-            start, end = start + 1, end - 1
-
     def rotate(self, nums: List[int], k: int) -> None:
         if not nums: return
         n = len(nums)
         k %= n
-        self.reverse(nums, 0, n - 1)
-        self.reverse(nums, 0, k - 1)
-        self.reverse(nums, k, n - 1)
+        start = count = 0
+        while count < n:
+            current, prev = start, nums[start]
+            while True:
+                next_idx = (current + k) % n
+                nums[next_idx], prev = prev, nums[next_idx]
+                current = next_idx
+                count += 1
+                if start == current:
+                    break
+            start += 1
+
+
+# # T=O(n), S=O(1)
+# class Solution:
+#     def reverse(self, nums: list, start: int, end: int) -> None:
+#         while start < end:
+#             nums[start], nums[end] = nums[end], nums[start]
+#             start, end = start + 1, end - 1
+
+#     def rotate(self, nums: List[int], k: int) -> None:
+#         if not nums: return
+#         n = len(nums)
+#         k %= n
+#         self.reverse(nums, 0, n - 1)
+#         self.reverse(nums, 0, k - 1)
+#         self.reverse(nums, k, n - 1)
