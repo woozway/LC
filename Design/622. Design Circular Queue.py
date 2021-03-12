b374f714f40caa8fcf55ec5f2040e8f4a1cc79fd
chopchap <13806680065@163.com>
2021-03-12 09:50:08 +0800

    -

--- Design/622. Design Circular Queue.py
+++ Design/622. Design Circular Queue.py
@@ -12,10 +12,18 @@
 class MyCircularQueue:
 
     def __init__(self, k: int):
-        self.queue = [0]*k
+        self.queue = [0] * k
         self.headIndex = 0
         self.count = 0
         self.capacity = k
+
+    def Front(self) -> int:
+        if self.count == 0: return -1
+        return self.queue[self.headIndex]
+
+    def Rear(self) -> int:
+        if self.count == 0: return -1
+        return self.queue[(self.headIndex + self.count - 1) % self.capacity]
 
     def enQueue(self, value: int) -> bool:
         if self.count == self.capacity: return False
@@ -28,14 +36,6 @@ class MyCircularQueue:
         self.headIndex = (self.headIndex + 1) % self.capacity
         self.count -= 1
         return True
-
-    def Front(self) -> int:
-        if self.count == 0: return -1
-        return self.queue[self.headIndex]
-
-    def Rear(self) -> int:
-        if self.count == 0: return -1
-        return self.queue[(self.headIndex + self.count - 1) % self.capacity]
 
     def isEmpty(self) -> bool:
         return self.count == 0
