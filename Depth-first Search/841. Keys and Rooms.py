2a87b2caa8425b70111df9f46988ce2e2ccf53a5
chopchap <13806680065@163.com>
2021-03-15 11:49:37 +0800

    -

--- Depth-first Search/841. Keys and Rooms.py
+++ Depth-first Search/841. Keys and Rooms.py
@@ -24,19 +24,19 @@ class Solution:
         return all(seen)
 
 
-# # T=O(n+e), S=O(n)
-# class Solution:
-#     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-#         if not rooms: return False
-#         n = len(rooms)
-#         num = 0
-#         vis = {0}
-#         que = collections.deque([0])
-#         while que:
-#             x = que.popleft()
-#             num += 1
-#             for it in rooms[x]:
-#                 if it not in vis:
-#                     vis.add(it)
-#                     que.append(it)
-#         return num == n
+# T=O(n+e), S=O(n)
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        if not rooms: return False
+        n = len(rooms)
+        num = 0
+        vis = {0}
+        que = collections.deque([0])
+        while que:
+            x = que.popleft()
+            num += 1
+            for it in rooms[x]:
+                if it not in vis:
+                    vis.add(it)
+                    que.append(it)
+        return num == n
