8084aad53911e09b90072200a9f80aa73f574f6d
chopchap <13806680065@163.com>
2021-03-15 10:03:01 +0800

    -

--- Stack/394. Decode String.py
+++ Stack/394. Decode String.py
@@ -31,26 +31,26 @@ class Solution:
         return curStr
 
 
-# # T=O(n), S=O(n)
-# class Solution:
-#     def decodeString(self, s: str) -> str:
-#         if not s or len(s) == 0: return s
-#         result, idx = self.dfs(0, s, 0, '')
-#         return result
+# T=O(n), S=O(n)
+class Solution:
+    def decodeString(self, s: str) -> str:
+        if not s or len(s) == 0: return s
+        result, idx = self.dfs(0, s, 0, '')
+        return result
 
-#     def dfs(self, idx, s, prev_num, prev_str):
-#         while idx < len(s):
-#             while s[idx].isdigit():
-#                 prev_num = prev_num * 10 + int(s[idx])
-#                 idx += 1
-#             if s[idx] == '[':
-#                 returned_str, ending_pos = self.dfs(idx + 1, s, 0, '')
-#                 prev_str += returned_str * prev_num
-#                 idx = ending_pos
-#                 prev_num = 0
-#             elif s[idx] == ']':
-#                 return prev_str, idx
-#             else:
-#                 prev_str += s[idx]
-#             idx += 1
-#         return prev_str, idx
+    def dfs(self, idx, s, prev_num, prev_str):
+        while idx < len(s):
+            while s[idx].isdigit():
+                prev_num = prev_num * 10 + int(s[idx])
+                idx += 1
+            if s[idx] == '[':
+                returned_str, ending_pos = self.dfs(idx + 1, s, 0, '')
+                prev_str += returned_str * prev_num
+                idx = ending_pos
+                prev_num = 0
+            elif s[idx] == ']':
+                return prev_str, idx
+            else:
+                prev_str += s[idx]
+            idx += 1
+        return prev_str, idx
