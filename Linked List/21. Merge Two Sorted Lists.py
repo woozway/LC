5bcf38613783fc529a4322941cd6accc761c9899
chopchap <13806680065@163.com>
2021-03-17 19:49:48 +0800

    -

--- Linked List/21. Merge Two Sorted Lists.py
+++ Linked List/21. Merge Two Sorted Lists.py
@@ -14,17 +14,17 @@
 #         self.val = val
 #         self.next = next
 
-# # T=O(n+m), S=O(n+m)
-# class Solution:
-#     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-#         if not l1: return l2
-#         if not l2: return l1
-#         if l1.val < l2.val:
-#             l1.next = self.mergeTwoLists(l1.next, l2)
-#             return l1
-#         else:
-#             l2.next = self.mergeTwoLists(l1, l2.next)
-#             return l2
+# T=O(n+m), S=O(n+m)
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1: return l2
+        if not l2: return l1
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
 
 
 # T=O(n+m), S=O(1)
