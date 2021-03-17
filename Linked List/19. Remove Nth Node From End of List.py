0686eec963781c10b41edaf423778f8cf543b06b
chopchap <13806680065@163.com>
2021-03-17 19:26:42 +0800

    -

--- Linked List/19. Remove Nth Node From End of List.py
+++ Linked List/19. Remove Nth Node From End of List.py
@@ -15,41 +15,41 @@
 #         self.val = val
 #         self.next = next
 
-# # T=O(l), S=O(1)
-# class Solution:
-#     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-#         def getLength(head: ListNode) -> int:
-#             length = 0
-#             while head:
-#                 length += 1
-#                 head = head.next
-#             return length
-#
-#         if not head: return None
-#         dummy = ListNode(0, head)
-#         length = getLength(head)
-#         cur = dummy
-#         for i in range(1, length - n + 1):
-#             cur = cur.next
-#         cur.next = cur.next.next
-#         return dummy.next
+# T=O(l), S=O(1)
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        def getLength(head: ListNode) -> int:
+            length = 0
+            while head:
+                length += 1
+                head = head.next
+            return length
+
+        if not head: return None
+        dummy = ListNode(0, head)
+        length = getLength(head)
+        cur = dummy
+        for i in range(1, length - n + 1):
+            cur = cur.next
+        cur.next = cur.next.next
+        return dummy.next
 
 
-# # T=O(l), S=O(l)
-# class Solution:
-#     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-#         if not head: return None
-#         dummy = ListNode(0, head)
-#         stack = list()
-#         cur = dummy
-#         while cur:
-#             stack.append(cur)
-#             cur = cur.next
-#         for i in range(n):
-#             stack.pop()
-#         prev = stack[-1]
-#         prev.next = prev.next.next
-#         return dummy.next
+# T=O(l), S=O(l)
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        if not head: return None
+        dummy = ListNode(0, head)
+        stack = list()
+        cur = dummy
+        while cur:
+            stack.append(cur)
+            cur = cur.next
+        for i in range(n):
+            stack.pop()
+        prev = stack[-1]
+        prev.next = prev.next.next
+        return dummy.next
 
 
 # T=O(l), S=O(1)
@@ -62,7 +62,6 @@ class Solution:
         for i in range(n):
             first = first.next
         while first:
-            first = first.next
-            second = second.next
+            first, second = first.next, second.next
         second.next = second.next.next
         return dummy.next
