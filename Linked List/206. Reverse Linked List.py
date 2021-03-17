3c7913850c77a508a91d7bf82951a66636f3c5b9
chopchap <13806680065@163.com>
2021-03-17 19:44:54 +0800

    -

--- Linked List/206. Reverse Linked List.py
+++ Linked List/206. Reverse Linked List.py
@@ -1,8 +1,8 @@
 """
 1. Clarification
 2. Possible solutions
- - iteratively
- - recursively
+     - Iteratively
+     - Recursively
 3. Coding
 4. Tests
 """
@@ -14,13 +14,13 @@
 #         self.val = val
 #         self.next = next
 
-# # T=O(n), S=O(1)
-# class Solution:
-#     def reverseList(self, head: ListNode) -> ListNode:
-#         cur, prev = head, None
-#         while cur:
-#             cur.next, prev, cur = prev, cur, cur.next
-#         return prev
+# T=O(n), S=O(1)
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        cur, prev = head, None
+        while cur:
+            cur.next, prev, cur = prev, cur, cur.next
+        return prev
 
 
 # T=O(n), S=O(n)
