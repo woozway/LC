c20a09317e9736a8503d6f012767403371cf57b7
chopchap <13806680065@163.com>
2021-03-18 19:57:18 +0800

    -

--- Linked List/328. Odd Even Linked List.py
+++ Linked List/328. Odd Even Linked List.py
@@ -16,13 +16,22 @@
 # T=O(n), S=O(1)
 class Solution:
     def oddEvenList(self, head: ListNode) -> ListNode:
-        if not head: return head
-        evenHead = head.next
-        odd, even = head, evenHead
-        while even and even.next:
+        if not head: return None
+        odd, even = ListNode(0), ListNode(0)
+        prevodd, preveven = odd, even
+        cur, cnt = head, 1
+        while cur:
+            if cnt % 2 == 1:
+                prevodd.next = cur
+                prevodd = cur
+            else:
+                preveven.next = cur
+                preveven = cur
+            cur = cur.next
+            cnt += 1
+        prevodd.next, preveven.next = None, None
+        if odd.next is None:
             odd.next = even.next
-            odd = odd.next
-            even.next = odd.next
-            even = even.next
-        odd.next = evenHead
-        return head
+        else:
+            prevodd.next = even.next
+        return odd.next
