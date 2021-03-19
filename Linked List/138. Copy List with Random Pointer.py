3fe4fedb028c7e61802b4e73b78072ae9c228230
chopchap <13806680065@163.com>
2021-03-19 13:34:54 +0800

    -

--- Linked List/138. Copy List with Random Pointer.py
+++ Linked List/138. Copy List with Random Pointer.py
@@ -55,26 +55,26 @@ class Solution:
         return self.visited[head]
 
 
-# T=O(n), S=O(1)
-class Solution:
-    def copyRandomList(self, head: 'Node') -> 'Node':
-        if not head: return None
-        ptr = head
-        while ptr:
-            new_node = Node(ptr.val)
-            new_node.next = ptr.next
-            ptr.next = new_node
-            ptr = new_node.next
-        ptr = head
-        while ptr:
-            ptr.next.random = ptr.random.next if ptr.random else None
-            ptr = ptr.next.next
-        ptr_old_list = head
-        ptr_new_list = head.next
-        head_old = head.next
-        while ptr_old_list:
-            ptr_old_list.next = ptr_old_list.next.next
-            ptr_new_list.next = ptr_new_list.next.next if ptr_new_list.next else None
-            ptr_old_list = ptr_old_list.next
-            ptr_new_list = ptr_new_list.next
-        return head_old
+# # T=O(n), S=O(1)
+# class Solution:
+#     def copyRandomList(self, head: 'Node') -> 'Node':
+#         if not head: return None
+#         ptr = head
+#         while ptr:
+#             new_node = Node(ptr.val)
+#             new_node.next = ptr.next
+#             ptr.next = new_node
+#             ptr = new_node.next
+#         ptr = head
+#         while ptr:
+#             ptr.next.random = ptr.random.next if ptr.random else None
+#             ptr = ptr.next.next
+#         ptr_old_list = head
+#         ptr_new_list = head.next
+#         head_old = head.next
+#         while ptr_old_list:
+#             ptr_old_list.next = ptr_old_list.next.next
+#             ptr_new_list.next = ptr_new_list.next.next if ptr_new_list.next else None
+#             ptr_old_list = ptr_old_list.next
+#             ptr_new_list = ptr_new_list.next
+#         return head_old
