59729136cc2d5bc54437fa5b00a6277c7bb62a79
chopchap <13806680065@163.com>
2021-03-20 10:43:29 +0800

    -

--- Hash Table/202. Happy Number.py
+++ Hash Table/202. Happy Number.py
@@ -53,7 +53,7 @@ class Solution:
 #                 number, digit = divmod(number, 10)
 #                 total_sum += digit ** 2
 #             return total_sum
-#
+
 #         cycle_members = {4, 16, 37, 58, 89, 145, 42, 20}
 #         while n != 1 and n not in cycle_members:
 #             n = get_next(n)
