75162ab9ed0fd2aa8a6cd3183075b17b665bcf43
chopchap <13806680065@163.com>
2021-03-20 14:13:54 +0800

    -

--- Hash Table/350. Intersection of Two Arrays II.py
+++ Hash Table/350. Intersection of Two Arrays II.py
@@ -24,11 +24,9 @@ class Solution:
         m = collections.Counter(nums1)
         intersection = list()
         for num in nums2:
-            if (count := m.get(num, 0)) > 0:
+            if m.get(num, 0) > 0:
                 intersection.append(num)
                 m[num] -= 1
-                if m[num] == 0:
-                    m.pop(num)
         return intersection
 
 
@@ -36,12 +34,11 @@ class Solution:
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         if not nums1 or not nums2: return []
-        nums1.sort()
-        nums2.sort()
-        length1, length2 = len(nums1), len(nums2)
+        nums1.sort(); nums2.sort()
+        m, n = len(nums1), len(nums2)
         intersection = list()
         index1 = index2 = 0
-        while index1 < length1 and index2 < length2:
+        while index1 < m and index2 < n:
             if nums1[index1] < nums2[index2]:
                 index1 += 1
             elif nums1[index1] > nums2[index2]:
