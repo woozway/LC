974d271ff0ea24f42f852fa4f54b035571cb2c85
chopchap <13806680065@163.com>
2021-03-20 15:15:30 +0800

    -

--- Backtracking/36. Valid Sudoku.py
+++ Backtracking/36. Valid Sudoku.py
@@ -33,18 +33,18 @@ class Solution:
 # T=O(1), S=O(1)
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [{} for _ in range(9)]
-        cols = [{} for _ in range(9)]
-        boxes = [{} for _ in range(9)]
+        rows = [collections.Counter() for _ in range(9)]
+        cols = [collections.Counter() for _ in range(9)]
+        boxes = [collections.Counter() for _ in range(9)]
         for i in range(9):
             for j in range(9):
                 num = board[i][j]
                 if num != '.':
                     num = int(num)
                     box_index = (i // 3 ) * 3 + j // 3
+                    if rows[i][num] or cols[j][num] or boxes[box_index][num]:
+                        return False
                     rows[i][num] = rows[i].get(num, 0) + 1
                     cols[j][num] = cols[j].get(num, 0) + 1
                     boxes[box_index][num] = boxes[box_index].get(num, 0) + 1
-                    if rows[i][num] > 1 or cols[j][num] > 1 or boxes[box_index][num] > 1:
-                        return False         
         return True
