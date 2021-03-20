33b85004419a0573a95b5a82d8f43626435dc744
chopchap <13806680065@163.com>
2021-03-20 10:37:39 +0800

    -

--- Hash Table/349. Intersection of Two Arrays.py
+++ Hash Table/349. Intersection of Two Arrays.py
@@ -1,8 +1,8 @@
 """
 1. Clarification
 2. Possible solutions
- - built-in set intersection
- - set
+     - Python built-in set intersection
+     - Set
 3. Coding
 4. Tests
 """
@@ -16,16 +16,16 @@ class Solution:
         return set(nums1) & set(nums2)
 
 
-# # T=O(m+n), S=O(m+n)
-# class Solution:
-#     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-#         if not nums1 or not nums2: return []
-#         set1 = set(nums1)
-#         set2 = set(nums2)
-#         if len(set1) < len(set2):
-#             return self.set_intersection(set1, set2)
-#         else:
-#             return self.set_intersection(set2, set1)
-#
-#     def set_intersection(self, set1, set2):
-#         return [x for x in set1 if x in set2]
+# T=O(m+n), S=O(m+n)
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if not nums1 or not nums2: return []
+        set1 = set(nums1)
+        set2 = set(nums2)
+        if len(set1) < len(set2):
+            return self.set_intersection(set1, set2)
+        else:
+            return self.set_intersection(set2, set1)
+
+    def set_intersection(self, set1, set2):
+        return [x for x in set1 if x in set2]
