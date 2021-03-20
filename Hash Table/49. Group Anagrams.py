5c9f947cb38523a57f1a8043ac3562dd47b88c6f
chopchap <13806680065@163.com>
2021-03-20 14:53:18 +0800

    -

--- Hash Table/49. Group Anagrams.py
+++ Hash Table/49. Group Anagrams.py
@@ -8,7 +8,7 @@
 """
 
 
-# T=O(nklgk), S=O(nk)
+# T=O(nklgk), S=O(nk), k=avg length of strs
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ans = collections.defaultdict(list)
