632e1ba6ed2686126f8098450ed5e0177489ec4b
chopchap <13806680065@163.com>
2021-03-20 10:45:08 +0800

    -

--- Hash Table/202. Happy Number.py
+++ Hash Table/202. Happy Number.py
@@ -26,22 +26,22 @@ class Solution:
         return n == 1
 
 
-# # T=O(lgn), S=O(1), Floyd's cycle detection algo
-# class Solution:
-#     def isHappy(self, n: int) -> bool:
-#         def get_next(number):
-#             total_sum = 0
-#             while number > 0:
-#                 number, digit = divmod(number, 10)
-#                 total_sum += digit ** 2
-#             return total_sum
+# T=O(lgn), S=O(1), Floyd's cycle detection algo
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def get_next(number):
+            total_sum = 0
+            while number > 0:
+                number, digit = divmod(number, 10)
+                total_sum += digit ** 2
+            return total_sum
 
-#         slow = n
-#         fast = get_next(n)
-#         while fast != 1 and slow != fast:
-#             slow = get_next(slow)
-#             fast = get_next(get_next(fast))
-#         return fast == 1
+        slow = n
+        fast = get_next(n)
+        while fast != 1 and slow != fast:
+            slow = get_next(slow)
+            fast = get_next(get_next(fast))
+        return fast == 1
 
 
 # # T=O(lgn), S=O(1)
