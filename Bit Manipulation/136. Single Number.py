68ed3fd983deb03fb4498c93f7d021990a298a99
chopchap <13806680065@163.com>
2021-03-20 10:30:03 +0800

    -

--- Bit Manipulation/136. Single Number.py
+++ Bit Manipulation/136. Single Number.py
@@ -1,9 +1,9 @@
 """
 1. Clarification
 2. Possible solutions
-     - list operation
-     - hash table
-     - maths
+     - List operation
+     - Hash table
+     - Maths
      - Bit Manipulation
 3. Coding
 4. Tests
@@ -22,21 +22,21 @@
 #         return no_duplicate_list.pop()
 
 
-# # T=O(n), S=O(n)
-# class Solution:
-#     def singleNumber(self, nums: List[int]) -> int:
-#         hash_table = collections.defaultdict(int)
-#         for i in nums:
-#             hash_table[i] += 1
-#         for i in hash_table:
-#             if hash_table[i] == 1:
-#                 return i
+# T=O(n), S=O(n)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash_table = collections.defaultdict(int)
+        for i in nums:
+            hash_table[i] += 1
+        for i in hash_table:
+            if hash_table[i] == 1:
+                return i
 
 
-# # T=O(n), S=O(n)
-# class Solution:
-#     def singleNumber(self, nums: List[int]) -> int:
-#         return 2 * sum(set(nums)) - sum(nums)
+# T=O(n), S=O(n)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return 2 * sum(set(nums)) - sum(nums)
 
 
 # T=O(n), S=O(1)
