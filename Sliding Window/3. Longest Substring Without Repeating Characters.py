7f215c56b65fdfea13122698213cd331908e491f
chopchap <13806680065@163.com>
2021-03-21 11:52:12 +0800

    -

--- Sliding Window/3. Longest Substring Without Repeating Characters.py
+++ Sliding Window/3. Longest Substring Without Repeating Characters.py
@@ -31,14 +31,11 @@ class Solution:
 # T=O(n), S=O(len(alphabet))
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        occ = set()
-        n = len(s)
-        rk, ans = -1, 0
-        for i in range(n):
-            if i != 0:
-                occ.remove(s[i - 1])
-            while rk + 1 < n and s[rk + 1] not in occ:
-                occ.add(s[rk + 1])
-                rk += 1
-            ans = max(ans, rk - i + 1)
+        hashMap = {}
+        window_left, ans = 0, 0
+        for i, c in enumerate(s):
+            if c in hashMap:
+                window_left = max(hashMap[c], window_left)
+            ans = max(ans, i - window_left + 1)
+            hashMap[c] = i + 1
         return ans
