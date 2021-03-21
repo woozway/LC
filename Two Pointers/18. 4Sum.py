7f4af28db8ba06d780675a62dd8dacba5f085a1c
chopchap <13806680065@163.com>
2021-03-21 13:54:07 +0800

    -

--- Two Pointers/18. 4Sum.py
+++ Two Pointers/18. 4Sum.py
@@ -15,9 +15,9 @@
 #         nums.sort()
 #         res = set()
 #         for i in range(n):
-#             for j in range(i+1, n):
-#                 for k in range(j+1, n):
-#                     for l in range(k+1, n):
+#             for j in range(i + 1, n):
+#                 for k in range(j + 1, n):
+#                     for l in range(k + 1, n):
 #                         if nums[i] + nums[j] + nums[k] + nums[l] == target:
 #                             res.add((nums[i], nums[j], nums[k], nums[l]))
 #         return res
