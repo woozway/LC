7b5ec58ae7a27c3ad9a32c67899598f9f627e5b4
chopchap <13806680065@163.com>
2021-03-21 10:01:58 +0800

    -

--- Hash Table/652. Find Duplicate Subtrees.py
+++ Hash Table/652. Find Duplicate Subtrees.py
@@ -19,8 +19,8 @@
 class Solution:
     def findDuplicateSubtrees(self, root: TreeNode) -> List[TreeNode]:
         def collect(node):
-            if not node: return "#"
-            serial = "{},{},{}".format(node.val, collect(node.left), collect(node.right))
+            if not node: return '#'
+            serial = "{} {} {}".format(node.val, collect(node.left), collect(node.right))
             count[serial] += 1
             if count[serial] == 2:
                 ans.append(node)
