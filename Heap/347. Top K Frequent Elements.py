2fd2d2b0c52e16d5eaf1ed11e76cf74f007393a7
chopchap <13806680065@163.com>
2021-03-21 14:45:25 +0800

    -

--- Heap/347. Top K Frequent Elements.py
+++ Heap/347. Top K Frequent Elements.py
@@ -2,6 +2,7 @@
 1. Clarification
 2. Possible solutions
     - Heap
+    - Bucket Sort
     - Quickselect (Hoare's selection algorithm)
 3. Coding
 4. Tests
@@ -15,6 +16,17 @@ class Solution:
         if k == len(nums): return nums
         count = collections.Counter(nums)
         return heapq.nlargest(k, count.keys(), key=count.get)
+
+
+# T=O(n), S=O(n)
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        if not nums or k < 1: return []
+        bucket = [[] for _ in range(len(nums) + 1)]
+        count = collections.Counter(nums).items()  
+        for num, freq in count: bucket[freq].append(num) 
+        flat_list = list(itertools.chain(*bucket))
+        return flat_list[::-1][:k]
 
 
 # T=O(n), S=O(n)
