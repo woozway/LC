c4e1a7aec7184f4f5f1f244272c0ce0dfb3dffd8
chopchap <13806680065@163.com>
2021-03-22 11:27:23 +0800

    -

--- Breadth-first Search/102. Binary Tree Level Order Traversal.py
+++ Breadth-first Search/102. Binary Tree Level Order Traversal.py
@@ -45,8 +45,7 @@ class Solution:
 
     def _dfs(self, node, level):
         if not node: return
-        if len(self.result) < level + 1:
-            self.result.append([])
+        if len(self.result) < level + 1: self.result.append([])
         self.result[level].append(node.val)
         self._dfs(node.left, level + 1)
         self._dfs(node.right, level + 1)
