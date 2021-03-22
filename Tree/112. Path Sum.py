150d1ee065b6ff351b1a1ead750ba72e84575904
chopchap <13806680065@163.com>
2021-03-22 14:20:41 +0800

    -

--- Tree/112. Path Sum.py
+++ Tree/112. Path Sum.py
@@ -27,19 +27,15 @@ class Solution:
 class Solution:
     def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
         if not root: return False
-        Q_node = collections.deque([root])
-        Q_val = collections.deque([root.val])
-        while Q_node:
-            node = Q_node.popleft()
-            Sum = Q_val.popleft()
+        Q = collections.deque([(root, root.val)])
+        while Q:
+            node, Sum = Q.popleft()
             if not node.left and not node.right:
                 if Sum == targetSum:
                     return True
                 continue
             if node.left:
-                Q_node.append(node.left)
-                Q_val.append(node.left.val + Sum)
+                Q.append((node.left, node.left.val + Sum))
             if node.right:
-                Q_node.append(node.right)
-                Q_val.append(node.right.val + Sum)
+                Q.append((node.right, node.right.val + Sum))
         return False
