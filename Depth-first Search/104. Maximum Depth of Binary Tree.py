9fedcfe5fc26bfe0e360fad7a3131d555d04594c
chopchap <13806680065@163.com>
2021-03-22 13:38:39 +0800

    -

--- Depth-first Search/104. Maximum Depth of Binary Tree.py
+++ Depth-first Search/104. Maximum Depth of Binary Tree.py
@@ -1,9 +1,9 @@
 """
 1. Clarification
 2. Possible solutions
- - dfs, bottom-up
- - dfs, top-down
- - bfs
+    - dfs, bottom-up
+    - dfs, top-down
+    - bfs
 3. Coding
 4. Tests
 """
@@ -23,35 +23,35 @@ class Solution:
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
 
-# # T=O(n), S=O(n)
-# class Solution:
-#     def maxDepth(self, root: TreeNode) -> int:
-#         if not root: return 0
-#         self.MaxDep = 0
-#         self.dfs(root, 1)
-#         return self.MaxDep
-#
-#     def dfs(self, root, depth):
-#         if not root: return
-#         if not root.left and not root.right:
-#             self.MaxDep = max(self.MaxDep, depth)
-#             return
-#         self.dfs(root.left, depth + 1)
-#         self.dfs(root.right, depth + 1)
+# T=O(n), S=O(n)
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root: return 0
+        self.MaxDep = 0
+        self.dfs(root, 1)
+        return self.MaxDep
+
+    def dfs(self, root, depth):
+        if not root: return
+        if not root.left and not root.right:
+            self.MaxDep = max(self.MaxDep, depth)
+            return
+        self.dfs(root.left, depth + 1)
+        self.dfs(root.right, depth + 1)
 
 
-# # T=O(n), S=O(n)
-# class Solution:
-#     def maxDepth(self, root: TreeNode) -> int:
-#         if not root: return 0
-#         queue = collections.deque()
-#         queue.append(root)
-#         ret = 0
-#         while queue:
-#             level_size = len(queue)
-#             for _ in range(level_size):
-#                 node = queue.popleft()
-#                 if node.left: queue.append(node.left)
-#                 if node.right: queue.append(node.right)
-#             ret += 1
-#         return ret
+# T=O(n), S=O(n)
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root: return 0
+        Q = collections.deque()
+        Q.append(root)
+        ret = 0
+        while Q:
+            level_size = len(Q)
+            for _ in range(level_size):
+                node = Q.popleft()
+                if node.left: Q.append(node.left)
+                if node.right: Q.append(node.right)
+            ret += 1
+        return ret
