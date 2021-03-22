55e342a9aee5ddf43fb03dd4564b7b8bc788695a
chopchap <13806680065@163.com>
2021-03-22 15:30:54 +0800

    -

--- Breadth-first Search/116. Populating Next Right Pointers in Each Node.py
+++ Breadth-first Search/116. Populating Next Right Pointers in Each Node.py
@@ -36,17 +36,17 @@ class Solution:
         return root
 
 
-# # T=O(n), S=O(1)
-# class Solution:
-#     def connect(self, root: 'Node') -> 'Node':
-#         if not root: return root
-#         leftmost = root
-#         while leftmost.left:
-#             head = leftmost
-#             while head:
-#                 head.left.next = head.right
-#                 if head.next:
-#                     head.right.next = head.next.left
-#                 head = head.next
-#             leftmost = leftmost.left
-#         return root
+# T=O(n), S=O(1)
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root: return root
+        leftmost = root
+        while leftmost.left:
+            head = leftmost
+            while head:
+                head.left.next = head.right
+                if head.next:
+                    head.right.next = head.next.left
+                head = head.next
+            leftmost = leftmost.left
+        return root
