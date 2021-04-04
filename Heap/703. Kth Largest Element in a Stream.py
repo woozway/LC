e03a7d8f717dcf3b35de56443adc7e5d55419318
chopchap <13806680065@163.com>
2021-04-04 10:44:35 +0800

    -

--- Heap/703. Kth Largest Element in a Stream.py
+++ Heap/703. Kth Largest Element in a Stream.py
@@ -1,27 +1,27 @@
 """
 1. Clarification
 2. Possible solutions
- - keep max k elements by sorting
- - use MinHeap of size k
+    - Keep max k elements by sorting
+    - Use MinHeap of size k
 3. Coding
 4. Tests
 """
 
 
-# # T=O(klgk) per operation
-# class KthLargest:
-# 
-#     def __init__(self, k: int, nums: List[int]):
-#         self.k = k
-#         self.arr = sorted(nums, reverse=True)[:k]
-# 
-#     def add(self, val: int) -> int:
-#         if len(self.arr) < self.k:
-#             self.arr.append(val)
-#         elif val > self.arr[-1]:
-#             self.arr[-1] = val
-#         self.arr.sort(reverse=True)
-#         return self.arr[-1]
+# T=O(klgk) per operation
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.arr = sorted(nums, reverse=True)[:k]
+
+    def add(self, val: int) -> int:
+        if len(self.arr) < self.k:
+            self.arr.append(val)
+        elif val > self.arr[-1]:
+            self.arr[-1] = val
+        self.arr.sort(reverse=True)
+        return self.arr[-1]
 
 
 # T=O(lgk) per operation
