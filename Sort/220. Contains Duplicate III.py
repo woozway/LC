acce11bb605ac14c1f02be5c929532dc36bdd269
chopchap <13806680065@163.com>
2021-04-04 11:21:28 +0800

    -

--- Sort/220. Contains Duplicate III.py
+++ Sort/220. Contains Duplicate III.py
@@ -1,9 +1,9 @@
 """
 1. Clarification
 2. Possible solutions
- - brute force
- - binary search tree
- - bucket sort
+    - Brute force
+    - Binary search tree
+    - Bucket sort
 3. Coding
 4. Tests
 """
@@ -21,38 +21,38 @@
 #         return False
 
 
-# # T=O(nlgk), S=O(k)
-# import sortedcontainers
-#
-# class Solution:
-#     def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
-#         def floor(n, bst):
-#             le = bst.bisect_right(n) - 1
-#             return bst[le] if le >= 0 else None
-#
-#         def ceiling(n, bst):
-#             ge = bst.bisect_right(n)
-#             return bst[ge] if ge < len(bst) else None
-#
-#         n = len(nums)
-#         if n < 2 or t < 0 or k < 0: return False
-#         bst = sortedcontainers.SortedList()
-#         for i, n in enumerate(nums):
-#             le = floor(n, bst)
-#             if le is not None and n <= le + t:
-#                 return True
-#             ge = ceiling(n, bst)
-#             if ge is not None and ge <= n + t:
-#                 return True
-#             bst.add(n)
-#             if len(bst) > k:
-#                 bst.remove(nums[i - k])
-#         return False
+# T=O(nlgk), S=O(k)
+import sortedcontainers
+
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        def floor(n, bst):
+            le = bst.bisect_right(n) - 1
+            return bst[le] if le >= 0 else None
+
+        def ceiling(n, bst):
+            ge = bst.bisect_right(n)
+            return bst[ge] if ge < len(bst) else None
+
+        n = len(nums)
+        if n < 2 or t < 0 or k < 0: return False
+        bst = sortedcontainers.SortedList()
+        for i, n in enumerate(nums):
+            le = floor(n, bst)
+            if le is not None and n <= le + t:
+                return True
+            ge = ceiling(n, bst)
+            if ge is not None and ge <= n + t:
+                return True
+            bst.add(n)
+            if len(bst) > k:
+                bst.remove(nums[i - k])
+        return False
 
 
 # T=O(n), S=O(k)
 class Solution:
-    def containsNearbyAlmostDuplicate(self, nums, k, t):
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
         n = len(nums)
         if n < 2 or t < 0 or k < 0: return False
         buckets = {}
