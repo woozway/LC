d8ae3949a4169fb03e53ebf977543c2e519dc888
chopchap <13806680065@163.com>
2021-04-05 20:28:12 +0800

    -

--- Backtracking/77. Combinations.py
+++ Backtracking/77. Combinations.py
@@ -1,19 +1,19 @@
 """
 1. Clarification
 2. Possible solutions
-     - pythonic
-     - backtracking or recursive
-     - non-recursive
+    - Pythonic
+    - Backtracking or recursive
+    - Non-recursive
 3. Coding
 4. Tests
 """
 
 
-# # T=O((n, k)*k), S=O(k)
-# class Solution:
-#     def combine(self, n: int, k: int) -> List[List[int]]:
-#         if n < 1 or k < 1: return [[]]
-#         return list(itertools.combinations(range(1, n + 1), k))
+# T=O((n, k)*k), S=O(k)
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if n < 1 or k < 1: return [[]]
+        return list(itertools.combinations(range(1, n + 1), k))
 
 
 # T=O((n, k)*k), S=O(n)
@@ -38,20 +38,20 @@ class Solution:
         self.dfs(cur + 1, n, k)
 
 
-# # T=O((n, k)*k), S=O(k)
-# class Solution:
-#     def combine(self, n: int, k: int) -> List[List[int]]:
-#         if n < 1 or k < 1: return [[]]
-#         tmp, ans = [], []
-#         for i in range(1, k + 1):
-#             tmp.append(i)
-#         tmp.append(n + 1)
-#         j = 0
-#         while j < k:
-#             ans.append(tmp[:k])
-#             j = 0
-#             while j < k and tmp[j] + 1 == tmp[j + 1]:
-#                 tmp[j] = j + 1
-#                 j += 1
-#             tmp[j] += 1
-#         return ans
+# T=O((n, k)*k), S=O(k)
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if n < 1 or k < 1: return [[]]
+        tmp, ans = [], []
+        for i in range(1, k + 1):
+            tmp.append(i)
+        tmp.append(n + 1)
+        j = 0
+        while j < k:
+            ans.append(tmp[:k])
+            j = 0
+            while j < k and tmp[j] + 1 == tmp[j + 1]:
+                tmp[j] = j + 1
+                j += 1
+            tmp[j] += 1
+        return ans
