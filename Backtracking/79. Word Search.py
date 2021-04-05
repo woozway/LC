1ac134511f6abe6e87ad3f7b8ea7508ea5044c06
chopchap <13806680065@163.com>
2021-04-05 21:05:16 +0800

    -

--- Backtracking/79. Word Search.py
+++ Backtracking/79. Word Search.py
@@ -1,14 +1,14 @@
 """
 1. Clarification
 2. Possible solutions
-     - dfs + backtracking
-     - trie + dfs + backtracking
+    - Backtracking
+    - Trie + backtracking
 3. Coding
 4. Tests
 """
 
 
-# dfs + backtracking
+# backtrack
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         if not board or not board[0] or len(word) == 0: return False
@@ -36,40 +36,40 @@ class Solution:
         return result
 
     
-# # trie + dfs + backtracking
-# dx = [-1, 1, 0, 0]
-# dy = [0, 0, -1, 1]
-#
-# END_OF_WORD = '#'
-#
-# class Solution:
-#     def exist(self, board: List[List[str]], word: str) -> bool:
-#         if not board or not board[0]: return False
-#         self.m, self.n, self.done = len(board), len(board[0]), False
-#         root = dict()
-#         node = root
-#         for char in word:
-#             node = node.setdefault(char, {})
-#         node[END_OF_WORD] = END_OF_WORD
-#         for i in range(self.m):
-#             for j in range(self.n):
-#                 if board[i][j] in root:
-#                     self.dfs(board, i, j, '', root)
-#                     if self.done:
-#                         return True
-#         return False
-#
-#     def dfs(self, board, row, col, cur_word, cur_dict):
-#         cur_word += board[row][col]
-#         cur_dict = cur_dict[board[row][col]]
-#         if END_OF_WORD in cur_dict:
-#             self.done = True
-#             return
-#         tmp, board[row][col] = board[row][col], '@'
-#         for i in range(4):
-#             x, y = row + dx[i], col + dy[i]
-#             if 0 <= x < self.m and 0 <= y < self.n and board[x][y] != '@' and board[x][y] in cur_dict:
-#                 self.dfs(board, x, y, cur_word, cur_dict)
-#                 if self.done:
-#                     return
-#         board[row][col] = tmp
+# trie + backtracking
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+END_OF_WORD = '#'
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        if not board or not board[0]: return False
+        self.m, self.n, self.done = len(board), len(board[0]), False
+        root = dict()
+        node = root
+        for char in word:
+            node = node.setdefault(char, {})
+        node[END_OF_WORD] = END_OF_WORD
+        for i in range(self.m):
+            for j in range(self.n):
+                if board[i][j] in root:
+                    self.dfs(board, i, j, '', root)
+                    if self.done:
+                        return True
+        return False
+
+    def dfs(self, board, row, col, cur_word, cur_dict):
+        cur_word += board[row][col]
+        cur_dict = cur_dict[board[row][col]]
+        if END_OF_WORD in cur_dict:
+            self.done = True
+            return
+        tmp, board[row][col] = board[row][col], '@'
+        for i in range(4):
+            x, y = row + dx[i], col + dy[i]
+            if 0 <= x < self.m and 0 <= y < self.n and board[x][y] != '@' and board[x][y] in cur_dict:
+                self.dfs(board, x, y, cur_word, cur_dict)
+                if self.done:
+                    return
+        board[row][col] = tmp
