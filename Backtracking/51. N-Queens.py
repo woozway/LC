0aef1a7a5c90b04c3ec8b7ff78620bdc8cfa85c3
chopchap <13806680065@163.com>
2021-04-05 21:02:38 +0800

    -

--- Backtracking/51. N-Queens.py
+++ Backtracking/51. N-Queens.py
@@ -1,8 +1,8 @@
 """
 1. Clarification
 2. Possible solutions
-     - brute force
-     - backtracking
+    - Brute force
+    - Backtracking
 3. Coding
 4. Tests
 """
@@ -14,10 +14,10 @@ class Solution:
         if n < 1: return []
         self.result = []
         self.cols = set(); self.pie = set(); self.na = set()
-        self.dfs(n, 0, [])
+        self.backtrack(n, 0, [])
         return self._generate_result(n)
 
-    def dfs(self, n, row, cur_list):
+    def backtrack(self, n, row, cur_list):
         if row >= n:
             self.result.append(cur_list)
             return
@@ -27,7 +27,7 @@ class Solution:
             self.cols.add(col)
             self.pie.add(row + col)
             self.na.add(row - col)
-            self.dfs(n, row + 1, cur_list + [col])
+            self.backtrack(n, row + 1, cur_list + [col])
             self.cols.remove(col)
             self.pie.remove(row + col)
             self.na.remove(row - col)
