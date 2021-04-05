509e62aedd204b722be0b89d663f67207c51fb13
chopchap <13806680065@163.com>
2021-04-05 10:20:00 +0800

    -

--- Linked List/21. Merge Two Sorted Lists.py
+++ Linked List/21. Merge Two Sorted Lists.py
@@ -32,7 +32,7 @@ class Solution:
     def mergeTwoLists(self, l1, l2):
         if not l1: return l2
         if not l2: return l1
-        prehead = ListNode(-1)
+        prehead = ListNode()
         prev = prehead
         while l1 and l2:
             if l1.val <= l2.val:
