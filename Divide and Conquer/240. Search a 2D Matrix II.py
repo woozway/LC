2095be6eb69a657ca602a5c7d02bc31c9124a79b
chopchap <13806680065@163.com>
2021-04-05 15:36:55 +0800

    -

--- Divide and Conquer/240. Search a 2D Matrix II.py
+++ Divide and Conquer/240. Search a 2D Matrix II.py
@@ -20,37 +20,37 @@
 #         return False
 
 
-# T=O(lg(n!)), S=O(1)
-class Solution:
-    def binary_search(self, matrix, target, start, vertical):
-        lo = start
-        hi = len(matrix[0]) - 1 if vertical else len(matrix) - 1
-        while hi >= lo:
-            mid = (lo + hi) // 2
-            if vertical:
-                if matrix[start][mid] < target:
-                    lo = mid + 1
-                elif matrix[start][mid] > target:
-                    hi = mid - 1
-                else:
-                    return True
-            else:
-                if matrix[mid][start] < target:
-                    lo = mid + 1
-                elif matrix[mid][start] > target:
-                    hi = mid - 1
-                else:
-                    return True
-        return False
+# # T=O(lg(n!)), S=O(1)
+# class Solution:
+#     def binary_search(self, matrix, target, start, vertical):
+#         lo = start
+#         hi = len(matrix[0]) - 1 if vertical else len(matrix) - 1
+#         while hi >= lo:
+#             mid = (lo + hi) // 2
+#             if vertical:
+#                 if matrix[start][mid] < target:
+#                     lo = mid + 1
+#                 elif matrix[start][mid] > target:
+#                     hi = mid - 1
+#                 else:
+#                     return True
+#             else:
+#                 if matrix[mid][start] < target:
+#                     lo = mid + 1
+#                 elif matrix[mid][start] > target:
+#                     hi = mid - 1
+#                 else:
+#                     return True
+#         return False
 
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not matrix or not matrix[0]: return False
-        for i in range(min(len(matrix), len(matrix[0]))):
-            vertical_found = self.binary_search(matrix, target, i, True)
-            horizontal_found = self.binary_search(matrix, target, i, False)
-            if vertical_found or horizontal_found:
-                return True
-        return False
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         if not matrix or not matrix[0]: return False
+#         for i in range(min(len(matrix), len(matrix[0]))):
+#             vertical_found = self.binary_search(matrix, target, i, True)
+#             horizontal_found = self.binary_search(matrix, target, i, False)
+#             if vertical_found or horizontal_found:
+#                 return True
+#         return False
 
 
 # T=O(nlgn), S=O(lgn)
