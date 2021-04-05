6a70a92f8ee821dca56355912ad3e9517e4ee2ee
chopchap <13806680065@163.com>
2021-04-05 21:12:23 +0800

    -

--- Tree/100. Same Tree.py
+++ Tree/100. Same Tree.py
@@ -1,8 +1,8 @@
 """
 1. Clarification
 2. Possible solutions
-     - recursion or backtracking
-     - iteration
+    - Recursion
+    - Iteration
 3. Coding
 4. Tests
 """
@@ -25,21 +25,21 @@ class Solution:
                self.isSameTree(p.left, q.left)
 
 
-# # T=O(n), S=O(lgn)
-# class Solution:
-#     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-#         def check(p, q):
-#             if not p and not q: return True
-#             if not q or not p: return False
-#             if p.val != q.val: return False
-#             return True
-# 
-#         deq = collections.deque([(p, q), ])
-#         while deq:
-#             p, q = deq.popleft()
-#             if not check(p, q):
-#                 return False
-#             if p:
-#                 deq.append((p.left, q.left))
-#                 deq.append((p.right, q.right))
-#         return True
+# T=O(n), S=O(lgn)
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        def check(p, q):
+            if not p and not q: return True
+            if not q or not p: return False
+            if p.val != q.val: return False
+            return True
+
+        deq = collections.deque([(p, q), ])
+        while deq:
+            p, q = deq.popleft()
+            if not check(p, q):
+                return False
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+        return True
