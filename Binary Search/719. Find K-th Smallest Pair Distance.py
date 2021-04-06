05e4c7379081d4f6e61056dcf7999089f87911d5
chopchap <13806680065@163.com>
2021-04-06 20:14:01 +0800

    -

--- Binary Search/719. Find K-th Smallest Pair Distance.py
+++ Binary Search/719. Find K-th Smallest Pair Distance.py
@@ -1,10 +1,10 @@
 """
 1. Clarification
 2. Possible solutions
- - brute force
- - heap
- - Binary Search + Prefix Sum
- - Binary Search + Sliding Window
+    - Brute force
+    - Heap
+    - Binary Search + Prefix Sum
+    - Binary Search + Sliding Window
 3. Coding
 4. Tests
 """
@@ -37,35 +37,35 @@
 #         return d
 
 
-# # T=O(W+nlgW+nlgn), S=O(n+W)
-# class Solution:
-#     def smallestDistancePair(self, nums: List[int], k: int) -> int:
-#         def possible(guess):
-#             return sum(prefix[min(x + guess, W)] - prefix[x] + multiplicity[i]
-#                        for i, x in enumerate(nums)) >= k
-#
-#         n = len(nums)
-#         if n < 2: return -1
-#         nums.sort()
-#         W = nums[-1]
-#         multiplicity = [0] * len(nums)
-#         for i, x in enumerate(nums):
-#             if i and x == nums[i-1]:
-#                 multiplicity[i] = 1 + multiplicity[i - 1]
-#         prefix = [0] * (W + 1)
-#         left = 0
-#         for i in range(len(prefix)):
-#             while left < len(nums) and nums[left] == i:
-#                 left += 1
-#             prefix[i] = left
-#         lo, hi = 0, nums[-1] - nums[0]
-#         while lo < hi:
-#             mi = (lo + hi) // 2
-#             if possible(mi):
-#                 hi = mi
-#             else:
-#                 lo = mi + 1
-#         return lo
+# T=O(W+nlgW+nlgn), S=O(n+W)
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        def possible(guess):
+            return sum(prefix[min(x + guess, W)] - prefix[x] + multiplicity[i]
+                       for i, x in enumerate(nums)) >= k
+
+        n = len(nums)
+        if n < 2: return -1
+        nums.sort()
+        W = nums[-1]
+        multiplicity = [0] * len(nums)
+        for i, x in enumerate(nums):
+            if i and x == nums[i-1]:
+                multiplicity[i] = 1 + multiplicity[i - 1]
+        prefix = [0] * (W + 1)
+        left = 0
+        for i in range(len(prefix)):
+            while left < len(nums) and nums[left] == i:
+                left += 1
+            prefix[i] = left
+        lo, hi = 0, nums[-1] - nums[0]
+        while lo < hi:
+            mi = (lo + hi) // 2
+            if possible(mi):
+                hi = mi
+            else:
+                lo = mi + 1
+        return lo
 
 
 # T=O(n(lgW+lgn)), S=O(1)
