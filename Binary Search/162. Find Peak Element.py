8ea332f883b7e566d94eb49500b9f5d462b8d7aa
chopchap <13806680065@163.com>
2021-04-06 17:01:21 +0800

    -

--- Binary Search/162. Find Peak Element.py
+++ Binary Search/162. Find Peak Element.py
@@ -27,7 +27,7 @@ class Solution:
 
     def search(self, nums, left, right):
         if left == right: return left
-        mid = left + (right - left) // 2
+        mid = (left + right) // 2
         if nums[mid] > nums[mid + 1]:
             return self.search(nums, left, mid)
         return self.search(nums, mid + 1, right)
