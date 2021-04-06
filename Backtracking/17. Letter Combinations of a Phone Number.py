72e07e110c0e11fd6e3594b7dcc927ca9e1c78d6
chopchap <13806680065@163.com>
2021-04-06 13:48:19 +0800

    -

--- Backtracking/17. Letter Combinations of a Phone Number.py
+++ Backtracking/17. Letter Combinations of a Phone Number.py
@@ -10,23 +10,18 @@
 # T=O(3^n * 4^m), S=O(3^n * 4^m)
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        def backtrack(combination, next_digits):
-            if len(next_digits) == 0:
-                output.append(combination)
-            else:
-                for letter in phone[next_digits[0]]:
-                    backtrack(combination + letter, next_digits[1:])
-
         if not digits: return []
-        phone = {'2': ['a', 'b', 'c'],
-                 '3': ['d', 'e', 'f'],
-                 '4': ['g', 'h', 'i'],
-                 '5': ['j', 'k', 'l'],
-                 '6': ['m', 'n', 'o'],
-                 '7': ['p', 'q', 'r', 's'],
-                 '8': ['t', 'u', 'v'],
-                 '9': ['w', 'x', 'y', 'z']}
-        output = []
-        if digits:
-            backtrack("", digits)
-        return output
+        self.digit2alpha = { '2': 'abc', '3': 'def', '4': 'ghi',
+                             '5': 'jkl', '6': 'mno', '7': 'pqrs',
+                             '8': 'tuv', '9': 'wxyz'}
+        self.ans = []
+        self.backtrack(digits, 0, '')
+        return self.ans
+
+    def backtrack(self, digits, idx, tmp):
+        if idx == len(digits):
+            self.ans.append(tmp)
+            return
+        s = self.digit2alpha[digits[idx]]
+        for c in s:
+            self.backtrack(digits, idx + 1, tmp + c)
