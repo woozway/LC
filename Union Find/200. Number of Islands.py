1328e5a3edb00d0fc41204f6b6d5408fd54b5117
chopchap <13806680065@163.com>
2021-04-08 19:50:35 +0800

    -

--- Union Find/200. Number of Islands.py
+++ Union Find/200. Number of Islands.py
@@ -1,51 +1,51 @@
 """
 1. Clarification
 2. Possible solutions
-     - Flood fill, dfs or bfs
-     - Union-Find
+    - Flood fill, dfs or bfs
+    - Union-Find
 3. Coding
 4. Tests
 """
 
 
-# # T=O(mn), S=O(mn), flood fill
-# dx = [-1, 1, 0, 0]
-# dy = [0, 0, -1, 1]
+# T=O(mn), S=O(mn), flood fill
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-# class Solution:
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         if not grid or not grid[0]: return 0
-#         self.m, self.n = len(grid), len(grid[0])
-#         self.grid = grid
-#         self.visited = set()
-#         # return sum([self.floodfill_dfs(i, j) for i in range(self.m) for j in range(self.n)])
-#         return sum([self.floodfill_bfs(i, j) for i in range(self.m) for j in range(self.n)])
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]: return 0
+        self.m, self.n = len(grid), len(grid[0])
+        self.grid = grid
+        self.visited = set()
+        # return sum([self.floodfill_dfs(i, j) for i in range(self.m) for j in range(self.n)])
+        return sum([self.floodfill_bfs(i, j) for i in range(self.m) for j in range(self.n)])
 
-#     # def floodfill_dfs(self, x, y):
-#     #     if not self._is_valid(x, y): return 0
-#     #     self.visited.add((x, y))
-#     #     for k in range(4):
-#     #         self.floodfill_dfs(x + dx[k], y + dy[k])
-#     #     return 1
+    # def floodfill_dfs(self, x, y):
+    #     if not self._is_valid(x, y): return 0
+    #     self.visited.add((x, y))
+    #     for k in range(4):
+    #         self.floodfill_dfs(x + dx[k], y + dy[k])
+    #     return 1
 
-#     def floodfill_bfs(self, x, y):
-#         if not self._is_valid(x, y): return 0
-#         self.visited.add((x, y))
-#         queue = collections.deque()
-#         queue.append((x, y))
-#         while queue:
-#             cur_x, cur_y = queue.popleft()
-#             for i in range(4):
-#                 new_x, new_y = cur_x + dx[i], cur_y + dy[i]
-#                 if self._is_valid(new_x, new_y):
-#                     self.visited.add((new_x, new_y))
-#                     queue.append((new_x, new_y))
-#         return 1
+    def floodfill_bfs(self, x, y):
+        if not self._is_valid(x, y): return 0
+        self.visited.add((x, y))
+        queue = collections.deque()
+        queue.append((x, y))
+        while queue:
+            cur_x, cur_y = queue.popleft()
+            for i in range(4):
+                new_x, new_y = cur_x + dx[i], cur_y + dy[i]
+                if self._is_valid(new_x, new_y):
+                    self.visited.add((new_x, new_y))
+                    queue.append((new_x, new_y))
+        return 1
 
-#     def _is_valid(self, x, y):
-#         if x < 0 or x >= self.m or y < 0 or y >= self.n: return False
-#         if self.grid[x][y] == '0' or ((x, y) in self.visited): return False
-#         return True
+    def _is_valid(self, x, y):
+        if x < 0 or x >= self.m or y < 0 or y >= self.n: return False
+        if self.grid[x][y] == '0' or ((x, y) in self.visited): return False
+        return True
 
 
 # T=O(mn), S=O(mn), union find
