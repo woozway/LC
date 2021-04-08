7eefe24e81352eb3353130c722ebfcf1210edcf1
chopchap <13806680065@163.com>
2021-04-08 16:21:35 +0800

    -

--- Trie/648. Replace Words.py
+++ Trie/648. Replace Words.py
@@ -1,24 +1,24 @@
 """
 1. Clarification
 2. Possible solutions
-     - Prefix Hash
-     - Trie
+    - Prefix Hash
+    - Trie
 3. Coding
 4. Tests
 """
 
 
-# # T=O(sigma(wi^2)), S=O(n)
-# class Solution:
-#     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-#         def replace(word):
-#             for i in range(1, len(word)):
-#                 if word[:i] in rootset:
-#                     return word[:i]
-#             return word
-# 
-#         rootset = set(dictionary)
-#         return ' '.join(map(replace, sentence.split()))
+# T=O(sigma(wi^2)), S=O(n)
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        def replace(word):
+            for i in range(1, len(word)):
+                if word[:i] in rootset:
+                    return word[:i]
+            return word
+
+        rootset = set(dictionary)
+        return ' '.join(map(replace, sentence.split()))
 
 
 # T=O(n), S=O(n)
