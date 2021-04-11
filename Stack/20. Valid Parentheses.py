eca3c48f2bea9306295def0f7162e4c91f24a954
chopchap <13806680065@163.com>
2021-04-11 13:38:02 +0800

    -

--- Stack/20. Valid Parentheses.py
+++ Stack/20. Valid Parentheses.py
@@ -1,20 +1,20 @@
 """
 1. Clarification
 2. Possible solutions
-     - Replace adjacent matched brackets
-     - Use stack to match brackets
+    - Replace adjacent matched brackets
+    - Use stack to match brackets
 3. Coding
 4. Tests
 """
 
 
-# # T=O(n^2), S=O(1)
-# class Solution:
-#     def isValid(self, s: str) -> bool:
-#         if len(s) % 2: return False
-#         while '()' in s or '[]' in s or '{}' in s:
-#             s = s.replace('()', '').replace('[]', '').replace('{}', '')
-#         return s == ''
+# T=O(n^2), S=O(1)
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2: return False
+        while '()' in s or '[]' in s or '{}' in s:
+            s = s.replace('()', '').replace('[]', '').replace('{}', '')
+        return s == ''
 
 
 # T=O(n), S=O(n)
