6efbfd683d3a964392a53ddb1abc60f9e09a697d
chopchap <13806680065@163.com>
2021-04-11 11:35:19 +0800

    -

--- Hash Table/15. 3Sum.py
+++ Hash Table/15. 3Sum.py
@@ -1,12 +1,13 @@
 """
 1. Clarification
 2. Possible solutions
- - brute force
- - hashMap, c=-(a+b)
- - two pointers (sort and find)
+    - Brute force
+    - HashMap, c=-(a+b)
+    - Two pointers (sort and find)
 3. Coding
 4. Tests
 """
+
 
 # # T=O(n^3), S=O(1), Time Limit Exceeded
 # class Solution:
@@ -20,6 +21,7 @@
 #                     if nums[i] + nums[j] + nums[k] == 0:
 #                         res.add(tuple([nums[i], nums[j], nums[k]]))
 #         return list(res)
+
 
 # T=O(n^2), S=O(n)
 class Solution:
@@ -38,23 +40,24 @@ class Solution:
                     res.add((v, -v-x, x))
         return map(list, res)
 
-# # T=O(n^2), S=O(1)
-# class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         nums.sort()
-#         res = []
-#         n = len(nums)
-#         for i in range(n - 2):
-#             if i >= 1 and nums[i] == nums[i-1]:
-#                 continue
-#             l, r = i + 1, n - 1
-#             while l < r:
-#                 s = nums[i] + nums[l] + nums[r]
-#                 if s > 0: r -= 1
-#                 elif s < 0: l += 1
-#                 else:
-#                     res.append([nums[i], nums[l], nums[r]])
-#                     while l < r and nums[l] == nums[l+1]: l += 1
-#                     while l < r and nums[r] == nums[r-1]: r -= 1
-#                     l += 1; r -= 1
-#         return res
+
+# T=O(n^2), S=O(1)
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        n = len(nums)
+        for i in range(n - 2):
+            if i >= 1 and nums[i] == nums[i-1]:
+                continue
+            l, r = i + 1, n - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s > 0: r -= 1
+                elif s < 0: l += 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]: l += 1
+                    while l < r and nums[r] == nums[r-1]: r -= 1
+                    l += 1; r -= 1
+        return res
