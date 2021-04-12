04b7086dbc9fe69ece8be8b596b19e8451117d39
chopchap <13806680065@163.com>
2021-04-12 08:18:14 +0800

    -

--- Dynamic Programming/32. Longest Valid Parentheses.py
+++ Dynamic Programming/32. Longest Valid Parentheses.py
@@ -24,3 +24,49 @@ class Solution:
                     dp[i] = dp[i-1] + (dp[i - dp[i-1] - 2] if i - dp[i-1] >= 2 else 0) + 2
                 maxAns = max(maxAns, dp[i])
         return maxAns
+
+
+# T=O(n), S=O(n)
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        if not s: return 0
+        maxAns = 0
+        stack = [-1]
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    maxAns = max(maxAns, i - stack[-1])
+        return maxAns
+
+
+# T=O(n), S=O(1)
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        if not s: return 0
+        left, right = 0, 0
+        maxlen = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                maxlen = max(maxlen, 2 * right)
+            elif right > left:
+                left = right = 0
+        left = right = 0
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                maxlen = max(maxlen, 2 * left)
+            elif left > right:
+                left = right = 0
+        return maxlen
