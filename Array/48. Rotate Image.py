2cb2778fa52118322c08cefb1f6fbe636f6df513
chopchap <13806680065@163.com>
2021-04-13 21:50:40 +0800

    -

--- Array/48. Rotate Image.py
+++ Array/48. Rotate Image.py
@@ -1,8 +1,40 @@
+"""
+1. Clarification
+2. Possible solutions
+    - Rotate Groups of Four Cells
+    - Reverse on Diagonal and then Reverse Left to Right
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n^2), S=O(1)
 class Solution:
-  def rotate(self, matrix: List[List[int]]) -> None:
-    n = len(matrix[0])
-    for i in range(n):
-      for j in range(i, n):
-        matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
-    for i in range(n):
-      matrix[i].reverse()
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                tmp = matrix[n - 1 - j][i]
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+                matrix[j][n - 1 - i] = matrix[i][j]
+                matrix[i][j] = tmp
+
+
+# T=O(n^2), S=O(1)
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        self.transpose(matrix)
+        self.reflect(matrix)
+
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
+
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
