15550c7e9df6e3f5d8d8f17834abbb4f91be5c30
chopchap <13806680065@163.com>
2021-04-18 21:40:40 +0800

    -

--- Array/1299. Replace Elements with Greatest Element on Right Side.py
+++ Array/1299. Replace Elements with Greatest Element on Right Side.py
@@ -1,23 +1,23 @@
 """
 1. Clarification
 2. Possible solutions
-     - Brute force
-     - Reversed traverse
+    - Brute force
+    - Reversed traverse
 3. Coding
 4. Tests
 """
 
 
-# # T=O(n^2), S=O(1)
-# class Solution:
-#     def replaceElements(self, arr: List[int]) -> List[int]:
-#         if not arr: return []
-#         res = []
-#         n = len(arr)
-#         for i in range(n - 1):
-#             res.append(max(arr[i + 1:]))
-#         res.append(-1)
-#         return res
+# T=O(n^2), S=O(1)
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        if not arr: return []
+        res = []
+        n = len(arr)
+        for i in range(n - 1):
+            res.append(max(arr[i + 1:]))
+        res.append(-1)
+        return res
 
 
 # T=O(n), S=O(1)
