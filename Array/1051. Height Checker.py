12c70763096ddf577aa6dcb066d3b397255a1a0f
chopchap <13806680065@163.com>
2021-04-18 21:38:17 +0800

    -

--- Array/1051. Height Checker.py
+++ Array/1051. Height Checker.py
@@ -1,8 +1,8 @@
 """
 1. Clarification
 2. Possible solutions
-     - Sort
-     - Bucket Sort
+    - Quick sort
+    - Bucket Sort
 3. Coding
 4. Tests
 """
@@ -20,18 +20,18 @@ class Solution:
         return count
 
 
-# # T=O(n), S=O(1)
-# class Solution:
-#     def heightChecker(self, heights: List[int]) -> int:
-#         if not heights: return 0
-#         arr = [0] * 101
-#         for height in heights:
-#             arr[height] += 1
-#         j = count = 0
-#         for i in range(1, 101):
-#             while arr[i] > 0:
-#                 if heights[j] != i:
-#                     count += 1
-#                 j += 1
-#                 arr[i] -= 1
-#         return count
+# T=O(n), S=O(1)
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        if not heights: return 0
+        arr = [0] * 101
+        for height in heights:
+            arr[height] += 1
+        j = count = 0
+        for i in range(1, 101):
+            while arr[i] > 0:
+                if heights[j] != i:
+                    count += 1
+                j += 1
+                arr[i] -= 1
+        return count
