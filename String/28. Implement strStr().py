4c478a896f4dc18211916a1caa08a6029299f640
chopchap <13806680065@163.com>
2021-04-20 09:43:47 +0800

    -

--- String/28. Implement strStr().py
+++ String/28. Implement strStr().py
@@ -21,7 +21,7 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         L, n = len(needle), len(haystack)
         if L == 0: return 0
-        if L > n: return -1
+        if L > n or (L == n and haystack != needle): return -1
         for start in range(n - L + 1):
             if haystack[start: start + L] == needle:
                 return start
@@ -33,7 +33,7 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         L, n = len(needle), len(haystack)
         if L == 0: return 0
-        if L > n: return -1
+        if L > n or (L == n and haystack != needle): return -1
         pn = 0
         while pn < n - L + 1:
             while pn < n - L + 1 and haystack[pn] != needle[0]:
@@ -49,26 +49,25 @@ class Solution:
         return -1
 
 
-# # T=O(n), S=O(1)
-# class Solution:
-#     def strStr(self, haystack: str, needle: str) -> int:
-#         L, n = len(needle), len(haystack)
-#         if L == 0: return 0
-#         if L > n: return -1
-#         if L == n and haystack != needle: return -1
-#         a = 26
-#         modulus = 2 ** 31
-#         h_to_int = lambda i: ord(haystack[i]) - ord('a')
-#         needle_to_int = lambda i: ord(needle[i]) - ord('a')
-#         h = ref_h = 0
-#         for i in range(L):
-#             h = (h * a + h_to_int(i)) % modulus
-#             ref_h = (ref_h * a + needle_to_int(i)) % modulus
-#         if h == ref_h:
-#             return 0
-#         aL = pow(a, L, modulus)
-#         for start in range(1, n - L + 1):
-#             h = (h * a - h_to_int(start - 1) * aL + h_to_int(start + L - 1)) % modulus
-#             if h == ref_h:
-#                 return start
-#         return -1
+# T=O(n), S=O(1)
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        L, n = len(needle), len(haystack)
+        if L == 0: return 0
+        if L > n or (L == n and haystack != needle): return -1
+        a = 26
+        modulus = 2 ** 31
+        h_to_int = lambda i: ord(haystack[i]) - ord('a')
+        needle_to_int = lambda i: ord(needle[i]) - ord('a')
+        h = ref_h = 0
+        for i in range(L):
+            h = (h * a + h_to_int(i)) % modulus
+            ref_h = (ref_h * a + needle_to_int(i)) % modulus
+        if h == ref_h:
+            return 0
+        aL = pow(a, L, modulus)
+        for start in range(1, n - L + 1):
+            h = (h * a - h_to_int(start - 1) * aL + h_to_int(start + L - 1)) % modulus
+            if h == ref_h:
+                return start
+        return -1
