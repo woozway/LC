39763912da77ced9a67a97ea550e6c477c2dfeff
chopchap <13806680065@163.com>
2021-04-23 17:14:10 +0800

    -

--- Sliding Window/239. Sliding Window Maximum.py
+++ Sliding Window/239. Sliding Window Maximum.py
@@ -1,23 +1,25 @@
 """
 1. Clarification
 2. Possible solutions
- - use MaxHeap
- - use monotonous Queue
+    - MaxHeap
+    - Monotonous Queue
 3. Coding
 4. Tests
 """
 
-# # T=O(nlgn), S=O(n)
-# class Solution:
-#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-#         q, res = [], []
-#         for i in range(len(nums)):
-#             while q and q[0][1] <= i - k:
-#                 heapq.heappop(q)
-#             heapq.heappush(q, (-nums[i], i))
-#             if i >= k - 1:
-#                 res.append(-q[0][0])
-#         return res
+
+# T=O(nlgn), S=O(n)
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q, res = [], []
+        for i in range(len(nums)):
+            while q and q[0][1] <= i - k:
+                heapq.heappop(q)
+            heapq.heappush(q, (-nums[i], i))
+            if i >= k - 1:
+                res.append(-q[0][0])
+        return res
+
 
 # T=O(n), S=O(k)
 class Solution:
