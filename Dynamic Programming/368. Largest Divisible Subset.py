cd873a5d6c3bac5744e227cfb6b04b48ef927177
chopchap <13806680065@163.com>
2021-04-23 15:00:10 +0800

    -

--- Dynamic Programming/368. Largest Divisible Subset.py
+++ Dynamic Programming/368. Largest Divisible Subset.py
@@ -36,10 +36,10 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         if not nums or len(nums) == 1: return nums
-        l = len(nums)
+        n = len(nums)
         nums.sort()
         dp = [[i] for i in nums]
-        for i in range(1, l):
+        for i in range(1, n):
             for j in range(i - 1, -1, -1):
                 if nums[i] % nums[j] == 0:
                     dp[i] = max(dp[j] + [nums[i]], dp[i], key=len)
