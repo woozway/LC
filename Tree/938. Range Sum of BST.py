56cdd73c10f795e4d56274494bef71ec0479204e
chopchap <13806680065@163.com>
2021-04-27 11:57:49 +0800

    -

--- Tree/938. Range Sum of BST.py
+++ Tree/938. Range Sum of BST.py
@@ -1,14 +1,47 @@
+"""
+1. Clarification
+2. Possible solutions
+    - dfs, top-down
+    - bfs
+3. Coding
+4. Tests
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# T=O(n), S=O(n)
 class Solution:
-  def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
-    ans = 0
-    stack = [root]
-    while stack:
-      node = stack.pop()
-      if node:
-        if low <= node.val <= high:
-          ans += node.val
-        if low < node.val:
-          stack.append(node.left)
-        if node.val < high:
-          stack.append(node.right)
-    return ans
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        if not root:
+            return 0
+        if root.val > high:
+            return self.rangeSumBST(root.left, low, high)
+        if root.val < low:
+            return self.rangeSumBST(root.right, low, high)
+        return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+
+
+# T=O(n), S=O(n)
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        total = 0
+        q = collections.deque([root])
+        while q:
+            node = q.popleft()
+            if not node:
+                continue
+            if node.val > high:
+                q.append(node.left)
+            elif node.val < low:
+                q.append(node.right)
+            else:
+                total += node.val
+                q.append(node.left)
+                q.append(node.right)
+        return total
