390257be9b4a0c03264b544072040e2109775fa6
chopchap <13806680065@163.com>
2021-05-01 10:21:28 +0800

    -

--- Depth-first Search/690. Employee Importance.py
+++ Depth-first Search/690. Employee Importance.py
@@ -2,6 +2,7 @@
 1. Clarification
 2. Possible solutions
     - dfs
+    - bfs
 3. Coding
 4. Tests
 """
@@ -25,3 +26,18 @@ class Solution:
             return (employee.importance +
                     sum(dfs(eid) for eid in employee.subordinates))
         return dfs(id)
+
+
+# T=O(n), S=O(n)
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        emap = {e.id: e for e in employees}
+        total = 0
+        Q = collections.deque([id])
+        while Q:
+            idx = Q.popleft()
+            employee = emap[idx]
+            total += employee.importance
+            for sub in employee.subordinates:
+                Q.append(sub)
+        return total
