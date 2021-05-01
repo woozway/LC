99b54fc53d0d051e5934244f879599e224022848
chopchap <13806680065@163.com>
2021-05-01 10:08:13 +0800

    -

--- Depth-first Search/690. Employee Importance.py
+++ Depth-first Search/690. Employee Importance.py
@@ -1,7 +1,27 @@
+"""
+1. Clarification
+2. Possible solutions
+    - dfs
+3. Coding
+4. Tests
+"""
+
+
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+# T=O(n), S=O(n)
 class Solution:
-  def getImportance(self, employees: List['Employee'], id: int) -> int:
-    emap = {e.id: e for e in employees}
-    def dfs(eid):
-      employee = emap[eid]
-      return (employee.importance + sum(dfs(eid) for eid in employee.subordinates))
-    return dfs(id)
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        emap = {e.id: e for e in employees}
+        def dfs(eid):
+            employee = emap[eid]
+            return (employee.importance +
+                    sum(dfs(eid) for eid in employee.subordinates))
+        return dfs(id)
