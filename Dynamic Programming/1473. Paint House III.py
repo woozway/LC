bfc245dadb178b5d4810f189ebed449e9decaf87
chopchap <13806680065@163.com>
2021-05-04 11:06:40 +0800

    -

--- Dynamic Programming/1473. Paint House III.py
+++ Dynamic Programming/1473. Paint House III.py
@@ -4,6 +4,7 @@
     - Brute force + Backtracking
     - Dynamic programming
     - Dynamic programming + optimisation
+    - Dynamic programming v3
 3. Coding
 4. Tests
 """
@@ -104,3 +105,23 @@ class Solution:
                         info.second = dp[i][j][k]
         ans = min(dp[m - 1][j][target - 1] for j in range(n))
         return -1 if ans == math.inf else ans
+
+
+# T=O(m*n^2*target), S=O(m*n*target)
+class Solution:
+    def minCost(self, houses: List[int], cost: List[List[int]], m: int, n: int, target: int) -> int:
+        if m < 1 or n < 1: return -1
+        dp = {}
+        def dfs(i, t, p):
+            key = (i, t, p)
+            if i == len(houses) or t < 0 or m - i < t:
+                return 0 if t == 0 and i == len(houses) else math.inf
+            if key not in dp:
+                if houses[i] == 0:
+                    dp[key] = min(dfs(i + 1, t - (nc != p), nc) + cost[i][nc - 1] for nc in range(1, n + 1))
+                else:
+                    dp[key] = dfs(i + 1, t - (houses[i] != p), houses[i])
+            return dp[key]
+
+        ret = dfs(0, target, -1)
+        return ret if ret < math.inf else -1
