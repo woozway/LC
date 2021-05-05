f43d795909b369162280b1afc99e624b5f200d5e
chopchap <13806680065@163.com>
2021-05-05 10:32:17 +0800

    -

--- Dynamic Programming/198. House Robber.py
+++ Dynamic Programming/198. House Robber.py
@@ -27,9 +27,11 @@ class Solution:
 # T=O(n), S=O(1)
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums: return 0
+        if not nums:
+            return 0
         n = len(nums)
-        if n == 1: return nums[0]
+        if n == 1:
+            return nums[0]
         first, second = nums[0], max(nums[0], nums[1])
         for i in range(2, n):
             first, second = second, max(first + nums[i], second)
