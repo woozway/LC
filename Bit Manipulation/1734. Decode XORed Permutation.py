72fc1b8bdb6c95304014b6e1263c07cefc74b8b5
chopchap <13806680065@163.com>
2021-05-11 08:46:13 +0800

    -

---
+++ Bit Manipulation/1734. Decode XORed Permutation.py
@@ -0,0 +1,21 @@
+"""
+1. Clarification
+2. Possible solutions
+    - Bit Manipulation
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n), S=O(1)
+class Solution:
+    def decode(self, encoded: List[int]) -> List[int]:
+        n = len(encoded) + 1
+        total = functools.reduce(operator.xor, range(1, n + 1))
+        odd = 0
+        for i in range(1, n - 1, 2):
+            odd ^= encoded[i]
+        perm = [total ^ odd]
+        for i in range(n - 1):
+            perm.append(perm[-1] ^ encoded[i])
+        return perm
