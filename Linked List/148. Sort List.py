01c137c18acae61b6d82da4347ddb456059d87fd
chopchap <13806680065@163.com>
2021-05-11 20:29:14 +0800

    -

---
+++ Linked List/148. Sort List.py
@@ -0,0 +1,129 @@
+"""
+1. Clarification
+2. Possible solutions
+    - Brute force
+    - Top Down Merge Sort
+    - Bottom Up Merge Sort
+3. Coding
+4. Tests
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# T=O(nlgn), S=O(n)
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        if not head: return head
+        lst = []
+        node = head
+        while node:
+            lst.append(node)
+            node = node.next
+        ret = sorted(lst, key=lambda x: x.val)
+        for i in range(len(ret) - 1):
+            ret[i].next = ret[i + 1]
+        ret[-1].next = None
+        return ret[0]
+
+
+# T=O(nlgn), S=O(lgn)
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        def sortFunc(head: ListNode, tail: ListNode) -> ListNode:
+            if not head:
+                return head
+            if head.next == tail:
+                head.next = None
+                return head
+            slow = fast = head
+            while fast != tail:
+                slow = slow.next
+                fast = fast.next
+                if fast != tail:
+                    fast = fast.next
+            mid = slow
+            return merge(sortFunc(head, mid), sortFunc(mid, tail))
+
+        def merge(head1, head2):
+            dummyHead = ListNode()
+            temp, temp1, temp2 = dummyHead, head1, head2
+            while temp1 and temp2:
+                if temp1.val <= temp2.val:
+                    temp.next = temp1
+                    temp1 = temp1.next
+                else:
+                    temp.next = temp2
+                    temp2 = temp2.next
+                temp = temp.next
+            if temp1:
+                temp.next = temp1
+            elif temp2:
+                temp.next = temp2
+            return dummyHead.next
+
+        return sortFunc(head, None)
+
+
+# T=O(nlgn), S=O(1)
+class Solution:
+    def sortList(self, head: ListNode) -> ListNode:
+        def merge(head1: ListNode, head2: ListNode) -> ListNode:
+            dummyHead = ListNode(0)
+            temp, temp1, temp2 = dummyHead, head1, head2
+            while temp1 and temp2:
+                if temp1.val <= temp2.val:
+                    temp.next = temp1
+                    temp1 = temp1.next
+                else:
+                    temp.next = temp2
+                    temp2 = temp2.next
+                temp = temp.next
+            if temp1:
+                temp.next = temp1
+            elif temp2:
+                temp.next = temp2
+            return dummyHead.next
+
+        if not head:
+            return head
+        length = 0
+        node = head
+        while node:
+            length += 1
+            node = node.next
+        dummyHead = ListNode(0, head)
+        subLength = 1
+        while subLength < length:
+            prev, curr = dummyHead, dummyHead.next
+            while curr:
+                head1 = curr
+                for i in range(1, subLength):
+                    if curr.next:
+                        curr = curr.next
+                    else:
+                        break
+                head2 = curr.next
+                curr.next = None
+                curr = head2
+                for i in range(1, subLength):
+                    if curr and curr.next:
+                        curr = curr.next
+                    else:
+                        break
+                succ = None
+                if curr:
+                    succ = curr.next
+                    curr.next = None
+                merged = merge(head1, head2)
+                prev.next = merged
+                while prev.next:
+                    prev = prev.next
+                curr = succ
+            subLength <<= 1
+
+        return dummyHead.next
