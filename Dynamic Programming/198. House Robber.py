02c3df0fa13bf7838bec8c1f8a60ad4cb8c72624
chopchap <13806680065@163.com>
2021-05-12 20:23:56 +0800

    -

--- Dynamic Programming/198. House Robber.py
+++ Dynamic Programming/198. House Robber.py
@@ -8,23 +8,21 @@
 """
 
 
-# T=O(n), S=O(n)
+# T=O(n), S=O(n), dp[i]: max amount of money when reaching index i in list nums
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        if not nums: return 0
         n = len(nums)
-        if n == 1:
-            return nums[0]
+        if n == 1: return nums[0]
         dp = [0] * n
         dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
+        dp[1] = max(nums[:2])
         for i in range(2, n):
             dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
-        return dp[n - 1]
+        return dp[-1]
 
 
-# T=O(n), S=O(1), see leetcode 740.
+# T=O(n), S=O(1), see also leetcode 740.
 class Solution:
     def rob(self, nums: List[int]) -> int:
         if not nums:
