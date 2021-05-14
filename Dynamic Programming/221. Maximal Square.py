1274cb8fe3f59b094bfd014fd96c59da39460483
chopchap <13806680065@163.com>
2021-05-14 20:57:20 +0800

    -

--- Dynamic Programming/221. Maximal Square.py
+++ Dynamic Programming/221. Maximal Square.py
@@ -14,12 +14,12 @@ class Solution:
         if not matrix or not matrix[0]:
             return 0
         maxSide = 0
-        rows, columns = len(matrix), len(matrix[0])
+        rows, cols = len(matrix), len(matrix[0])
         for i in range(rows):
-            for j in range(columns):
+            for j in range(cols):
                 if matrix[i][j] == '1':
                     maxSide = max(maxSide, 1)
-                    currentMaxSide = min(rows - i, columns - j)
+                    currentMaxSide = min(rows - i, cols - j)
                     for k in range(1, currentMaxSide):
                         flag = True
                         if matrix[i + k][j + k] == '0':
@@ -43,10 +43,10 @@ class Solution:
         if len(matrix) == 0 or len(matrix[0]) == 0:
             return 0
         maxSide = 0
-        rows, columns = len(matrix), len(matrix[0])
-        dp = [[0] * columns for _ in range(rows)]
+        rows, cols = len(matrix), len(matrix[0])
+        dp = [[0] * cols for _ in range(rows)]
         for i in range(rows):
-            for j in range(columns):
+            for j in range(cols):
                 if matrix[i][j] == '1':
                     if i == 0 or j == 0:
                         dp[i][j] = 1
