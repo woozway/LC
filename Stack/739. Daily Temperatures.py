e25be63ee0f0afb5c9a7cfa719c1c419801c0c05
chopchap <13806680065@163.com>
2021-05-15 09:31:17 +0800

    -

--- Stack/739. Daily Temperatures.py
+++ Stack/739. Daily Temperatures.py
@@ -1,25 +1,25 @@
 """
 1. Clarification
 2. Possible solutions
-     - Brute force
-     - Monotonic stack
+    - Brute force
+    - Monotonic stack
 3. Coding
 4. Tests
 """
 
 
-# # T=O(nm), S=O(m), m=len(next)
-# class Solution:
-#     def dailyTemperatures(self, T: List[int]) -> List[int]:
-#         if not T: return []
-#         n = len(T)
-#         ans, nxt, big = [0] * n, dict(), 10**9
-#         for i in range(n - 1, -1, -1):
-#             warmer_index = min(nxt.get(t, big) for t in range(T[i] + 1, 102))
-#             if warmer_index != big:
-#                 ans[i] = warmer_index - i
-#             nxt[T[i]] = i
-#         return ans
+# T=O(nm), S=O(m), m=len(next)
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        if not T: return []
+        n = len(T)
+        ans, nxt, big = [0] * n, dict(), 10**9
+        for i in range(n - 1, -1, -1):
+            warmer_index = min(nxt.get(t, big) for t in range(T[i] + 1, 102))
+            if warmer_index != big:
+                ans[i] = warmer_index - i
+            nxt[T[i]] = i
+        return ans
 
 
 # T=O(n), S=O(n)
