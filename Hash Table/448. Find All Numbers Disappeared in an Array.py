bf982ce53af045842003cc1f996105ea2f0b811d
chopchap <13806680065@163.com>
2021-05-15 09:29:53 +0800

    -

--- Hash Table/448. Find All Numbers Disappeared in an Array.py
+++ Hash Table/448. Find All Numbers Disappeared in an Array.py
@@ -1,8 +1,8 @@
 """
 1. Clarification
 2. Possible solutions
-     - HashMap
-     - In-place
+    - HashMap
+    - In-place
 3. Coding
 4. Tests
 """
@@ -22,16 +22,16 @@ class Solution:
         return result
 
 
-# # T=O(n), S=O(1)
-# class Solution:
-#     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-#         if not nums: return []
-#         for i in range(len(nums)):
-#             new_index = abs(nums[i]) - 1
-#             if nums[new_index] > 0:
-#                 nums[new_index] *= -1
-#         result = []
-#         for i in range(1, len(nums) + 1):
-#             if nums[i - 1] > 0:
-#                 result.append(i)
-#         return result
+# T=O(n), S=O(1)
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        if not nums: return []
+        for i in range(len(nums)):
+            new_index = abs(nums[i]) - 1
+            if nums[new_index] > 0:
+                nums[new_index] *= -1
+        result = []
+        for i in range(1, len(nums) + 1):
+            if nums[i - 1] > 0:
+                result.append(i)
+        return result
