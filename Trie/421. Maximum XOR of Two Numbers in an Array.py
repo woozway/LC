0442a1585650e61433d523e5c75b0971be6e9429
chopchap <13806680065@163.com>
2021-05-16 09:45:27 +0800

    -

--- Trie/421. Maximum XOR of Two Numbers in an Array.py
+++ Trie/421. Maximum XOR of Two Numbers in an Array.py
@@ -8,6 +8,28 @@
 """
 
 
+# T=O(nlgC), S=O(n), C=max(nums)
+class Solution:
+    def findMaximumXOR(self, nums: List[int]) -> int:
+        HIGH_BIT = 30
+        x = 0
+        for k in range(HIGH_BIT, -1, -1):
+            seen = set()
+            for num in nums:
+                seen.add(num >> k)
+            x_next = x * 2 + 1
+            found = False
+            for num in nums:
+                if x_next ^ (num >> k) in seen:
+                    found = True
+                    break
+            if found:
+                x = x_next
+            else:
+                x = x_next - 1
+        return x
+
+
 # T=O(n), S=O(1)
 class Solution:
     def findMaximumXOR(self, nums: List[int]) -> int:
