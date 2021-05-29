3b872fc5f4cdd11f98a617fa5e33cad491e4639d
chopchap <13806680065@163.com>
2021-05-29 20:49:21 +0800

    -

--- Dynamic Programming/312. Burst Balloons.py
+++ Dynamic Programming/312. Burst Balloons.py
@@ -29,15 +29,16 @@
 
 
 # T=O(n^3), S=O(n^2)
+# dp[i][j]: max # of coins one can get for filling in range (i, j)
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
         n = len(nums)
-        rec = [[0] * (n + 2) for _ in range(n + 2)]
+        dp = [[0] * (n + 2) for _ in range(n + 2)]
         val = [1] + nums + [1]
         for i in range(n - 1, -1, -1):
             for j in range(i + 2, n + 2):
                 for k in range(i + 1, j):
                     total = val[i] * val[k] * val[j]
-                    total += rec[i][k] + rec[k][j]
-                    rec[i][j] = max(rec[i][j], total)
-        return rec[0][n + 1]
+                    total += dp[i][k] + dp[k][j]
+                    dp[i][j] = max(dp[i][j], total)
+        return dp[0][n + 1]
