f0657fc904409b7e659d166b529ba72926fd8cdc
chopchap <13806680065@163.com>
2021-06-10 20:46:17 +0800

    -

--- Heap/1046. Last Stone Weight.py
+++ Heap/1046. Last Stone Weight.py
@@ -1,14 +1,24 @@
+"""
+1. Clarification
+2. Possible solutions
+    - Heap
+3. Coding
+4. Tests
+"""
+
+
+# T=O(nlgn), S=O(n)
 class Solution:
-  def lastStoneWeight(self, stones: List[int]) -> int:
-    darkStones = [-1 * s for s in stones]
-    heapify(darkStones)
-    while True:
-      if not darkStones:
-        return 0
-      elif len(darkStones) == 1:
-        return -darkStones[0]
-      else:
-        a = heappop(darkStones)
-        b = heappop(darkStones)
-        if a != b:
-          heappush(darkStones, a-b)
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        darkStones = [-1 * s for s in stones]
+        heapq.heapify(darkStones)
+        while True:
+            if not darkStones:
+                return 0
+            elif len(darkStones) == 1:
+                return -darkStones[0]
+            else:
+                a = heapq.heappop(darkStones)
+                b = heapq.heappop(darkStones)
+                if a != b:
+                    heapq.heappush(darkStones, a - b)
