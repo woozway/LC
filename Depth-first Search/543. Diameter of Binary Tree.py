2291e6dfcc630721a356b9d01edf1edf33301d86
chopchap <13806680065@163.com>
2021-06-20 18:30:25 +0800

    -

--- Depth-first Search/543. Diameter of Binary Tree.py
+++ Depth-first Search/543. Diameter of Binary Tree.py
@@ -1,13 +1,30 @@
-class Solution:
-  def diameterOfBinaryTree(self, root: TreeNode) -> int:
-    self.ans = 1
-    def dfs(root):
-      if root is None:
-        return 0
-      L = dfs(root.left)
-      R = dfs(root.right)
-      self.ans = max(self.ans, L+R+1)
-      return max(L, R) + 1
+"""
+1. Clarification
+2. Possible solutions
+    - dfs
+3. Coding
+4. Tests
+"""
 
-    dfs(root)
-    return self.ans - 1
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# T=O(n), S=O(height of tree)
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.ans = 1
+        def depth(node):
+            if not node:
+                return 0
+            L = depth(node.left)
+            R = depth(node.right)
+            self.ans = max(self.ans, L + R + 1)
+            return max(L, R) + 1
+
+        depth(root)
+        return self.ans - 1
