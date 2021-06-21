1cd081021e1f33f71bae1a5f0b6d0b82f40ea6f5
chopchap <13806680065@163.com>
2021-06-21 09:34:18 +0800

    -

--- Math/171. Excel Sheet Column Number.py
+++ Math/171. Excel Sheet Column Number.py
@@ -1,6 +1,16 @@
+"""
+1. Clarification
+2. Possible solutions
+    - Brute force
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n), S=O(1)
 class Solution:
-  def titleToNumber(self, s: str) -> int:
-    ans = 0
-    for ch in s:
-      ans = ans*26 + ord(ch)-ord('A') + 1
-    return ans
+    def titleToNumber(self, columnTitle: str) -> int:
+        ans = 0
+        for ch in columnTitle:
+            ans = ans*26 + ord(ch)-ord('A') + 1
+        return ans
