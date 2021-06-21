893190a7e75577c39e0e6b3b64401b8ff9732998
chopchap <13806680065@163.com>
2021-06-21 09:54:37 +0800

    -

--- Math/172. Factorial Trailing Zeroes.py
+++ Math/172. Factorial Trailing Zeroes.py
@@ -1,10 +1,17 @@
+"""
+1. Clarification
+2. Possible solutions
+    - Maths
+3. Coding
+4. Tests
+"""
+
+
+# T=O(lgn), S=O(1)
 class Solution:
-  def trailingZeroes(self, n: int) -> int:
-    """
-    T=O(lgn), S=O(1)
-    """
-    zero_count = 0
-    while n > 0:
-      n //= 5
-      zero_count += n
-    return zero_count
+    def trailingZeroes(self, n: int) -> int:
+        zero_count = 0
+        while n > 0:
+            n //= 5
+            zero_count += n
+        return zero_count
