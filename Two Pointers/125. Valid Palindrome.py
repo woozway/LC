6aa2519466419b0c9d5318df0617b018bc78cd77
chopchap <13806680065@163.com>
2021-06-21 09:31:00 +0800

    -

--- Two Pointers/125. Valid Palindrome.py
+++ Two Pointers/125. Valid Palindrome.py
@@ -1,11 +1,46 @@
+"""
+1. Clarification
+2. Possible solutions
+    - Reverse
+    - Two pointers v1
+    - Two pointers v2
+3. Coding
+4. Tests
+"""
+
+
+# T=O(n), S=O(n)
 class Solution:
-  def isPalindrome(self, s: str) -> bool:
-    s1 = [c.lower() for c in s if c.isalnum()]
-    i = 0
-    j = len(s1)-1
-    while i < j:
-      if s1[i] != s1[j]:
-        return False
-      i += 1
-      j -= 1
-    return True
+    def isPalindrome(self, s: str) -> bool:
+        sgood = "".join(ch.lower() for ch in s if ch.isalnum())
+        return sgood == sgood[::-1]
+
+
+# T=O(n), S=O(n)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sgood = "".join(ch.lower() for ch in s if ch.isalnum())
+        n = len(sgood)
+        left, right = 0, n - 1
+        while left < right:
+            if sgood[left] != sgood[right]:
+                return False
+            left, right = left + 1, right - 1
+        return True
+
+
+# T=O(n), S=O(1)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n = len(s)
+        left, right = 0, n - 1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if left < right:
+                if s[left].lower() != s[right].lower():
+                    return False
+                left, right = left + 1, right - 1
+        return True
