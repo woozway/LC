67be9b18a29956c6d89c1e8caed0b0b813741d4e
chopchap <13806680065@163.com>
2021-06-21 09:59:15 +0800

    -

--- Bit Manipulation/190. Reverse Bits.py
+++ Bit Manipulation/190. Reverse Bits.py
@@ -1,8 +1,48 @@
+"""
+1. Clarification
+2. Possible solutions
+    - Bit by Bit
+    - Byte by Byte with Memoization
+    - Mask and Shift
+3. Coding
+4. Tests
+"""
+
+
+# T=O(1), S=O(1)
 class Solution:
-  def reverseBits(self, n):
-    n = (n >> 16) | (n << 16)
-    n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8)
-    n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4)
-    n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2)
-    n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1)
-    return n
+    def reverseBits(self, n: int) -> int:
+        ret, power = 0, 31
+        while n:
+            ret += (n & 1) << power
+            n = n >> 1
+            power -= 1
+        return ret
+
+
+# T=O(1), S=O(1)
+import functools
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ret, power = 0, 24
+        while n:
+            ret += self.reverseByte(n & 0xff) << power
+            n = n >> 8
+            power -= 8
+        return ret
+
+    @functools.lru_cache(maxsize=256)
+    def reverseByte(self, byte):
+        return (byte * 0x0202020202 & 0x010884422010) % 1023
+
+
+# T=O(1), S=O(1)
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        n = (n >> 16) | (n << 16)
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8)
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4)
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2)
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1)
+        return n
