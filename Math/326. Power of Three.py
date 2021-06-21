17545e1bfbcb68c890acc37ef6b4141a5ed47684
chopchap <13806680065@163.com>
2021-06-21 10:31:26 +0800

    -

--- Math/326. Power of Three.py
+++ Math/326. Power of Three.py
@@ -1,6 +1,24 @@
+"""
+1. Clarification
+2. Possible solutions
+    - Brute force
+    - Behave good
+3. Coding
+4. Tests
+"""
+
+
+# T=O(1), S=O(1)
 class Solution:
-  def isPowerOfThree(self, n: int) -> bool:
-    """
-    3**19 is the maximun num of power of 3 in integer range
-    """
-    return n > 0 and 3**19 % n == 0
+    def isPowerOfThree(self, n: int) -> bool:
+        return n > 0 and 3**19 % n == 0
+
+
+# T=O(1), S=O(1)
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n == 0:
+            return False
+        while n % 3 == 0:
+            n //= 3
+        return n == 1
