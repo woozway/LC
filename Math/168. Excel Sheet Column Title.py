79408616a59484e9c21cea91239229ab964469f0
chopchap <13806680065@163.com>
2021-06-29 16:38:11 +0800

    -

--- Math/168. Excel Sheet Column Title.py
+++ Math/168. Excel Sheet Column Title.py
@@ -10,9 +10,9 @@
 # T=O(lg26 * columnNumber), S=O(lg26 * columnNumber)
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
-        ret = ''
-        while columnNumber:
+        ans = list()
+        while columnNumber > 0:
             columnNumber -= 1
-            ret += chr(columnNumber%26 + 65)
+            ans.append(chr(columnNumber % 26 + ord("A")))
             columnNumber //= 26
-        return ret[::-1]
+        return ''.join(ans[::-1])
