dd07a8f3fc276a84a9aca3a110089befb415feb2
chopchap <13806680065@163.com>
2021-07-08 22:06:14 +0800

    -

--- Backtracking/131. Palindrome Partitioning.py
+++ Backtracking/131. Palindrome Partitioning.py
@@ -17,20 +17,19 @@ class Solution:
             for j in range(i + 1, n):
                 dp[i][j] = (s[i] == s[j]) and dp[i + 1][j - 1]
 
-        ret = list()
-        ans = list()
+        ret, ans = list(), list()
 
-        def dfs(i: int):
+        def backtrack(i: int):
             if i == n:
                 ret.append(ans[:])
                 return
             for j in range(i, n):
                 if dp[i][j]:
                     ans.append(s[i:j + 1])
-                    dfs(j + 1)
+                    backtrack(j + 1)
                     ans.pop()
 
-        dfs(0)
+        backtrack(0)
         return ret
 
 
@@ -46,16 +45,16 @@ class Solution:
                 return 1
             return isPalindrome(i + 1, j - 1) if s[i] == s[j] else -1
 
-        def dfs(i: int):
+        def backtrack(i: int):
             if i == n:
                 ret.append(ans[:])
                 return
             for j in range(i, n):
                 if isPalindrome(i, j) == 1:
                     ans.append(s[i:j + 1])
-                    dfs(j + 1)
+                    backtrack(j + 1)
                     ans.pop()
 
-        dfs(0)
+        backtrack(0)
         isPalindrome.cache_clear()
         return ret
