208a2f5fe68520d279d246dcd2adbbab0f1e0516
chopchap <13806680065@163.com>
2021-07-14 22:36:59 +0800

    -

--- Array/73. Set Matrix Zeroes.py
+++ Array/73. Set Matrix Zeroes.py
@@ -1,14 +1,54 @@
+"""
+1. Clarification
+2. Possible solutions
+    - HashSet
+    - O(1) Space, Efficient Solution
+3. Coding
+4. Tests
+"""
+
+
+# T=O(m*n), S=O(m+n)
 class Solution:
-  def setZeroes(self, matrix: List[List[int]]) -> None:
-    R = len(matrix)
-    C = len(matrix[0])
-    rows, cols = set(), set()
-    for i in range(R):
-      for j in range(C):
-        if matrix[i][j] == 0:
-          rows.add(i)
-          cols.add(j)
-    for i in range(R):
-      for j in range(C):
-        if i in rows or j in cols:
-          matrix[i][j] = 0
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        R = len(matrix)
+        C = len(matrix[0])
+        rows, cols = set(), set()
+
+        for i in range(R):
+            for j in range(C):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+
+        for i in range(R):
+            for j in range(C):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
+
+
+# T=O(m*n), S=O(1)
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        is_col = False
+        R = len(matrix)
+        C = len(matrix[0])
+
+        for i in range(R):
+            if matrix[i][0] == 0:
+                is_col = True
+            for j in range(1, C):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+            for j in range(1, C):
+                if not matrix[i][0] or not matrix[0][j]:
+                    matrix[i][j] = 0
+
+        if matrix[0][0] == 0:
+            for j in range(C):
+                matrix[0][j] = 0
+
+        if is_col:
+            for i in range(R):
+                matrix[i][0] = 0
