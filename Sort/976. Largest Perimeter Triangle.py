a54f1c8d8787c539b5699062425d241df37b88a9
chopchap <22060468@zju.edu.cn>
2021-07-17 13:40:43 +0800

    -

--- Sort/976. Largest Perimeter Triangle.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-  def largestPerimeter(self, A: List[int]) -> int:
-    A.sort()
-    for i in range(len(A)-1, 1, -1):
-      if A[i-2] + A[i-1] > A[i]:
-        return A[i-2] + A[i-1] + A[i]
-    else:
-      return 0
