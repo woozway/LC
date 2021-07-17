1dbc516fbbc85c2fff7193f9f655d6208204bc0a
chopchap <22060468@zju.edu.cn>
2021-07-17 13:38:44 +0800

    -

--- Greedy/1518. Water Bottles.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-  def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-    cnt = numBottles
-    while numBottles >= numExchange:
-      cnt += 1
-      numBottles = numBottles - numExchange + 1
-    return cnt
