c17b3ba427ea4ce4cf1e77e71771bcebc2ecef83
chopchap <22060468@zju.edu.cn>
2021-07-27 14:12:16 +0800

    -

--- Algorithms/Bit Manipulation/191. Number of 1 Bits.py
+++ Algorithms/Bit Manipulation/191. Number of 1 Bits.py
@@ -1,12 +1,20 @@
 """
 1. Clarification
 2. Possible solutions
- - x & 1 + count + x >> 1
- - Brian Kernighan Algorithm: x & (x - 1) to get the last 1-bit
+    - pythonic
+    - x & 1 + count + x >> 1
+    - Brian Kernighan Algorithm: x & (x - 1) to get the last 1-bit
 3. Coding
 4. Tests
 """
 
+
+# T=O(n), S=O(1)
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        return bin(n).count("1")
+    
+    
 # # T=O(1), S=O(1)
 # class Solution:
 #     def hammingWeight(self, n: int) -> int:
@@ -16,6 +24,7 @@
 #                 rst += 1
 #             mask <<= 1
 #         return rst
+
 
 # T=O(1), S=O(1)
 class Solution:
