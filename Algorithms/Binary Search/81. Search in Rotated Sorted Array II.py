29fc5abf8c9ef00d27ee74c94fb3242e11490563
chopchap <22060468@zju.edu.cn>
2021-08-14 20:43:32 +0800

    -

---
+++ Algorithms/Binary Search/81. Search in Rotated Sorted Array II.py
@@ -0,0 +1,60 @@
+"""
+1. Clarification
+2. Possible solutions
+    - Binary search I
+3. Coding
+4. Tests
+"""
+
+
+# T=O(lgn)*, S=O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if not nums: return False
+        n = len(nums)
+        if n == 1: return nums[0] == target
+        lo, hi = 0, n-1
+        while lo <= hi:
+            mid = (lo + hi)//2
+            if nums[mid] == target:
+                return True
+            if nums[lo] == nums[mid] == nums[hi]:
+                lo, hi = lo+1, hi-1
+            elif nums[lo] <= nums[mid]:
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid-1
+                else:
+                    lo = mid+1
+            else:
+                if nums[mid] < target <= nums[hi]:
+                    lo = mid+1
+                else:
+                    hi = mid-1
+        return False
+
+
+# T=O(lgn)*, S=O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if not nums: return False
+        n = len(nums)
+        if n == 1: return nums[0] == target
+        lo, hi = 0, n-1
+        while lo <= hi:
+            mid = (lo + hi)//2
+            if nums[mid] == target:
+                return True
+            if nums[lo] == nums[mid] == nums[hi]:
+                lo, hi = lo+1, hi-1
+            elif nums[mid] <= nums[hi]:
+                if nums[mid] < target <= nums[hi]:
+                    lo = mid+1
+                else:
+                    hi = mid-1
+            else:
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid-1
+                else:
+                    lo = mid+1
+        return False
+
