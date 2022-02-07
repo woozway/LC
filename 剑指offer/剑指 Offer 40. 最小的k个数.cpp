dcbb93195f5a6c1a7e5fa7a7c84a1888792c839b
Woo Zway <wuzw@zju.edu.cn>
2022-02-07 17:13:37 +0800

    -

--- 剑指offer/剑指 Offer 40. 最小的k个数.cpp
+++ 剑指offer/剑指 Offer 40. 最小的k个数.cpp
@@ -11,7 +11,7 @@ public:
     }
     // need to run Knuth shuffle algorithm to randomise arr first
     int lo = 0, hi = arr.size()-1;
-    while (hi > lo) {
+    while (lo < hi) {
       int i = partition(arr, lo, hi);
       if (i < k) lo = i + 1;
       else if (i > k) hi = i - 1;
@@ -23,12 +23,22 @@ public:
     return v;
   }
 
+  int median3(vector<int>& arr, int l, int r) {
+    int centre = (l+r)/2;
+    if (arr[l] > arr[centre]) swap(arr[l], arr[centre]);
+    if (arr[l] > arr[r]) swap(arr[l], arr[r]);
+    if (arr[centre] > arr[r]) swap(arr[centre], arr[r]);
+    swap(arr[centre], arr[l]);
+    return arr[l];
+  }
+
   int partition(vector<int>& arr, int lo, int hi) {
-    int i = lo + 1, j = hi;
+    int pivot = median3(arr, lo, hi);
+    int i = lo+1, j = hi;
     while (i <= j) {
-      while (i <= hi && arr[i] <= arr[lo]) i += 1;
+      while (i <= hi && arr[i] <= pivot) i += 1;
       if (i > j) break;
-      while (j > lo && arr[j] >= arr[lo]) j -= 1;
+      while (j > lo && arr[j] >= pivot) j -= 1;
       if (i > j) break;
       swap(arr[i], arr[j]);
     }
