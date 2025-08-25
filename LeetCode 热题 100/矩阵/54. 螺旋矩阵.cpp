14310fad2a1ef460d8b91f7797dd9fd3ceeb6109
woozway <22060468@zju.edu.cn>
2025-08-25 19:09:52 +0800

    -

--- LeetCode 热题 100/矩阵/54. 螺旋矩阵.cpp
+++ LeetCode 热题 100/矩阵/54. 螺旋矩阵.cpp
@@ -6,29 +6,26 @@ public:
 
     vector<int> res;
     int di[4] = {0, 1, 0, -1}, dj[4] = {1, 0, -1, 0}, k = 0;
-    int rlim = n - 1, dlim = m - 1, llim = 0, ulim = 1; // right, down, left, up
+    // right, down, left, up
+    int rlim = n - 1, dlim = m - 1, llim = 0, ulim = 1;
     int x = 0, y = -1, cnt = 0;
-    while (1) {
-      while (y + dj[k] <= rlim)
-        y += dj[k], res.push_back(a[x][y]), cnt ++ ;
+    for ( ; ; ) {
+      while (y + dj[k] <= rlim) y += dj[k], res.push_back(a[x][y]), cnt ++ ;
       if (cnt >= m * n) break;
-      rlim --;
+      rlim -- ;
       k = (k + 1) % 4;
 
-      while (x + di[k] <= dlim)
-        x += di[k], res.push_back(a[x][y]), cnt ++ ;
+      while (x + di[k] <= dlim) x += di[k], res.push_back(a[x][y]), cnt ++ ;
       if (cnt >= m * n) break;
       dlim -- ;
       k = (k + 1) % 4;
 
-      while (y + dj[k] >= llim)
-        y += dj[k], res.push_back(a[x][y]), cnt ++ ;
+      while (y + dj[k] >= llim) y += dj[k], res.push_back(a[x][y]), cnt ++ ;
       if (cnt >= m * n) break;
       llim ++ ;
       k = (k + 1) % 4;
 
-      while (x + di[k] >= ulim)
-        x += di[k], res.push_back(a[x][y]), cnt ++ ;
+      while (x + di[k] >= ulim) x += di[k], res.push_back(a[x][y]), cnt ++ ;
       if (cnt >= m * n) break;
       ulim ++ ;
       k = (k + 1) % 4;
