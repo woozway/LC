39c9ccdd2e95af2a4dc94046d092ac055b9dc203
woozway <22060468@zju.edu.cn>
2025-09-04 18:34:35 +0800

    -

--- LeetCode 热题 100/动态规划/152. 乘积最大子数组.cpp
+++ LeetCode 热题 100/动态规划/152. 乘积最大子数组.cpp
@@ -15,8 +15,8 @@ public:
 
     f[0][0] = f[0][1] = a[0];
     for (int i = 1; i < n; i ++ ) {
-      f[i][0] = max(max(f[i - 1][0] * a[i], f[i - 1][1] * a[i]), a[i]);
-      f[i][1] = min(min(f[i - 1][0] * a[i], f[i - 1][1] * a[i]), a[i]);
+      f[i][0] = max({f[i - 1][0] * a[i], f[i - 1][1] * a[i], a[i]});
+      f[i][1] = min({f[i - 1][0] * a[i], f[i - 1][1] * a[i], a[i]});
     }
 
     int res = INT_MIN;
